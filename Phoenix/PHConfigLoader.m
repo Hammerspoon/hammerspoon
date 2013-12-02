6948060ecebe899c6b977d54150851738b8e6f3b
Steven Degutis <steven@cleancoders.com>
2013-12-02 01:33:20 -0600

    FINE apple, FINE. jerks.

--- Phoenix/PHConfigLoader.m
+++ Phoenix/PHConfigLoader.m
@@ -11,42 +11,9 @@
 #import <JavaScriptCore/JavaScriptCore.h>
 #import "PHAPI.h"
 
-
-
-
-
-#import <objc/runtime.h>
-
-
 @implementation PHConfigLoader
 
 - (void) reload {
-    
-    Class k =
-    [PHAPI self];
-    
-    Protocol* p = objc_allocateProtocol("PHHotKeyJSExport1234");
-    
-    protocol_addProtocol(p, @protocol(JSExport));
-    
-    Method* methods = class_copyMethodList(k, NULL);
-    
-    for (Method* method = methods; *method; method++) {
-        SEL name = method_getName(*method);
-        const char* types = method_getTypeEncoding(*method);
-        protocol_addMethodDescription(p, name, types, YES, YES);
-    }
-    
-    free(methods);
-    
-    objc_registerProtocol(p);
-    
-    
-    
-    class_addProtocol(k, p);
-    
-    
-    
     JSContext* ctx = [[JSContext alloc] initWithVirtualMachine:[[JSVirtualMachine alloc] init]];
     
     NSURL* _jsURL = [[NSBundle mainBundle] URLForResource:@"underscore-min" withExtension:@"js"];
