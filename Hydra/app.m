972edc18eff4ccd4043cbbead9178e3aff9613ae
Steven Degutis <steven@cleancoders.com>
2014-04-24 11:56:53 -0500

    experimenting with userdata

--- Hydra/app.m
+++ Hydra/app.m
@@ -1,10 +1,19 @@
 #import "lua/lauxlib.h"
 
+// stack: [table]
 static pid_t app_get_app_pid(lua_State* L) {
-    lua_getfield(L, 1, "pid");
+    lua_getfield(L, -1, "pid");
     pid_t pid = lua_tonumber(L, -1);
     lua_pop(L, 1);
     return pid;
+}
+
+// stack: [table]
+static AXUIElementRef app_get_app_carbonapp(lua_State* L) {
+    lua_getfield(L, -1, "_carbonapp");
+    AXUIElementRef* ud = lua_touserdata(L, -1);
+    lua_pop(L, 2);
+    return *ud;
 }
 
 static NSRunningApplication* app_get_app(lua_State* L) {
@@ -18,8 +27,7 @@ static int app_title(lua_State* L) {
 }
 
 static int app_is_hidden(lua_State* L) {
-    pid_t pid = app_get_app_pid(L);
-    AXUIElementRef carbonapp = AXUIElementCreateApplication(pid); // TODO: put this in a field on self and give it a __gc
+    AXUIElementRef carbonapp = app_get_app_carbonapp(L);
     
     CFTypeRef _isHidden;
     NSNumber* isHidden = @NO;
@@ -27,10 +35,15 @@ static int app_is_hidden(lua_State* L) {
         isHidden = CFBridgingRelease(_isHidden);
     }
     
-    CFRelease(carbonapp); // TODO: move this into a __gc
-    
     lua_pushboolean(L, [isHidden boolValue]);
     return 1;
+}
+
+static int app_gc(lua_State* L) {
+    AXUIElementRef* ud = lua_touserdata(L, 1);
+    AXUIElementRef app = *ud;
+    CFRelease(app);
+    return 0;
 }
 
 static int app_running_apps(lua_State* L) {
@@ -39,14 +52,25 @@ static int app_running_apps(lua_State* L) {
     
     for (NSRunningApplication* runningApp in [[NSWorkspace sharedWorkspace] runningApplications]) {
         pid_t p = [runningApp processIdentifier];
+        AXUIElementRef carbonapp = AXUIElementCreateApplication(p);
         
-        lua_pushnumber(L, i++);                 // [i]
-        lua_newtable(L);                        // [i, {}]
-        lua_pushnumber(L, p);                   // [i, {}, pid]
-        lua_setfield(L, -2, "pid");             // [i, {}]
-        lua_pushvalue(L, lua_upvalueindex(1));  // [i, {}, mt]
-        lua_setmetatable(L, -2);                // [i, {}]
-        lua_settable(L, -3);                    // []
+        lua_pushnumber(L, i++);                                           // [apps, i]
+        lua_newtable(L);                                                  // [apps, i, {}]
+        
+        lua_pushnumber(L, p);                                             // [apps, i, {}, pid]
+        lua_setfield(L, -2, "pid");                                       // [apps, i, {}]
+        
+        AXUIElementRef* ud = lua_newuserdata(L, sizeof(AXUIElementRef));  // [apps, i, {}, userdata]
+        *ud = carbonapp;                                                  // [apps, i, {}, userdata]
+        lua_pushvalue(L, lua_upvalueindex(2));                            // [apps, i, {}, userdata, userdata_mt]
+        
+        lua_setmetatable(L, -2);                                          // [apps, i, {}, userdata]
+        lua_setfield(L, -2, "_carbonapp");                                // [apps, i, {}]
+        
+        lua_pushvalue(L, lua_upvalueindex(1));                            // [apps, i, {}, mt]
+        lua_setmetatable(L, -2);                                          // [apps, i, {}]
+        
+        lua_settable(L, -3);                                              // [apps]
     }
     
     return 1;
@@ -61,9 +85,12 @@ static const luaL_Reg applib[] = {
 int luaopen_app(lua_State * L) {
     lua_newtable(L);                           // [app]
     lua_newtable(L);                           // [app, {}]
-    luaL_newlib(L, applib);                    // [app, {}, {...}]
-    lua_setfield(L, -2, "__index");            // [app, {__index = {...}}]
-    lua_pushcclosure(L, app_running_apps, 1);  // [app, running_apps]
+    luaL_newlib(L, applib);                    // [app, {}, {..app..}]
+    lua_setfield(L, -2, "__index");            // [app, {__index = {..app..}}]
+    lua_newtable(L);                           // [app, {__index = {..app..}}, {}]
+    lua_pushcfunction(L, app_gc);              // [app, {__index = {..app..}}, {}, gc}
+    lua_setfield(L, -2, "__gc");               // [app, {__index = {..app..}}, {__gc = gc}]
+    lua_pushcclosure(L, app_running_apps, 2);  // [app, running_apps]
     lua_setfield(L, -2, "running_apps");       // [app]
     
     return 1;
