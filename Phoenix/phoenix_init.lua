b96d74d3bbd2300bf78c25c151e6eea8a3a2fd56
Steven Degutis <steven@cleancoders.com>
2014-06-15 01:29:09 -0500

    Starting to add a nicer API layer.

--- Phoenix/phoenix_init.lua
+++ Phoenix/phoenix_init.lua
@@ -1,48 +1,58 @@
--- print(__api)
--- print(__api.app_running_apps)
+__api.alert_show("Phoenix config loaded", 1.5)
 
--- for k, v in pairs(__api.app_running_apps()) do
---    print(k, __api.app_title(v))
+
+
+
+
+
+
+
+
+-- -- print(__api)
+-- -- print(__api.app_running_apps)
+
+-- -- for k, v in pairs(__api.app_running_apps()) do
+-- --    print(k, __api.app_title(v))
+-- -- end
+
+-- __api.menu_icon_show()
+
+-- -- lol = __api.path_watcher_start("/Users/sdegutis/projects/phoenix", function()
+-- --                             print(lol)
+-- --                             __api.path_watcher_stop(lol)
+-- --                                                              end)
+
+-- -- __api.alert_show("hi!", 1)
+
+-- if false then
+
+--    -- TODO: fix this!
+
+--    for k, pid in pairs(__api.app_running_apps()) do
+--       local x = __api.app_get_windows(pid)
+--       print(__api.app_title(pid))
+--       for k, v in pairs(x) do
+--          print(k, v)
+--       end
+--    end
+
 -- end
 
-__api.menu_icon_show()
-
--- lol = __api.path_watcher_start("/Users/sdegutis/projects/phoenix", function()
---                             print(lol)
---                             __api.path_watcher_stop(lol)
---                                                              end)
-
--- __api.alert_show("hi!", 1)
-
-if false then
-
-   -- TODO: fix this!
-
-   for k, pid in pairs(__api.app_running_apps()) do
-      local x = __api.app_get_windows(pid)
-      print(__api.app_title(pid))
-      for k, v in pairs(x) do
-         print(k, v)
-      end
-   end
-
-end
-
--- __api.hotkey_setup(function(uid)
---                       __api.menu_icon_hide()
---                    end)
--- __api.hotkey_register(true, true, true, false, "s")
+-- -- __api.hotkey_setup(function(uid)
+-- --                       __api.menu_icon_hide()
+-- --                    end)
+-- -- __api.hotkey_register(true, true, true, false, "s")
 
 
-print("done")
+-- print("done")
 
--- local x, y = __api.mouse_get()
+-- -- local x, y = __api.mouse_get()
 
--- __api.hotkey_setup(function(uid)
---                       __api.mouse_set(x, y)
---                       print("got: " .. tostring(uid))
---                    end)
+-- -- __api.hotkey_setup(function(uid)
+-- --                       __api.mouse_set(x, y)
+-- --                       print("got: " .. tostring(uid))
+-- --                    end)
 
--- local uid, carbonkey = __api.hotkey_register(true, true, true, false, "s")
+-- -- local uid, carbonkey = __api.hotkey_register(true, true, true, false, "s")
 
--- print(uid, carbonkey)
+-- -- print(uid, carbonkey)
