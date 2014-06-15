7b4606a01d0be00ba780dbf0c007069bdb9df245
Steven Degutis <steven@cleancoders.com>
2014-06-14 19:40:47 -0500

    Making sure that the new hotkey API works.

--- Phoenix/phoenix_init.lua
+++ Phoenix/phoenix_init.lua
@@ -1,1 +1,9 @@
 print("done")
+
+__api.hotkey_setup(function(uid)
+    print("got: " .. tostring(uid))
+end)
+
+local uid, carbonkey = __api.hotkey_register(true, true, true, false, "s")
+
+print(uid, carbonkey)
