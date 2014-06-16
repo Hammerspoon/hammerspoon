c394672da3c3a1c3ea6fff14011ee58f6215c682
Steven Degutis <steven@cleancoders.com>
2014-06-16 16:15:44 -0500

    Changed application Lua API to not use separate __index instance.

--- Phoenix/application.lua
+++ Phoenix/application.lua
@@ -2,33 +2,31 @@ local util = require("util")
 
 local application = {}
 
-local application_instance = {}
+function application:title() return __api.application_title(self.pid) end
+function application:ishidden() return __api.application_is_hidden(self.pid) end
+function application:show() __api.application_show(self.pid) end
+function application:hide() __api.application_hide(self.pid) end
+function application:kill() __api.application_kill(self.pid) end
+function application:kill9() __api.application_kill9(self.pid) end
 
-function application_instance:title() return __api.application_title(self.pid) end
-function application_instance:ishidden() return __api.application_is_hidden(self.pid) end
-function application_instance:show() __api.application_show(self.pid) end
-function application_instance:hide() __api.application_hide(self.pid) end
-function application_instance:kill() __api.application_kill(self.pid) end
-function application_instance:kill9() __api.application_kill9(self.pid) end
-
-function application_instance:windows()
+function application:windows()
   local window = require("window")
   return util.map(__api.application_get_windows(self.pid), window.rawinit)
 end
 
-function application_instance:visible_windows()
+function application:visible_windows()
   local window = require("window")
   return util.filter(self:windows(), window.isvisible)
 end
 
-local application_instance_metadata = {__index = application_instance}
+local application_metadata = {__index = application}
 
-function application_instance_metadata.__eq(a, b)
+function application_metadata.__eq(a, b)
   return a.pid == b.pid
 end
 
 function application.rawinit(pid)
-  return setmetatable({pid = pid}, application_instance_metadata)
+  return setmetatable({pid = pid}, application_metadata)
 end
 
 function application.running_applications()
