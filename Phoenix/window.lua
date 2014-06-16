a60971cf67bdb6cc0d2a6811ed230fe964d400fb
Steven Degutis <steven@cleancoders.com>
2014-06-15 20:20:47 -0500

    Move more methods to their eventual home.

--- Phoenix/window.lua
+++ Phoenix/window.lua
@@ -68,3 +68,153 @@ print("window!")
 --
 --     return lastScreen;
 -- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- + (NSArray*) allWindows;
+-- + (NSArray*) visibleWindows;
+-- + (PHWindow*) focusedWindow;
+-- + (NSArray*) visibleWindowsMostRecentFirst;
+-- - (NSArray*) otherWindowsOnSameScreen;
+-- - (NSArray*) otherWindowsOnAllScreens;
+--
+-- - (void) maximize;
+--
+--
+-- - (NSScreen*) screen;
+-- - (PHApp*) app;
+--
+-- - (void) focusWindowLeft;
+-- - (void) focusWindowRight;
+-- - (void) focusWindowUp;
+-- - (void) focusWindowDown;
+--
+-- - (NSArray*) windowsToWest;
+-- - (NSArray*) windowsToEast;
+-- - (NSArray*) windowsToNorth;
+-- - (NSArray*) windowsToSouth;
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+-- - (void) maximize {
+--     CGRect screenRect = [[self screen] frameWithoutDockOrMenu];
+--     [self setFrame: screenRect];
+-- }
+--
+--
+--
+--
+--
+-- // focus
+--
+--
+-- NSPoint SDMidpoint(NSRect r) {
+--     return NSMakePoint(NSMidX(r), NSMidY(r));
+-- }
+--
+-- - (NSArray*) windowsInDirectionFn:(double(^)(double angle))whichDirectionFn
+--                 shouldDisregardFn:(BOOL(^)(double deltaX, double deltaY))shouldDisregardFn
+-- {
+--     PHWindow* thisWindow = [PHWindow focusedWindow];
+--     NSPoint startingPoint = SDMidpoint([thisWindow frame]);
+--
+--     NSArray* otherWindows = [thisWindow otherWindowsOnAllScreens];
+--     NSMutableArray* closestOtherWindows = [NSMutableArray arrayWithCapacity:[otherWindows count]];
+--
+--     for (PHWindow* win in otherWindows) {
+--         NSPoint otherPoint = SDMidpoint([win frame]);
+--
+--         double deltaX = otherPoint.x - startingPoint.x;
+--         double deltaY = otherPoint.y - startingPoint.y;
+--
+--         if (shouldDisregardFn(deltaX, deltaY))
+--             continue;
+--
+--         double angle = atan2(deltaY, deltaX);
+--         double distance = hypot(deltaX, deltaY);
+--
+--         double angleDifference = whichDirectionFn(angle);
+--
+--         double score = distance / cos(angleDifference / 2.0);
+--
+--         [closestOtherWindows addObject:@{
+--                                          @"score": @(score),
+--                                          @"win": win,
+--                                          }];
+--     }
+--
+--     NSArray* sortedOtherWindows = [closestOtherWindows sortedArrayUsingComparator:^NSComparisonResult(NSDictionary* pair1, NSDictionary* pair2) {
+--         return [[pair1 objectForKey:@"score"] compare: [pair2 objectForKey:@"score"]];
+--     }];
+--
+--     return sortedOtherWindows;
+-- }
+--
+-- - (void) focusFirstValidWindowIn:(NSArray*)closestWindows {
+--     for (PHWindow* win in closestWindows) {
+--         if ([win focusWindow])
+--             break;
+--     }
+-- }
+--
+-- - (NSArray*) windowsToWest {
+--     return [[self windowsInDirectionFn:^double(double angle) { return M_PI - abs(angle); }
+--                      shouldDisregardFn:^BOOL(double deltaX, double deltaY) { return (deltaX >= 0); }] valueForKeyPath:@"win"];
+-- }
+--
+-- - (NSArray*) windowsToEast {
+--     return [[self windowsInDirectionFn:^double(double angle) { return 0.0 - angle; }
+--                      shouldDisregardFn:^BOOL(double deltaX, double deltaY) { return (deltaX <= 0); }] valueForKeyPath:@"win"];
+-- }
+--
+-- - (NSArray*) windowsToNorth {
+--     return [[self windowsInDirectionFn:^double(double angle) { return -M_PI_2 - angle; }
+--                      shouldDisregardFn:^BOOL(double deltaX, double deltaY) { return (deltaY >= 0); }] valueForKeyPath:@"win"];
+-- }
+--
+-- - (NSArray*) windowsToSouth {
+--     return [[self windowsInDirectionFn:^double(double angle) { return M_PI_2 - angle; }
+--                      shouldDisregardFn:^BOOL(double deltaX, double deltaY) { return (deltaY <= 0); }] valueForKeyPath:@"win"];
+-- }
+--
+-- - (void) focusWindowLeft {
+--     [self focusFirstValidWindowIn:[self windowsToWest]];
+-- }
+--
+-- - (void) focusWindowRight {
+--     [self focusFirstValidWindowIn:[self windowsToEast]];
+-- }
+--
+-- - (void) focusWindowUp {
+--     [self focusFirstValidWindowIn:[self windowsToNorth]];
+-- }
+--
+-- - (void) focusWindowDown {
+--     [self focusFirstValidWindowIn:[self windowsToSouth]];
+-- }
+--
+-- @end
