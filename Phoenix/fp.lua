44f982cb9c5b79b136447679257a2808764d4acd
Steven Degutis <steven@cleancoders.com>
2014-06-15 23:55:37 -0500

    Adding filter function.

--- Phoenix/fp.lua
+++ Phoenix/fp.lua
@@ -8,6 +8,14 @@ function fp.map(t, fn)
   return nt
 end
 
+function fp.filter(t, fn)
+  local nt = {}
+  for k, v in pairs(t) do
+    if fn(v) then table.insert(nt, v) end
+  end
+  return nt
+end
+
 function fp.contains(t, el)
   for k, v in pairs(t) do
     if v == el then
