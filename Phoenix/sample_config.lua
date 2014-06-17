b2fb1381f9a8b06ea8c51e231d906ae866bb707f
Steven Degutis <steven@cleancoders.com>
2014-06-17 17:28:51 -0500

    Adding a sample config to get started.

--- Phoenix/sample_config.lua
+++ Phoenix/sample_config.lua
@@ -1,3 +1,16 @@
 -- Save this as ~/.phoenix/init.lua and choose Reload Config from the menu
 
+local hotkey = require("hotkey")
+local window = require("window")
+local alert = require("alert")
+
 alert.show("Phoenix config loaded", 1.5)
+
+hotkey.bind({"cmd", "ctrl", "alt"}, "J", function()
+    -- move the window to the right a bit, and make it a little shorter
+    local win = window.focusedwindow()
+    local frame = win:frame()
+    frame.x = frame.x + 10
+    frame.h = frame.h - 10
+    win:setframe(frame)
+end)
