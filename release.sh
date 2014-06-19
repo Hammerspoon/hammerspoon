65fb91f19fe61dd2b2da0360face746428bd8b03
Steven Degutis <steven@cleancoders.com>
2014-06-19 07:39:55 -0500

    Phoenix -> Hydra

--- release.sh
+++ release.sh
@@ -4,13 +4,13 @@ set -e
 
 # build app
 xcodebuild clean build
-VERSION=$(defaults read $(pwd)/Phoenix/Phoenix-Info CFBundleVersion)
-FILENAME="Builds/Phoenix-$VERSION.app.tar.gz"
-LATEST="Builds/Phoenix-LATEST.app.tar.gz"
+VERSION=$(defaults read $(pwd)/Hydra/Hydra-Info CFBundleVersion)
+FILENAME="Builds/Hydra-$VERSION.app.tar.gz"
+LATEST="Builds/Hydra-LATEST.app.tar.gz"
 
 # build .zip
 rm -rf $FILENAME
-tar -zcf $FILENAME -C build/Release Phoenix.app
+tar -zcf $FILENAME -C build/Release Hydra.app
 echo "Created $FILENAME"
 
 # make "latest" version for the link in the readme
