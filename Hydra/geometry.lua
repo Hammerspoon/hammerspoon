1b4cdd8434ce22840489aa37e89a0697632061f3
Steven Degutis <steven@cleancoders.com>
2014-06-19 12:47:09 -0500

    Converting geometry.

--- Hydra/geometry.lua
+++ Hydra/geometry.lua
@@ -1,17 +1,5 @@
-local geometry = {}
-
-function geometry.rectmidpoint(r)
-  local x, y = __api.geometry_rectmidpoint(r.x, r.y, r.w, r.h)
-  return {x = x, y = y}
-end
-
-function geometry.rectintersection(r1, r2)
-  local x, y, w, h = __api.geometry_rectintersection(r1.x, r1.y, r1.w, r1.h, r2.x, r2.y, r2.w, r2.h)
-  return {x = x, y = y, w = w, h = h}
-end
-
 -- rotates counter-clockwise, n times (default 1)
-function geometry.rotate(point, aroundpoint, ntimes)
+function hydra.geometry.rotate(point, aroundpoint, ntimes)
   local p = {x = point.x, y = point.y}
   for i = 1, ntimes or 1 do
     local px = p.x
@@ -21,8 +9,18 @@ function geometry.rotate(point, aroundpoint, ntimes)
   return p
 end
 
-function geometry.hypot(p)
+function hydra.geometry.hypot(p)
   return math.sqrt(p.x * p.x + p.y * p.y)
 end
 
-return geometry
+function hydra.geometry.rect(x, y, w, h)
+  return {x = x, y = y, w = w, h = h}
+end
+
+function hydra.geometry.point(x, y)
+  return {x = x, y = y}
+end
+
+function hydra.geometry.size(w, h)
+  return {w = w, h = h}
+end
--- Hydra/geometry.m
+++ Hydra/geometry.m
@@ -1,37 +1,55 @@
 #import "lua/lauxlib.h"
 
+// args: [rect]
+// returns: [point]
 int geometry_rectmidpoint(lua_State* L) {
-    CGFloat x = lua_tonumber(L, 1);
-    CGFloat y = lua_tonumber(L, 2);
-    CGFloat w = lua_tonumber(L, 3);
-    CGFloat h = lua_tonumber(L, 4);
+    CGFloat x = (lua_getfield(L, 1, "x"), lua_tonumber(L, -1));
+    CGFloat y = (lua_getfield(L, 1, "y"), lua_tonumber(L, -1));
+    CGFloat w = (lua_getfield(L, 1, "w"), lua_tonumber(L, -1));
+    CGFloat h = (lua_getfield(L, 1, "h"), lua_tonumber(L, -1));
     
     NSRect r = NSMakeRect(x, y, w, h);
-    lua_pushnumber(L, NSMidX(r));
-    lua_pushnumber(L, NSMidY(r));
-    return 2;
+    
+    lua_newtable(L);
+    lua_pushnumber(L, NSMidX(r)); lua_setfield(L, -2, "x");
+    lua_pushnumber(L, NSMidY(r)); lua_setfield(L, -2, "y");
+    
+    return 1;
 }
 
-int geometry_rectintersection(lua_State* L) {
-    CGFloat x1 = lua_tonumber(L, 1);
-    CGFloat y1 = lua_tonumber(L, 2);
-    CGFloat w1 = lua_tonumber(L, 3);
-    CGFloat h1 = lua_tonumber(L, 4);
+// args: [rect]
+// returns: [point]
+int geometry_intersectionrect(lua_State* L) {
+    CGFloat x1 = (lua_getfield(L, 1, "x"), lua_tonumber(L, -1));
+    CGFloat y1 = (lua_getfield(L, 1, "y"), lua_tonumber(L, -1));
+    CGFloat w1 = (lua_getfield(L, 1, "w"), lua_tonumber(L, -1));
+    CGFloat h1 = (lua_getfield(L, 1, "h"), lua_tonumber(L, -1));
     
-    CGFloat x2 = lua_tonumber(L, 5);
-    CGFloat y2 = lua_tonumber(L, 6);
-    CGFloat w2 = lua_tonumber(L, 7);
-    CGFloat h2 = lua_tonumber(L, 8);
+    CGFloat x2 = (lua_getfield(L, 2, "x"), lua_tonumber(L, -1));
+    CGFloat y2 = (lua_getfield(L, 2, "y"), lua_tonumber(L, -1));
+    CGFloat w2 = (lua_getfield(L, 2, "w"), lua_tonumber(L, -1));
+    CGFloat h2 = (lua_getfield(L, 2, "h"), lua_tonumber(L, -1));
     
     NSRect r1 = NSMakeRect(x1, y1, w1, h1);
     NSRect r2 = NSMakeRect(x2, y2, w2, h2);
     NSRect r3 = NSIntersectionRect(r1, r2);
     
-    lua_pushnumber(L, r3.origin.x);
-    lua_pushnumber(L, r3.origin.y);
-    lua_pushnumber(L, r3.size.width);
-    lua_pushnumber(L, r3.size.height);
-    return 4;
+    lua_newtable(L);
+    lua_pushnumber(L, r3.origin.x);    lua_setfield(L, -2, "x");
+    lua_pushnumber(L, r3.origin.y);    lua_setfield(L, -2, "y");
+    lua_pushnumber(L, r3.size.width);  lua_setfield(L, -2, "w");
+    lua_pushnumber(L, r3.size.height); lua_setfield(L, -2, "h");
+    
+    return 1;
 }
 
-int luaopen_geometry(lua_State* L) { return 0; }
+static const luaL_Reg geometrylib[] = {
+    {"intersectionrect", geometry_intersectionrect},
+    {"rectmidpoint", geometry_rectmidpoint},
+    {NULL, NULL}
+};
+
+int luaopen_geometry(lua_State* L) {
+    luaL_newlib(L, geometrylib);
+    return 1;
+}
