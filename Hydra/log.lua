444f6dcf70fde25489cfa681f547c070b4f9f11a
Steven Degutis <steven@cleancoders.com>
2014-06-22 22:23:17 -0500

    Made easier API for registering log callbacks. Satisfies #29.

--- Hydra/log.lua
+++ Hydra/log.lua
@@ -2,16 +2,28 @@ api.log.lines = {}
 api.log._buffer = ""
 api.log.maxlines = 500
 
-function api.log.gotline(str)
+api.log.handlers = {}
+
+function api.log.addhandler(fn)
+  table.insert(api.log.handlers, fn)
+  return #api.log.handlers
+end
+
+function api.log.removehandler(idx)
+  api.log.handlers[idx] = nil
+end
+
+local function addline(str)
   table.insert(api.log.lines, str)
 
   if # api.log.lines == api.log.maxlines + 1 then
     -- we get called once per line; can't ever be maxlen + 2
     table.remove(api.log.lines, 1)
   end
-
-  api.alert("log: " .. str)
 end
+
+api.log.addhandler(addline)
+api.log.addhandler(function(str) api.alert("log: " .. str) end)
 
 function api.log._gotline(str)
   api.log._buffer = api.log._buffer .. str:gsub("\r", "\n")
@@ -22,7 +34,11 @@ function api.log._gotline(str)
 
     local newstr = string.sub(api.log._buffer, 1, startindex - 1)
     api.log._buffer = string.sub(api.log._buffer, endindex + 1, -1)
-    api.log.gotline(newstr)
+
+    -- call all the registered callbacks
+    for _, fn in pairs(api.log.handlers) do
+      fn(newstr)
+    end
   end
 end
 
