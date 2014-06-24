dbd6c01f6de8caed048df29df9543f006c58823a
Steven Degutis <steven@cleancoders.com>
2014-06-24 08:38:12 -0500

    Default implementation of api.updates.available also saves the vars for later.

--- Hydra/updates.lua
+++ Hydra/updates.lua
@@ -1,4 +1,8 @@
 api.doc.updates.available = {"api.updates.available = function(newversion, currentversion, changelog)", "Called when an update is avaiable, determined by api.updates.check(). Default implementation pushes a notification about it with the tag 'showupdate'."}
+
 function api.updates.available(newversion, currentversion, changelog)
-  api.notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "update")
+  api.updates.newversion = newversion
+  api.updates.currentversion = currentversion
+  api.updates.changelog = changelog
+  api.notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
 end
