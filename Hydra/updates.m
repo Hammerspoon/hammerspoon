1fc037b74c3641d5c85e5ca0fd4c9010cf009d66
Steven Degutis <steven@cleancoders.com>
2014-06-25 14:51:30 -0500

    Fixing bug in api.updates.check().

--- Hydra/updates.m
+++ Hydra/updates.m
@@ -17,14 +17,21 @@ int updates_check(lua_State* L) {
         return 0;
     }
     
+    int fnindex = luaL_ref(L, LUA_REGISTRYINDEX);
+    lua_pop(L, 2);
+    
     NSURL* url = [NSURL URLWithString:updates_url];
     NSURLRequest* req = [NSURLRequest requestWithURL:url];
     [NSURLConnection sendAsynchronousRequest:req
                                        queue:[NSOperationQueue mainQueue]
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
      {
+         lua_rawgeti(L, LUA_REGISTRYINDEX, fnindex);
+         luaL_unref(L, LUA_REGISTRYINDEX, fnindex);
+         
          if ([(NSHTTPURLResponse*)response statusCode] != 200) {
              printf("checked for update but github's api seems broken\n");
+             lua_pop(L, 1);
              return;
          }
          
@@ -35,8 +42,6 @@ int updates_check(lua_State* L) {
          lua_pushboolean(L, updateAvailable);
          if (lua_pcall(L, 1, 0, 0))
              hydra_handle_error(L);
-         
-         lua_pop(L, 2);
      }];
     
     return 0;
