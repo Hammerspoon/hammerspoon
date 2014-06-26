8bbb8742485e08234551c4d79ec14a630eeb9699
Steven Degutis <steven@cleancoders.com>
2014-06-26 11:13:16 -0500

    Don't check for updates on every launch; fixes #64.

--- Hydra/sample_config.lua
+++ Hydra/sample_config.lua
@@ -1,9 +1,15 @@
-api.alert("Hydra config loaded", 1.5)
+api.alert("Hydra sample config loaded", 1.5)
+
+-- save the time when updates are checked
+function checkforupdates()
+  api.updates.check()
+  api.settings.set('lastcheckedupdates', os.time())
+end
 
 -- show a helpful menu
 api.menu.show(function()
     local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
-    local updatefns = {[true] = api.updates.install, [false] = api.updates.check}
+    local updatefns = {[true] = api.updates.install, [false] = checkforupdates}
     local hasupdate = (api.updates.newversion ~= nil)
 
     return {
@@ -45,8 +51,11 @@ end
 -- api.autolaunch.set(true)
 
 -- check for updates every week
-api.timer.new(api.timer.weeks(1), api.updates.check):start()
+api.timer.new(api.timer.weeks(1), checkforupdates):start()
 api.notify.register("showupdate", showupdate)
 
--- also check right away
-api.updates.check()
+-- if this is your first time running Hydra, you're launching it more than a week later, check now
+local lastcheckedupdates = api.settings.get('lastcheckedupdates')
+if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - api.timer.days(7) then
+  checkforupdates()
+end
