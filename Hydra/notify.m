7c133179082643a8cc932dd0837b06b37f38068c
Steven Degutis <steven@cleancoders.com>
2014-06-28 20:32:26 -0500

    Getting rid of lua_getglobal from notify api

--- Hydra/notify.m
+++ Hydra/notify.m
@@ -3,7 +3,10 @@
 // you hate us Apple, don't you
 @interface PHNotificationDelegate : NSObject <NSUserNotificationCenterDelegate>
 @property lua_State* L;
+@property int notifyref;
 @end
+
+static PHNotificationDelegate* delegate;
 
 @implementation PHNotificationDelegate
 
@@ -11,17 +14,16 @@
 {
     NSString* tag = [[notification userInfo] objectForKey:@"tag"];
     
-    lua_State*L = self.L;
+    lua_State* L = self.L;
     
-    lua_getglobal(L, "api");
-    lua_getfield(L, -1, "notify");
+    lua_rawgeti(L, LUA_REGISTRYINDEX, delegate.notifyref);
     lua_getfield(L, -1, "_clicked");
     
     if (lua_isfunction(L, -1)) {
         lua_pushstring(L, [tag UTF8String]);
         if (lua_pcall(L, 1, 0, 0))
             hydra_handle_error(L);
-        lua_pop(L, 3);
+        lua_pop(L, 1);
     }
     else {
         lua_pop(L, 2);
@@ -40,22 +42,13 @@ static hydradoc doc_notify_show = {
 };
 
 int notify_show(lua_State* L) {
-    NSUserNotificationCenter* userNotificationCenter = [NSUserNotificationCenter defaultUserNotificationCenter]; // I'm starting to get the feeling you really do
-    
-    static PHNotificationDelegate* delegate;
-    if (!delegate) {
-        delegate = [[PHNotificationDelegate alloc] init];
-        delegate.L = L;
-        userNotificationCenter.delegate = delegate;
-    }
-    
     NSUserNotification* note = [[NSUserNotification alloc] init];
     note.title = [NSString stringWithUTF8String: lua_tostring(L, 1)];
     note.subtitle = [NSString stringWithUTF8String: lua_tostring(L, 2)];
     note.informativeText = [NSString stringWithUTF8String: lua_tostring(L, 3)];
     note.userInfo = @{@"tag": [NSString stringWithUTF8String: lua_tostring(L, 4)]};
     
-    [userNotificationCenter deliverNotification:note];
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:note];
     
     return 0;
 }
@@ -70,5 +63,13 @@ int luaopen_notify(lua_State* L) {
     hydra_add_doc_item(L, &doc_notify_show);
     
     luaL_newlib(L, notifylib);
+    
+    delegate = [[PHNotificationDelegate alloc] init];
+    delegate.L = L;
+    [NSUserNotificationCenter defaultUserNotificationCenter].delegate = delegate;
+    
+    lua_pushvalue(L, -1);
+    delegate.notifyref = luaL_ref(L, LUA_REGISTRYINDEX);
+    
     return 1;
 }
