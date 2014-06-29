439f960a6976bba6d94e887f2813f97637951d7c
Steven Degutis <steven@cleancoders.com>
2014-06-29 00:21:47 -0500

    Adding high-level error handler.

--- Hydra/rawinit.lua
+++ Hydra/rawinit.lua
@@ -1,32 +1,41 @@
--- core functions
-dofile(hydra.resourcesdir .. "/api.lua")
-dofile(hydra.resourcesdir .. "/fnutils.lua")
-dofile(hydra.resourcesdir .. "/geometry.lua")
-dofile(hydra.resourcesdir .. "/screen.lua")
-dofile(hydra.resourcesdir .. "/application.lua")
-dofile(hydra.resourcesdir .. "/window.lua")
-dofile(hydra.resourcesdir .. "/hotkey.lua")
-dofile(hydra.resourcesdir .. "/repl.lua")
-dofile(hydra.resourcesdir .. "/timer.lua")
-dofile(hydra.resourcesdir .. "/pathwatcher.lua")
-dofile(hydra.resourcesdir .. "/textgrid.lua")
-dofile(hydra.resourcesdir .. "/logger.lua")
-dofile(hydra.resourcesdir .. "/updates.lua")
-dofile(hydra.resourcesdir .. "/notify.lua")
-dofile(hydra.resourcesdir .. "/doc.lua")
-dofile(hydra.resourcesdir .. "/webview.lua")
+local ok, err = pcall(function()
 
--- make lives of third party authors easier
-doc.ext = {__doc = "Standard high-level namespace for third-party extensions."}
-ext = {}
+    -- core functions
+    dofile(hydra.resourcesdir .. "/api.lua")
+    dofile(hydra.resourcesdir .. "/fnutils.lua")
+    dofile(hydra.resourcesdir .. "/geometry.lua")
+    dofile(hydra.resourcesdir .. "/screen.lua")
+    dofile(hydra.resourcesdir .. "/application.lua")
+    dofile(hydra.resourcesdir .. "/window.lua")
+    dofile(hydra.resourcesdir .. "/hotkey.lua")
+    dofile(hydra.resourcesdir .. "/repl.lua")
+    dofile(hydra.resourcesdir .. "/timer.lua")
+    dofile(hydra.resourcesdir .. "/pathwatcher.lua")
+    dofile(hydra.resourcesdir .. "/textgrid.lua")
+    dofile(hydra.resourcesdir .. "/logger.lua")
+    dofile(hydra.resourcesdir .. "/updates.lua")
+    dofile(hydra.resourcesdir .. "/notify.lua")
+    dofile(hydra.resourcesdir .. "/doc.lua")
+    dofile(hydra.resourcesdir .. "/webview.lua")
 
-package.path = hydra.userfile("?") .. ";" .. package.path
+    -- make lives of third party authors easier
+    doc.ext = {__doc = "Standard high-level namespace for third-party extensions."}
+    ext = {}
 
-hydra._initiate_documentation_system()
+    package.path = hydra.userfile("?") .. ";" .. package.path
 
-if not hydra.check_accessibility(true) then
-  notify.show("Enable accessibility first", "", "Otherwise Hydra can't do very much.", "needs_accessibility")
+    hydra._initiate_documentation_system()
+
+    if not hydra.check_accessibility(true) then
+      notify.show("Enable accessibility first", "", "Otherwise Hydra can't do very much.", "needs_accessibility")
+    end
+
+    -- load user's config
+    hydra.call(hydra.reload)
+
+end)
+
+if not ok then
+  notify.show("Hydra apparently failed to initialize. Nothing else to do now but quit.", "", tostring(err), "")
+  os.exit()
 end
-
--- load user's config
-hydra.call(hydra.reload)
