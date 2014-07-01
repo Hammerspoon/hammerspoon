52d0204dcced776bde6ccfed966a0c498af583e6
Steven Degutis <steven@cleancoders.com>
2014-07-01 09:28:33 -0500

    Adding donation encouragement to sample config.

--- Hydra/sample_config.lua
+++ Hydra/sample_config.lua
@@ -65,3 +65,18 @@ local lastcheckedupdates = settings.get('lastcheckedupdates')
 if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
   checkforupdates()
 end
+
+
+
+
+-- I've worked hard to make Hydra useful and easy to use. I've also
+-- released it with a liberal open source license, so that you can do
+-- with it as you please. So, instead of charging for licenses, I'm
+-- asking for donations. If you find it helpful, I encourage you to
+-- donate what you believe would have been a fair price for a license:
+
+local function donate()
+  os.execute("open 'https://www.paypal.com/cgi-bin/webscr?business=sbdegutis@gmail.com&cmd=_donations&item_name=Hydra.app%20donation'")
+end
+
+hotkey.bind({"cmd", "alt", "ctrl"}, "D", donate)
