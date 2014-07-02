39aa1227d5d08d58e2b9596742652d04cf4c5968
Steven Degutis <steven@cleancoders.com>
2014-07-02 14:55:59 -0500

    Turning REPL into its own module.

--- Hydra/repl.lua
+++ Hydra/repl.lua
@@ -134,8 +134,16 @@ function Stdin:transposechar()
   self.chars[pos] = tmp
 end
 
-doc.hydra.repl = {"hydra.repl([opts]) -> textgrid", "Opens a readline-like REPL (Read-Eval-Print-Loop) that has full access to Hydra's API; type 'help' for more info.\n\nOptional `opts` arg can be a table with keys: inputcolor, stdoutcolor, resultcolor, backgroundcolor."}
-function hydra.repl(opts)
+
+repl = {}
+doc.repl = {__doc = [[
+The REPL (Read-Eval-Print-Loop) is excellent for exploring and experiment with Hydra's API.
+
+It has most of the familiar readline-like keybindings, including C-b, C-f, M-b, M-f to navigate your text, C-p and C-n to browse command history, etc.
+
+Type `help` in the REPL for info on how to use the documentation system.]]}
+doc.repl.open = {"repl.open([opts]) -> textgrid", "Opens a new REPL; the `opts` parameter is an optional table with keys: inputcolor, stdoutcolor, resultcolor, backgroundcolor; these are 6-digit CSS-like hex strings."}
+function repl.open(opts)
   local win = textgrid.open()
   win:settitle("Hydra REPL")
   win:protect()
