183262de514c234b2c7e5bb31425058e44439fdb
Steven Degutis <steven@cleancoders.com>
2014-07-06 09:52:37 -0500

    Mostly finished with webview; still needs GC method though.

--- Hydra/webview.m
+++ Hydra/webview.m
@@ -44,13 +44,56 @@ decisionListener:(id<WebPolicyDecisionListener>)listener
 
 @end
 
+
+
+#define hydra_webview(L, idx) (__bridge PHWebViewController*)*((void**)luaL_checkudata(L, idx, "webview"))
+
+static int webview_create(lua_State* L) {
+    PHWebViewController* wc = [[PHWebViewController alloc] init];
+    
+    void** ptr = lua_newuserdata(L, sizeof(void*));
+    *ptr = (__bridge_retained void*)wc;
+    
+    luaL_getmetatable(L, "webview");
+    lua_setmetatable(L, -2);
+    
+    lua_newtable(L);
+    lua_setuservalue(L, -2);
+    
+    return 1;
+}
+
+static void replace_webview_callback(lua_State* L, const char* key, int ref) {
+    lua_getfield(L, -1, key);
+    if (lua_isnumber(L, -1))
+        luaL_unref(L, LUA_REGISTRYINDEX, lua_tonumber(L, -1));
+    lua_pop(L, 1);
+    
+    lua_pushnumber(L, ref);
+    lua_setfield(L, -2, key);
+}
+
 static hydradoc doc_webview_clicked = {
     "webview", "clicked", "webview:clicked(fn(str))",
     "When a link is clicked with a URL like 'hydra:foo', the given function is called with 'foo' as its argument."
 };
 
 static int webview_clicked(lua_State* L) {
-    // TODO
+    PHWebViewController* wc = hydra_webview(L, 1);
+    luaL_checktype(L, LUA_REGISTRYINDEX, 2);
+    
+    int ref = luaL_ref(L, LUA_REGISTRYINDEX);
+    
+    lua_getuservalue(L, 1);
+    replace_webview_callback(L, "clicked_ref", ref);
+    
+    wc.clicked = ^(NSString* tag) {
+        lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
+        lua_pushstring(L, [tag UTF8String]);
+        if (lua_pcall(L, 1, 0, 0))
+            hydra_handle_error(L);
+    };
+    
     return 0;
 }
 
@@ -60,62 +103,43 @@ static hydradoc doc_webview_hidden = {
 };
 
 static int webview_hidden(lua_State* L) {
-    // TODO
+    PHWebViewController* wc = hydra_webview(L, 1);
+    luaL_checktype(L, LUA_REGISTRYINDEX, 2);
+    
+    int ref = luaL_ref(L, LUA_REGISTRYINDEX);
+    
+    lua_getuservalue(L, 1);
+    replace_webview_callback(L, "hidden_ref", ref);
+    
+    wc.closed = ^{
+        lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
+        if (lua_pcall(L, 0, 0, 0))
+            hydra_handle_error(L);
+    };
+    
     return 0;
 }
 
-static int webview_create(lua_State* L) {
-    PHWebViewController* wc = [[PHWebViewController alloc] init];
-    [wc showWindow: nil];
-    
-    lua_newtable(L);
-    
-    lua_pushvalue(L, -1);
-    int tableref = luaL_ref(L, LUA_REGISTRYINDEX);
-    
-    wc.clicked = ^(NSString* tag) {
-        lua_rawgeti(L, LUA_REGISTRYINDEX, tableref);
-        lua_getfield(L, -1, "clicked");
-        lua_remove(L, -2);
-        
-        if (lua_isfunction(L, -1)) {
-            lua_pushstring(L, [tag UTF8String]);
-            if (lua_pcall(L, 1, 0, 0))
-                hydra_handle_error(L);
-        }
-        else {
-            lua_pop(L, 1);
-        }
-    };
-    
-    wc.closed = ^{
-        lua_rawgeti(L, LUA_REGISTRYINDEX, tableref);
-        lua_getfield(L, -1, "closed");
-        
-        if (lua_isfunction(L, -1)) {
-            if (lua_pcall(L, 0, 0, 0))
-                hydra_handle_error(L);
-            
-            lua_pop(L, 1);
-        }
-        else {
-            lua_pop(L, 2);
-        }
-        
-        luaL_unref(L, LUA_REGISTRYINDEX, tableref);
-    };
-    
-    lua_pushlightuserdata(L, (__bridge_retained void*)wc);
-    lua_setfield(L, -2, "__wc");
-    
-    return 1;
+static hydradoc doc_webview_show = {
+    "webview", "show", "webview:show()",
+    "Makes the webview not hidden."
+};
+
+static int webview_show(lua_State* L) {
+    PHWebViewController* wc = hydra_webview(L, 1);
+    [[wc window] orderFront: nil];
+    return 0;
 }
 
-static PHWebViewController* get_window_controller(lua_State* L, int idx) {
-    lua_getfield(L, idx, "__wc");
-    PHWebViewController* wc = (__bridge id)lua_touserdata(L, -1);
-    lua_pop(L, 1);
-    return wc;
+static hydradoc doc_webview_hide = {
+    "webview", "hide", "webview:hide()",
+    "Makes the webview hidden."
+};
+
+static int webview_hide(lua_State* L) {
+    PHWebViewController* wc = hydra_webview(L, 1);
+    [wc close];
+    return 0;
 }
 
 static hydradoc doc_webview_settitle = {
@@ -124,11 +148,8 @@ static hydradoc doc_webview_settitle = {
 };
 
 static int webview_settitle(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
-    
-    NSString* title = [NSString stringWithUTF8String: luaL_checkstring(L, 2)];
-    [[wc window] setTitle:title];
-    
+    PHWebViewController* wc = hydra_webview(L, 1);
+    [[wc window] setTitle: [NSString stringWithUTF8String: luaL_checkstring(L, 2)]];
     return 0;
 }
 
@@ -138,9 +159,9 @@ static hydradoc doc_webview_setlevel = {
 };
 
 static int webview_setlevel(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
-    
+    PHWebViewController* wc = hydra_webview(L, 1);
     NSInteger level = luaL_checknumber(L, 2);
+    
     NSWindowCollectionBehavior coBehave = NSWindowAnimationBehaviorDefault;
     switch (level) {
         case -1: level = kCGDesktopIconWindowLevel + 1; coBehave = NSWindowCollectionBehaviorStationary; break;
@@ -159,14 +180,12 @@ static hydradoc doc_webview_sethasborder = {
 };
 
 static int webview_sethasborder(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
+    PHWebViewController* wc = hydra_webview(L, 1);
     BOOL hasborder = lua_toboolean(L, 2);
     
     NSUInteger mask = [[wc window] styleMask];
-    
     if (hasborder) mask = mask & NSBorderlessWindowMask;
     else mask = mask ^ NSBorderlessWindowMask;
-    
     [[wc window] setStyleMask:mask];
     
     return 0;
@@ -178,11 +197,8 @@ static hydradoc doc_webview_sethasshadow = {
 };
 
 static int webview_sethasshadow(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
-    BOOL hasshadow = lua_toboolean(L, 2);
-    
-    [[wc window] setHasShadow:hasshadow];
-    
+    PHWebViewController* wc = hydra_webview(L, 1);
+    [[wc window] setHasShadow:lua_toboolean(L, 2)];
     return 0;
 }
 
@@ -192,12 +208,10 @@ static hydradoc doc_webview_loadstring = {
 };
 
 static int webview_loadstring(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
+    PHWebViewController* wc = hydra_webview(L, 1);
     NSString* string = [NSString stringWithUTF8String: luaL_checkstring(L, 2)];
     NSString* basepath = [NSString stringWithUTF8String: luaL_checkstring(L, 3)];
-    
     [[[wc webview] mainFrame] loadHTMLString:string baseURL:[NSURL fileURLWithPath:basepath]];
-    
     return 0;
 }
 
@@ -207,11 +221,9 @@ static hydradoc doc_webview_loadurl = {
 };
 
 static int webview_loadurl(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
+    PHWebViewController* wc = hydra_webview(L, 1);
     NSString* url = [NSString stringWithUTF8String: luaL_checkstring(L, 2)];
-    
     [[wc webview] setMainFrameURL:url];
-    
     return 0;
 }
 
@@ -221,12 +233,10 @@ static hydradoc doc_webview_setignoresmouse = {
 };
 
 static int webview_setignoresmouse(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
+    PHWebViewController* wc = hydra_webview(L, 1);
     BOOL ignoresmouse = lua_toboolean(L, 2);
-    
     [[wc window] setIgnoresMouseEvents:ignoresmouse];
     [[wc window] setAcceptsMouseMovedEvents:!ignoresmouse];
-    
     return 0;
 }
 
@@ -236,13 +246,17 @@ static hydradoc doc_webview_id = {
 };
 
 static int webview_id(lua_State* L) {
-    PHWebViewController* wc = get_window_controller(L, 1);
+    PHWebViewController* wc = hydra_webview(L, 1);
     lua_pushnumber(L, [[wc window] windowNumber]);
     return 1;
 }
 
 static const luaL_Reg webviewlib[] = {
     {"_create", webview_create},
+    
+    // methods
+    {"show", webview_show},
+    {"hide", webview_hide},
     {"settitle", webview_settitle},
     {"setlevel", webview_setlevel},
     {"sethasborder", webview_sethasborder},
@@ -251,13 +265,18 @@ static const luaL_Reg webviewlib[] = {
     {"loadurl", webview_loadurl},
     {"setignoresmouse", webview_setignoresmouse},
     {"id", webview_id},
+    
+    // callbacks
     {"clicked", webview_clicked},
     {"hidden", webview_hidden},
+    
     {NULL, NULL}
 };
 
 int luaopen_webview(lua_State* L) {
     hydra_add_doc_group(L, "webview", "For showing stuff in web views!");
+    hydra_add_doc_item(L, &doc_webview_show);
+    hydra_add_doc_item(L, &doc_webview_hide);
     hydra_add_doc_item(L, &doc_webview_settitle);
     hydra_add_doc_item(L, &doc_webview_setlevel);
     hydra_add_doc_item(L, &doc_webview_sethasborder);
