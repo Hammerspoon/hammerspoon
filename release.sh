b446a2e4b240c2363ff604b4565e2ede3a109dcd
Steven Degutis <steven@cleancoders.com>
2014-07-07 12:37:02 -0500

    Fixed release script for new paths

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 
 # build app
 xcodebuild clean build
-VERSION=$(defaults read "$(pwd)/Hydra/Hydra-Info" CFBundleVersion)
+VERSION=$(defaults read "$(pwd)/Hydra/XcodeCrap/Hydra-Info" CFBundleVersion)
 FILENAME="Hydra-$VERSION.app.zip"
 
 # build .zip
