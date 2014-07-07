47d82ba81ab37123fc2b7249726a6a4bb8d6dfd8
Steven Degutis <steven@cleancoders.com>
2014-07-07 11:29:16 -0500

    Normalizing versions for future updates.

--- Hydra/updates.lua
+++ Hydra/updates.lua
@@ -1,3 +1,15 @@
+local function normalizeversion(str)
+  local fn = str:gmatch("(b?)(%d+)")
+  local _, major = fn()
+  local _, minor = fn()
+  local bug = 0
+  local beta = 0
+  for a, b in fn do
+    if a == 'b' then beta = b else bug = b end
+  end
+  return string.format("%02d-%02d-%02d-%02d", major, minor, bug, beta)
+end
+
 doc.updates.available = {"updates.available = function(isavailable)", "Called after updates.check() runs, with a boolean parameter specifying whether an update is available. Default implementation pushes a notification when an update is available with the tag 'showupdate'."}
 function updates.available(available)
   if available then
@@ -9,10 +21,10 @@ doc.updates.check = {"updates.check()", "Checks for an update. If one is availab
 function updates.check()
   updates.getversions(function(versions)
       local hasupdate = false
-      local thisversion = updates.currentversion()
+      local thisversion = normalizeversion(updates.currentversion())
 
       for _, version in pairs(versions) do
-        if version > thisversion then hasupdate = true end
+        if normalizeversion(version) > thisversion then hasupdate = true end
       end
 
       if hasupdate then
