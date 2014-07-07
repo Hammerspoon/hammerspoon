ca04ed0014715a7d36582b56cb22f5557f42f880
Steven Degutis <steven@cleancoders.com>
2014-07-07 16:09:41 -0500

    Changed docs method as part of #186.

--- Hydra/API/doc.lua
+++ Hydra/API/doc.lua
@@ -32,33 +32,14 @@ local function doc_tostring(doc)
   return str
 end
 
-hydra.jsondocs = {"hydra.jsondocs() -> string", "Returns the documentation as a JSON string for you to generate pretty docs with. The top-level is a list of groups. Groups have keys: name (string), doc (string), items (list of items); Items have keys: name (string), def (string), doc (string)."}
-function hydra.jsondocs()
-  local groups = {}
-
-  for groupname, group in pairs(doc) do
-    local g = {}
-    g.name = groupname
-    g.doc = group.__doc
-    g.items = {}
-
-    for itemname, item in pairs(group) do
-      if itemname ~= '__doc' and itemname ~= '__name' then
-        local i = {}
-        i.name = itemname
-        i.def = item[1]
-        i.doc = item[2]
-        table.insert(g.items, i)
-      end
-    end
-    table.insert(groups, g)
-  end
-
-  return json.encode(groups)
+--- hydra.docsfile() -> string
+--- Returns the path of a JSON file containing the docs, for you to generate pretty docs with. The top-level is a list of groups. Groups have keys: name (string), doc (string), items (list of items); Items have keys: name (string), def (string), doc (string).
+function hydra.docsfile()
+  return hydra.resourcesdir .. '/docs.json'
 end
 
 function hydra._initiate_documentation_system()
-  local docsfile = hydra.resourcesdir .. '/docs.json'
+  local docsfile = hydra.docsfile()
 
   local f = io.open(docsfile)
   local content = f:read("*a")
