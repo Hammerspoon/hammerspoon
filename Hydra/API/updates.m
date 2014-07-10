cdbbf49a9c19396ea6b5b9a663babe77bec5b071
Steven Degutis <steven@cleancoders.com>
2014-07-09 23:36:04 -0500

    Adding questionable mktmpdir function; part of #196.

--- Hydra/API/updates.m
+++ Hydra/API/updates.m
@@ -93,6 +93,19 @@ cleanup:
     return verified;
 }
 
+static NSString* make_tmp_dir(void) {
+    NSString* tmpdir = NSTemporaryDirectory();
+    if (tmpdir == nil) tmpdir = @"/tmp";
+    
+    NSString* template = [tmpdir stringByAppendingPathComponent:@"hydra.XXXXXX"];
+    NSMutableData * bufferData = [[template dataUsingEncoding:NSUTF8StringEncoding] mutableCopy];
+    char* buffer = [bufferData mutableBytes];
+    mkdtemp(buffer);
+    return [NSString stringWithUTF8String:buffer];
+}
+
+
+
 
 
 static NSString* updates_url = @"https://api.github.com/repos/sdegutis/hydra/releases";
