1e468229de1e7320a254d90971ba552b70c4f3d1
Steven Degutis <steven@cleancoders.com>
2014-07-11 09:48:14 -0500

    Fixing build for licenses (Ruby can't handle UTF-8).

--- Hydra/API/hydra.lua
+++ Hydra/API/hydra.lua
@@ -95,7 +95,7 @@ end
 hydra.licenses = [[
 ### Lua 5.2
 
-Copyright © 1994–2014 Lua.org, PUC-Rio.
+Copyright (c) 1994-2014 Lua.org, PUC-Rio.
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
