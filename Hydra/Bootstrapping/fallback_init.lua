9f8e0e3c782d671598d45e36f6d61ea36eb31ba4
Steven Degutis <steven@cleancoders.com>
2014-07-11 21:13:45 -0500

    Adding REPL to fallback init; fixes #225.

--- Hydra/Bootstrapping/fallback_init.lua
+++ Hydra/Bootstrapping/fallback_init.lua
@@ -9,6 +9,7 @@ function fallbackinit.run()
       return {
         {title = "Open Sample Init", fn = fallbackinit.open_sample_init},
         {title = "Reload Config", fn = hydra.reload},
+        {title = "Open REPL", fn = repl.open},
         {title = "-"},
         {title = "About", fn = hydra.showabout},
         {title = "Quit Hydra", fn = os.exit},
@@ -16,6 +17,8 @@ function fallbackinit.run()
   end)
 
   hydra.alert("Welcome to Hydra 1.0! Click the menu icon to find a sample config :)", 10)
+
+  hotkey.bind({"cmd", "alt", "ctrl"}, "r", repl.open)
 end
 
 return fallbackinit
