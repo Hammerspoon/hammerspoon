b9eb8de1ece522ebbe62cfe3a1670c743becd991
Steven Degutis <steven@cleancoders.com>
2014-07-12 15:51:28 -0500

    WIP: slightly better mousemoved code

--- Hydra/API/event.m
+++ Hydra/API/event.m
@@ -4,6 +4,12 @@
 ///
 /// For tapping into system events and stuff.
 
+/// event:start()
+/// Starts an event; must be in stopped state.
+
+/// event:stop()
+/// Stops an event; must be in started state.
+
 typedef struct _tapevent {
     lua_State* L;
     CFMachPortRef tap;
@@ -12,10 +18,10 @@ typedef struct _tapevent {
 } tapevent;
 
 CGEventRef mousemoved_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
-    tapevent* tapevent = refcon;
-    lua_State* L = tapevent->L;
+    tapevent* e = refcon;
+    lua_State* L = e->L;
     
-    lua_rawgeti(L, LUA_REGISTRYINDEX, tapevent->fn);
+    lua_rawgeti(L, LUA_REGISTRYINDEX, e->fn);
     hydra_pushpoint(L, [NSEvent mouseLocation]);
     
     if (lua_pcall(L, 1, 0, 0))
@@ -24,67 +30,86 @@ CGEventRef mousemoved_callback(CGEventTapProxy proxy, CGEventType type, CGEventR
     return event;
 }
 
-static int event__mousemoved_start(lua_State* L) {
-    luaL_checktype(L, 1, LUA_TTABLE);
+static int event_mousemoved_start(lua_State* L) {
+    tapevent* e = luaL_checkudata(L, 1, "mousemovedevent");
     
-    lua_getfield(L, 1, "callback");
-    int fn = luaL_ref(L, LUA_REGISTRYINDEX);
+    if (e->tap)
+        return 0;
     
-    tapevent* tapevent = malloc(sizeof(tapevent));
-    tapevent->L = L;
-    tapevent->fn = fn;
-    tapevent->tap = CGEventTapCreate(kCGHIDEventTap,
+    lua_getuservalue(L, 1);
+    lua_getfield(L, -1, "fn");
+    e->fn = luaL_ref(L, LUA_REGISTRYINDEX);
+    lua_pop(L, 1);
+    
+    e->tap = CGEventTapCreate(kCGHIDEventTap,
                                      kCGHeadInsertEventTap,
                                      kCGEventTapOptionListenOnly,
                                      CGEventMaskBit(kCGEventMouseMoved),
                                      mousemoved_callback,
-                                     tapevent);
+                                     e);
     
-    CGEventTapEnable(tapevent->tap, true);
-    tapevent->runloopsrc = CFMachPortCreateRunLoopSource(NULL, tapevent->tap, 0);
-    CFRunLoopAddSource(CFRunLoopGetMain(), tapevent->runloopsrc, kCFRunLoopCommonModes);
-    
-    lua_pushlightuserdata(L, tapevent);
-    lua_setfield(L, 1, "__tapevent");
+    CGEventTapEnable(e->tap, true);
+    e->runloopsrc = CFMachPortCreateRunLoopSource(NULL, e->tap, 0);
+    CFRunLoopAddSource(CFRunLoopGetMain(), e->runloopsrc, kCFRunLoopCommonModes);
     
     return 0;
 }
 
-static int event__mousemoved_stop(lua_State* L) {
-    luaL_checktype(L, 1, LUA_TTABLE);
+static int event_mousemoved_stop(lua_State* L) {
+    tapevent* e = luaL_checkudata(L, 1, "mousemovedevent");
     
-    lua_getfield(L, 1, "__tapevent");
-    tapevent* tapevent = lua_touserdata(L, -1);
+    if (!e->tap)
+        return 0;
     
-    CGEventTapEnable(tapevent->tap, false);
-    CFMachPortInvalidate(tapevent->tap);
-    CFRunLoopRemoveSource(CFRunLoopGetMain(), tapevent->runloopsrc, kCFRunLoopCommonModes);
-    CFRelease(tapevent->runloopsrc);
-    CFRelease(tapevent->tap);
+    CGEventTapEnable(e->tap, false);
+    CFMachPortInvalidate(e->tap);
+    CFRunLoopRemoveSource(CFRunLoopGetMain(), e->runloopsrc, kCFRunLoopCommonModes);
+    CFRelease(e->runloopsrc);
+    CFRelease(e->tap);
     
-    luaL_unref(L, LUA_REGISTRYINDEX, tapevent->fn);
+    luaL_unref(L, LUA_REGISTRYINDEX, e->fn);
+    e->tap = NULL;
     
     return 0;
 }
 
-static int event__mousemoved_gc(lua_State *L) {
-    luaL_checktype(L, 1, LUA_TTABLE);
+/// event.mousemoved(fn(point)) -> event
+/// Returns a new event with the given callback for mouse-moved events; is not started automatically.
+static int event_mousemoved(lua_State* L) {
+    luaL_checktype(L, 1, LUA_TFUNCTION);
     
-    lua_getfield(L, 1, "__tapevent");
-    tapevent* tapevent = lua_touserdata(L, -1);
-    free(tapevent);
+    tapevent* e = lua_newuserdata(L, sizeof(e));
+    e->L = L;
+    e->tap = NULL;
     
-    return 0;
+    lua_newtable(L);
+    lua_pushvalue(L, 1);
+    lua_setfield(L, -2, "fn");
+    lua_setuservalue(L, -2);
+    
+    luaL_getmetatable(L, "mousemovedevent");
+    lua_setmetatable(L, -2);
+    
+    return 1;
 }
 
 static luaL_Reg eventlib[] = {
-    {"_mousemoved_start", event__mousemoved_start},
-    {"_mousemoved_stop", event__mousemoved_stop},
-    {"_mousemoved_gc", event__mousemoved_gc},
+    {"mousemoved", event_mousemoved},
+    {NULL, NULL}
+};
+
+static luaL_Reg mousemovedlib[] = {
+    {"start", event_mousemoved_start},
+    {"stop", event_mousemoved_stop},
     {NULL, NULL}
 };
 
 int luaopen_event(lua_State* L) {
+    luaL_newmetatable(L, "mousemovedevent");
+    luaL_newlib(L, mousemovedlib);
+    lua_setfield(L, -2, "__index");
+    lua_pop(L, 1);
+    
     luaL_newlib(L, eventlib);
     return 1;
 }
--- Hydra/Bootstrapping/rawinit.lua
+++ Hydra/Bootstrapping/rawinit.lua
@@ -17,7 +17,6 @@ local ok, err = pcall(function()
     dofile(hydra.resourcesdir .. "/notify.lua")
     dofile(hydra.resourcesdir .. "/pprint.lua")
     dofile(hydra.resourcesdir .. "/ipc.lua")
-    dofile(hydra.resourcesdir .. "/event.lua")
     dofile(hydra.resourcesdir .. "/doc.lua")
 
     package.path = os.getenv("HOME") .. "/.hydra/?.lua" .. ';' .. package.path
