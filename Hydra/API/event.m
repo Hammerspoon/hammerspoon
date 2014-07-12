ae26b32c680b5fe326b2f5a1c25d550e3576a56d
Steven Degutis <steven@cleancoders.com>
2014-07-12 13:13:50 -0500

    WIP: writing some very weird code for events.

--- Hydra/API/event.m
+++ Hydra/API/event.m
@@ -1,57 +1,90 @@
 #import "helpers.h"
 
-typedef struct _mousemoved_state {
+/// event
+///
+/// For tapping into system events and stuff.
+
+typedef struct _tapevent {
     lua_State* L;
     CFMachPortRef tap;
     CFRunLoopSourceRef runloopsrc;
-} mousemoved_state;
+    int fn;
+} tapevent;
 
 CGEventRef mousemoved_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
-    mousemoved_state* state = refcon;
-    lua_State* L = state->L;
+    tapevent* tapevent = refcon;
+    lua_State* L = tapevent->L;
+    
+    lua_rawgeti(L, LUA_REGISTRYINDEX, tapevent->fn);
+    hydra_pushpoint(L, [NSEvent mouseLocation]);
+    
+    if (lua_pcall(L, 1, 0, 0))
+        hydra_handle_error(L);
+    
     return event;
 }
 
-static int event_mousemoved(lua_State* L) {
-    mousemoved_state* state = malloc(sizeof(mousemoved_state));
-    state->L = L;
-    state->tap = CGEventTapCreate(kCGHIDEventTap,
-                                  kCGHeadInsertEventTap,
-                                  kCGEventTapOptionListenOnly,
-                                  CGEventMaskBit(kCGEventMouseMoved),
-                                  mousemoved_callback,
-                                  state);
+static int event__mousemoved_start(lua_State* L) {
+    luaL_checktype(L, 1, LUA_TTABLE);
     
-    CGEventTapEnable(state->tap, true);
-    state->runloopsrc = CFMachPortCreateRunLoopSource(NULL, state->tap, 0);
-    CFRunLoopAddSource(CFRunLoopGetMain(), state->runloopsrc, kCFRunLoopCommonModes);
+    lua_getfield(L, 1, "callback");
+    int fn = luaL_ref(L, LUA_REGISTRYINDEX);
     
-    lua_pushlightuserdata(L, state);
-    return 1;
+    tapevent* tapevent = malloc(sizeof(tapevent));
+    tapevent->L = L;
+    tapevent->fn = fn;
+    tapevent->tap = CGEventTapCreate(kCGHIDEventTap,
+                                     kCGHeadInsertEventTap,
+                                     kCGEventTapOptionListenOnly,
+                                     CGEventMaskBit(kCGEventMouseMoved),
+                                     mousemoved_callback,
+                                     tapevent);
     
+    CGEventTapEnable(tapevent->tap, true);
+    tapevent->runloopsrc = CFMachPortCreateRunLoopSource(NULL, tapevent->tap, 0);
+    CFRunLoopAddSource(CFRunLoopGetMain(), tapevent->runloopsrc, kCFRunLoopCommonModes);
     
+    lua_pushlightuserdata(L, tapevent);
+    lua_setfield(L, 1, "__tapevent");
     
+    return 0;
+}
+
+static int event__mousemoved_stop(lua_State* L) {
+    luaL_checktype(L, 1, LUA_TTABLE);
     
-//    CGEventTapEnable(tap, false);
-//    CFMachPortInvalidate(tap);
-//    CFRunLoopRemoveSource(CFRunLoopGetMain(), runloopsrc, kCFRunLoopCommonModes);
-//    CFRelease(runloopsrc);
-//    CFRelease(tap);
+    lua_getfield(L, 1, "__tapevent");
+    tapevent* tapevent = lua_touserdata(L, -1);
+    
+    CGEventTapEnable(tapevent->tap, false);
+    CFMachPortInvalidate(tapevent->tap);
+    CFRunLoopRemoveSource(CFRunLoopGetMain(), tapevent->runloopsrc, kCFRunLoopCommonModes);
+    CFRelease(tapevent->runloopsrc);
+    CFRelease(tapevent->tap);
+    
+    luaL_unref(L, LUA_REGISTRYINDEX, tapevent->fn);
+    
+    return 0;
+}
+
+static int event__mousemoved_gc(lua_State *L) {
+    luaL_checktype(L, 1, LUA_TTABLE);
+    
+    lua_getfield(L, 1, "__tapevent");
+    tapevent* tapevent = lua_touserdata(L, -1);
+    free(tapevent);
     
     return 0;
 }
 
 static luaL_Reg eventlib[] = {
-    {"mousemoved", event_mousemoved},
+    {"_mousemoved_start", event__mousemoved_start},
+    {"_mousemoved_stop", event__mousemoved_stop},
+    {"_mousemoved_gc", event__mousemoved_gc},
     {NULL, NULL}
 };
 
 int luaopen_event(lua_State* L) {
     luaL_newlib(L, eventlib);
-    
-    if (luaL_newmetatable(L, "event")) {
-        // ...
-    }
-    
     return 1;
 }
