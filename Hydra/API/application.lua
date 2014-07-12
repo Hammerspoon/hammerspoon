fc9d5f57525e01250ba6ce1de37bf075ff879798
Steven Degutis <steven@cleancoders.com>
2014-07-11 22:52:08 -0500

    Skipping unresponsive apps in app:activate(); part of #226.

--- Hydra/API/application.lua
+++ Hydra/API/application.lua
@@ -13,6 +13,7 @@ end
 --- application:activate(allwindows = false) -> bool
 --- Tries to activate the app (make its key window focused) and returns whether it succeeded; if allwindows is true, all windows of the application are brought forward as well.
 function application:activate(allwindows)
+  if self:isunresponsive() then return false end
   local win = self:_focusedwindow()
   if win then
     return win:becomemain() and self:_bringtofront(allwindows)
