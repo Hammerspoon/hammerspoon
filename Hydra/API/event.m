6dd308a90910a0f48ca31e43f2ff9da0a572a9c2
Steven Degutis <steven@cleancoders.com>
2014-07-12 22:15:11 -0500

    WIP: making event taps even cooler

--- Hydra/API/event.m
+++ Hydra/API/event.m
@@ -16,7 +16,7 @@ typedef struct _eventtap {
     BOOL started;
     CFMachPortRef tap;
     CFRunLoopSourceRef runloopsrc;
-    CGEventMask mask; // TODO
+    CGEventMask mask;
     eventtap_closure fn;
     int ref;
 } eventtap;
@@ -40,13 +40,26 @@ static int event_eventtap_start(lua_State* L) {
     lua_pop(L, 1);
     
     e->fn = Block_copy(^CGEventRef(CGEventRef event){
-        lua_rawgeti(L, LUA_REGISTRYINDEX, e->ref);
-        hydra_pushpoint(L, [NSEvent mouseLocation]);
+        int stack = lua_gettop(L);
         
-        if (lua_pcall(L, 1, 0, 0))
+        lua_rawgeti(L, LUA_REGISTRYINDEX, e->ref);
+        if (lua_pcall(L, 0, LUA_MULTRET, 0))
             hydra_handle_error(L);
         
-        return event;
+        int nret = lua_gettop(L) - stack;
+        
+        if (nret == 1) {
+            if (lua_isnil(L, -1)) {
+                return NULL;
+            }
+            else {
+                // TODO: allow user to modify event somehow
+                return event;
+            }
+        }
+        else {
+            return event;
+        }
     });
     
     e->tap = CGEventTapCreate(kCGSessionEventTap,
@@ -82,17 +95,20 @@ static int event_eventtap_stop(lua_State* L) {
     return 0;
 }
 
-/// event.eventtap(fn(point)) -> event
+/// event.eventtap(type, fn(point)) -> event
 /// Returns a new event tap with the given callback for the given events; is not started automatically.
+/// The type param must be one of the values from the table `event.eventtaptypes`.
+/// If the callback function returns nothing, the event is not modified; if it returns nil, the event is deleted from the OS X event system and not seen by any other apps; all other return values are reserved for future features to this API.
 static int event_eventtap(lua_State* L) {
-    luaL_checktype(L, 1, LUA_TFUNCTION);
+    CGEventMask type = luaL_checknumber(L, 1);
+    luaL_checktype(L, 2, LUA_TFUNCTION);
     
     eventtap* e = lua_newuserdata(L, sizeof(eventtap));
     e->started = NO;
-    e->mask = CGEventMaskBit(kCGEventMouseMoved);
+    e->mask = type;
     
     lua_newtable(L);
-    lua_pushvalue(L, 1);
+    lua_pushvalue(L, 2);
     lua_setfield(L, -2, "fn");
     lua_setuservalue(L, -2);
     
@@ -113,6 +129,16 @@ static luaL_Reg eventtaplib[] = {
     {NULL, NULL}
 };
 
+/// event.eventtaptypes
+/// Table for use with `event.eventtap`, with the following keys:
+///   leftmousedown, leftmouseup, leftmousedragged
+///   rightmousedown, rightmouseup, rightmousedragged
+///   middlemousedown, middlemouseup, middlemousedragged
+///   keydown, keyup
+///   mousemoved
+///   flagschanged
+///   scrollwheel
+
 int luaopen_event(lua_State* L) {
     luaL_newmetatable(L, "eventtap");
     luaL_newlib(L, eventtaplib);
@@ -120,5 +146,23 @@ int luaopen_event(lua_State* L) {
     lua_pop(L, 1);
     
     luaL_newlib(L, eventlib);
+    
+    lua_newtable(L);
+    lua_pushnumber(L, CGEventMaskBit(kCGEventLeftMouseDown));     lua_setfield(L, -2, "leftmousedown");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventLeftMouseUp));       lua_setfield(L, -2, "leftmouseup");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventLeftMouseDragged));  lua_setfield(L, -2, "leftmousedragged");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventRightMouseDown));    lua_setfield(L, -2, "rightmousedown");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventRightMouseUp));      lua_setfield(L, -2, "rightmouseup");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventRightMouseDragged)); lua_setfield(L, -2, "rightmousedragged");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventOtherMouseDown));    lua_setfield(L, -2, "middlemousedown");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventOtherMouseUp));      lua_setfield(L, -2, "middlemouseup");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventOtherMouseDragged)); lua_setfield(L, -2, "middlemousedragged");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventMouseMoved));        lua_setfield(L, -2, "mousemoved");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventFlagsChanged));      lua_setfield(L, -2, "flagschanged");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventScrollWheel));       lua_setfield(L, -2, "scrollwheel");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventKeyDown));           lua_setfield(L, -2, "keydown");
+    lua_pushnumber(L, CGEventMaskBit(kCGEventKeyUp));             lua_setfield(L, -2, "keyup");
+    lua_setfield(L, -2, "eventtaptypes");
+    
     return 1;
 }
