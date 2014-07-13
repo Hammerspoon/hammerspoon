57474c5fc9151d010847ba1e8580a48055d56c54
Steven Degutis <steven@cleancoders.com>
2014-07-13 13:33:31 -0500

    Updated gendocs to work for multiple versions.

--- Hydra/docgen/gendocs.rb
+++ Hydra/docgen/gendocs.rb
@@ -3,17 +3,19 @@ require 'github/markdown'
 require 'json'
 require 'erb'
 
+version = `defaults read "$(pwd)/../XcodeCrap/Hydra-Info" CFBundleVersion`.strip
+
 template = ERB.new(File.read("template.erb"))
-system("mkdir -p docs && rm -f docs/api*html")
+system("mkdir -p docs/#{version} && rm -f docs/#{version}/api*html")
 
 groups = JSON.load(File.read("docs.json"))
 groups.each do |group|
-  File.write("docs/#{group['name']}.html", template.result(binding))
+  File.write("docs/#{version}/#{group['name']}.html", template.result(binding))
 end
 
 group = {}
-group['name'] = "Hydra API"
+group['name'] = "Hydra #{version} API"
 group['doc'] = File.read("index.md")
 group['items'] = []
 
-File.write("docs/index.html", template.result(binding))
+File.write("docs/#{version}/index.html", template.result(binding))
