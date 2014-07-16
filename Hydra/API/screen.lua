41e6168f87dd8c4c84fd20c2866b96f6c3d1df31
Steven Degutis <steven@cleancoders.com>
2014-07-16 09:15:04 -0500

    Added screens in direction functions; closes #182.

--- Hydra/API/screen.lua
+++ Hydra/API/screen.lua
@@ -33,3 +33,63 @@ function screen:previous()
   if i < 1 then i = # screens end
   return screens[i]
 end
+
+local function first_screen_in_direction(screen, numrotations)
+  if #screen.allscreens() == 1 then
+    return nil
+  end
+
+  -- assume looking to east
+
+  -- use the score distance/cos(A/2), where A is the angle by which it
+  -- differs from the straight line in the direction you're looking
+  -- for. (may have to manually prevent division by zero.)
+
+  -- thanks mark!
+
+  local otherscreens = fnutils.filter(screen.allscreens(), function(s) return s ~= screen end)
+  local startingpoint = geometry.rectmidpoint(screen:frame_including_dock_and_menu())
+  local closestscreens = {}
+
+  for _, s in pairs(otherscreens) do
+    local otherpoint = geometry.rectmidpoint(s:frame_including_dock_and_menu())
+    otherpoint = geometry.rotateccw(otherpoint, startingpoint, numrotations)
+
+    local delta = {
+      x = otherpoint.x - startingpoint.x,
+      y = otherpoint.y - startingpoint.y,
+    }
+
+    if delta.x > 0 then
+      local angle = math.atan2(delta.y, delta.x)
+      local distance = geometry.hypot(delta)
+      local anglediff = -angle
+      local score = distance / math.cos(anglediff / 2)
+      table.insert(closestscreens, {s = s, score = score})
+    end
+  end
+
+  table.sort(closestscreens, function(a, b) return a.score < b.score end)
+
+  if #closestscreens > 0 then
+    return closestscreens[1]
+  else
+    return nil
+  end
+end
+
+--- screen:toeast()
+--- Get the first screen to the east of this one, ordered by proximity.
+function screen:toeast()  return first_screen_in_direction(self, 0) end
+
+--- screen:towest()
+--- Get the first screen to the west of this one, ordered by proximity.
+function screen:towest()  return first_screen_in_direction(self, 2) end
+
+--- screen:tonorth()
+--- Get the first screen to the north of this one, ordered by proximity.
+function screen:tonorth() return first_screen_in_direction(self, 1) end
+
+--- screen:tosouth()
+--- Get the first screen to the south of this one, ordered by proximity.
+function screen:tosouth() return first_screen_in_direction(self, 3) end
