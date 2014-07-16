9c64da40cd781da9bdb09b4342ced12645e5db35
Steven Degutis <steven@cleancoders.com>
2014-07-16 08:40:52 -0500

    Fixed docs for hotkey:bind(...)

--- Hydra/API/hotkey.lua
+++ Hydra/API/hotkey.lua
@@ -1,7 +1,7 @@
 hotkey.keycodes = hotkey._cachekeycodes()
 
---- hotkey.bind(mods, key, fn) -> hotkey
---- Shortcut for: return hotkey.new(mods, key, fn):enable()
+--- hotkey.bind(mods, key, pressedfn, releasedfn) -> hotkey
+--- Shortcut for: return hotkey.new(mods, key, pressedfn, releasedfn):enable()
 function hotkey.bind(...)
   return hotkey.new(...):enable()
 end
