b99fee2298b8f97f29ac459edbc3e868e872d39c
Steven Degutis <steven@cleancoders.com>
2014-07-16 21:25:02 -0500

    Deleting redundant tostring() call.

--- Hydra/API/ipc.lua
+++ Hydra/API/ipc.lua
@@ -18,7 +18,7 @@ function ipc._handler(raw, str)
   local fn = ipc.handler
   if raw then fn = rawhandler end
   local ok, val = hydra.call(function() return fn(str) end)
-  return tostring(val)
+  return val
 end
 
 local function envstuff(prefix, dryrun)
