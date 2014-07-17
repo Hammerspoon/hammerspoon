cf8ecdbfb769e86667f299353cd3bd3066cc8c5b
Steven Degutis <steven@cleancoders.com>
2014-07-16 21:14:00 -0500

    Fixes potential free-after-use in ipc module; closes #251.

--- Hydra/API/ipc.m
+++ Hydra/API/ipc.m
@@ -1,14 +1,27 @@
 #import "helpers.h"
 
-static const char* (^ipc_closure)(const char* str);
-
 CFDataRef ipc_callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, void *info) {
+    lua_State* L = info;
+    
     CFStringRef instr = CFStringCreateFromExternalRepresentation(NULL, data, kCFStringEncodingUTF8);
-    const char* cinstr = CFStringGetCStringPtr(instr, kCFStringEncodingUTF8);
-    const char* coutstr = ipc_closure(cinstr);
+    const char* cmd = CFStringGetCStringPtr(instr, kCFStringEncodingUTF8);
+    
+    BOOL israw = (cmd[0] == 'r');
+    const char* commandstr = cmd+1;
+    
+    // result = ipc._handler(israw, cmdstring)
+    lua_getglobal(L, "ipc");
+    lua_getfield(L, -1, "_handler");
+    lua_pushboolean(L, israw);
+    lua_pushstring(L, commandstr);
+    lua_pcall(L, 2, 1, 0);
+    const char* coutstr = luaL_tolstring(L, -1, NULL);
+    CFStringRef outstr = CFStringCreateWithCString(NULL, coutstr, kCFStringEncodingUTF8);
+    lua_pop(L, 1); // ipc
+    
+    // this stays down here so commandstr can stay alive through the call
     CFRelease(instr);
     
-    CFStringRef outstr = CFStringCreateWithCString(NULL, coutstr, kCFStringEncodingUTF8);
     CFDataRef outdata = CFStringCreateExternalRepresentation(NULL, outstr, kCFStringEncodingUTF8, 0);
     CFRelease(outstr);
     
@@ -18,26 +31,9 @@ CFDataRef ipc_callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, voi
 // assumes the ipc table is pushed
 // leaves lua stack intact
 static void setup_ipc(lua_State* L) {
-    lua_pushvalue(L, -1);
-    int ipcmod = luaL_ref(L, LUA_REGISTRYINDEX);
-    ipc_closure = [^const char*(const char* cmd) {
-        lua_rawgeti(L, LUA_REGISTRYINDEX, ipcmod);
-        lua_getfield(L, -1, "_handler");
-        lua_pushboolean(L, cmd[0] == 'r');
-        lua_pushstring(L, cmd+1);
-        const char* result = "";
-        if (lua_pcall(L, 2, 1, 0)) {
-            hydra_handle_error(L);
-            lua_pop(L, 1); // ipcmod
-        }
-        else {
-            result = lua_tostring(L, -1);
-            lua_pop(L, 2); // return value and ipcmod
-        }
-        return result;
-    } copy];
-    
-    CFMessagePortRef messagePort = CFMessagePortCreateLocal(NULL, CFSTR("hydra"), ipc_callback, NULL, false);
+    CFMessagePortContext ctx = {0};
+    ctx.info = L;
+    CFMessagePortRef messagePort = CFMessagePortCreateLocal(NULL, CFSTR("hydra"), ipc_callback, &ctx, false);
     CFRunLoopSourceRef runloopSource = CFMessagePortCreateRunLoopSource(NULL, messagePort, 0);
     CFRunLoopAddSource(CFRunLoopGetMain(), runloopSource, kCFRunLoopCommonModes);
 }
