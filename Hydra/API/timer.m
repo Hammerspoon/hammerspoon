790855782c56af291bb31be0f6de664e2fc3a368
Steven Degutis <steven@cleancoders.com>
2014-07-19 11:53:05 -0500

    Changed the timer module's internals; part of #241.

--- Hydra/API/timer.m
+++ Hydra/API/timer.m
@@ -4,18 +4,21 @@
 ///
 /// Execute functions with various timing rules.
 
-@interface PHTimerDelegator : NSObject
-@property (copy) dispatch_block_t fired;
-@property NSTimer* timer;
-@property int closureRef;
-@end
+typedef struct _timer_t {
+    lua_State* L;
+    CFRunLoopTimerRef t;
+    int fn;
+    int self;
+    BOOL started;
+} timer_t;
 
-@implementation PHTimerDelegator
-- (void) fired:(NSTimer*)timer {
-    self.fired();
+void callback(CFRunLoopTimerRef timer, void *info) {
+    timer_t* t = info;
+    lua_State* L = t->L;
+    lua_rawgeti(L, LUA_REGISTRYINDEX, t->fn);
+    if (lua_pcall(L, 0, 0, 0))
+        hydra_handle_error(L);
 }
-@end
-
 
 /// timer.doafter(sec, fn())
 /// Runs the function after sec seconds.
@@ -34,50 +37,100 @@ static int timer_doafter(lua_State* L) {
     return 0;
 }
 
-// args: [fn, sec]
-// returns: [rawtimer]
-static int timer_start(lua_State* L) {
-    luaL_checktype(L, 1, LUA_TFUNCTION);
-    NSTimeInterval sec = luaL_checknumber(L, 2);
-    lua_settop(L, 2); // just to be safe
-    int closureref = luaL_ref(L, LUA_REGISTRYINDEX);
+/// timer.new(interval, fn) -> timer
+/// Creates a new timer that can be started; interval is specified in seconds as a decimal number.
+static int timer_new(lua_State* L) {
+    NSTimeInterval sec = luaL_checknumber(L, 1);
+    luaL_checktype(L, 2, LUA_TFUNCTION);
     
-    PHTimerDelegator* delegator = [[PHTimerDelegator alloc] init];
-    delegator.closureRef = closureref;
-    delegator.fired = ^{
-        lua_rawgeti(L, LUA_REGISTRYINDEX, closureref);
-        if (lua_pcall(L, 0, 0, 0))
-            hydra_handle_error(L);
-    };
+    timer_t* timer = lua_newuserdata(L, sizeof(timer_t));
+    memset(timer, 0, sizeof(timer_t));
+    timer->L = L;
     
-    delegator.timer = [NSTimer scheduledTimerWithTimeInterval:sec target:delegator selector:@selector(fired:) userInfo:nil repeats:YES];
+    lua_pushvalue(L, 2);
+    timer->fn = luaL_ref(L, LUA_REGISTRYINDEX);
     
-    lua_pushlightuserdata(L, (__bridge_retained void*)delegator);
+    luaL_getmetatable(L, "timer");
+    lua_setmetatable(L, -2);
+    
+    CFRunLoopTimerContext ctx = {0};
+    ctx.info = timer;
+    timer->t = CFRunLoopTimerCreate(NULL, 0, sec, 0, 0, callback, &ctx);
+    
     return 1;
 }
 
-// args: [rawtimer]
-// returns: []
+/// timer:start() -> self
+/// Begins to execute timer.fn every timer.seconds; calling this does not cause an initial firing of the timer immediately.
+static int timer_start(lua_State* L) {
+    timer_t* timer = luaL_checkudata(L, 1, "timer");
+    lua_settop(L, 1);
+    
+    if (timer->started) return 1;
+    timer->started = YES;
+    
+    timer->self = hydra_store_handler(L, 1);
+    CFRunLoopTimerSetNextFireDate(timer->t, CFAbsoluteTimeGetCurrent() + CFRunLoopTimerGetInterval(timer->t));
+    CFRunLoopAddTimer(CFRunLoopGetMain(), timer->t, kCFRunLoopCommonModes);
+    return 1;
+}
+
+/// timer:stop() -> self
+/// Stops the timer's fn from getting called until started again.
 static int timer_stop(lua_State* L) {
-    luaL_checktype(L, 1, LUA_TLIGHTUSERDATA);
-    PHTimerDelegator* delegator = (__bridge_transfer PHTimerDelegator*)lua_touserdata(L, 1);
+    timer_t* timer = luaL_checkudata(L, 1, "timer");
+    lua_settop(L, 1);
     
-    [delegator.timer invalidate];
-    delegator.timer = nil;
-    luaL_unref(L, LUA_REGISTRYINDEX, delegator.closureRef);
-    delegator = nil;
+    if (!timer->started) return 1;
+    timer->started = NO;
     
+    hydra_remove_handler(L, timer->self);
+    CFRunLoopRemoveTimer(CFRunLoopGetMain(), timer->t, kCFRunLoopCommonModes);
+    return 1;
+}
+
+/// timer.stopall()
+/// Stops all running timers; called automatically when user config reloads.
+static int timer_stopall(lua_State* L) {
+    lua_getglobal(L, "timer");
+    lua_getfield(L, -1, "stop");
+    hydra_remove_all_handlers(L, "timer");
+    return 0;
+}
+
+static int timer_gc(lua_State* L) {
+    NSLog(@"bye");
+    timer_t* timer = luaL_checkudata(L, 1, "timer");
+    luaL_unref(L, LUA_REGISTRYINDEX, timer->fn);
+    CFRunLoopTimerInvalidate(timer->t);
+    CFRelease(timer->t);
     return 0;
 }
 
 static const luaL_Reg timerlib[] = {
+    // class methods
     {"doafter", timer_doafter},
-    {"_start", timer_start},
-    {"_stop", timer_stop},
+    {"stopall", timer_stopall},
+    {"new", timer_new},
+    
+    // instance methods
+    {"start", timer_start},
+    {"stop", timer_stop},
+    
+    // metamethods
+    {"__gc", timer_gc},
+    
     {NULL, NULL}
 };
 
 int luaopen_timer(lua_State* L) {
     luaL_newlib(L, timerlib);
+    
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    
+    lua_pushvalue(L, -1);
+    lua_setfield(L, LUA_REGISTRYINDEX, "timer");
+    
     return 1;
 }
