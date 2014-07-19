5e60662832600e4e0aca942762cb34abfb32c23d
Jake <me@jh.gg>
2014-07-19 17:49:31 -0400

    Fix potential crash when CFStringGetCStringPtr returns NULL.

--- Hydra/API/hydra_ipc.m
+++ Hydra/API/hydra_ipc.m
@@ -4,7 +4,20 @@ CFDataRef ipc_callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, voi
     lua_State* L = info;
     
     CFStringRef instr = CFStringCreateFromExternalRepresentation(NULL, data, kCFStringEncodingUTF8);
+    
     const char* cmd = CFStringGetCStringPtr(instr, kCFStringEncodingUTF8);
+    bool shouldFree = NO;
+    
+    if (cmd == NULL) {
+        CFIndex inputLength = CFStringGetLength(instr);
+        CFIndex maxSize = CFStringGetMaximumSizeForEncoding(inputLength, kCFStringEncodingUTF8);
+        
+        cmd = malloc(maxSize + 1);
+        // We will cast down from const here, since we jsut allocated cmd, and are sure it's safe at this
+        // point to touch it's contents.
+        CFStringGetCString(instr, (char*)cmd, maxSize, kCFStringEncodingUTF8);
+        shouldFree = YES;
+    }
     
     BOOL israw = (cmd[0] == 'r');
     const char* commandstr = cmd+1;
@@ -22,6 +35,7 @@ CFDataRef ipc_callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, voi
     
     // this stays down here so commandstr can stay alive through the call
     CFRelease(instr);
+    if (shouldFree) free((char*) cmd);
     
     CFDataRef outdata = CFStringCreateExternalRepresentation(NULL, outstr, kCFStringEncodingUTF8, 0);
     CFRelease(outstr);
