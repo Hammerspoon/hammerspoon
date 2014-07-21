6a6e602b7ab72ca849cbe61f217615adb860ae47
Steven Degutis <steven@cleancoders.com>
2014-07-21 12:37:21 -0500

    Adding interactive mode to hydra-cli; closes #273.

--- hydra-cli/hydra.c
+++ hydra-cli/hydra.c
@@ -1,66 +1,17 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <getopt.h>
+#include <editline/readline.h>
 
-int main(int argc, char * argv[]) {
-    bool raw = false;
-    bool readstdin;
-    
-    int ch;
-    static struct option longopts[] = {
-        { "raw",   no_argument,  NULL,   'r' },
-        { "help",  no_argument,  NULL,   'h' },
-        { NULL,    0,            NULL,   0 },
-    };
-    
-    while ((ch = getopt_long(argc, argv, "rh", longopts, NULL)) != -1) {
-        switch (ch) {
-            case 'r':
-                raw = true;
-                break;
-            case 'h':
-            default:
-                printf("usage: %s [-r] <code>\n", argv[0]);
-                exit(0);
-        }
-    }
-    argc -= optind;
-    argv += optind;
-    
-    readstdin = (argc == 0);
-    
-    CFStringRef initial = raw ? CFSTR("r") : CFSTR("x");
-    CFMutableStringRef inputstr = CFStringCreateMutableCopy(NULL, 0, initial);
-    
-    if (readstdin) {
-        char buffer[1024];
-        while (fgets(buffer, 1024, stdin)) {
-            CFStringRef tmp = CFStringCreateWithCString(NULL, buffer, kCFStringEncodingUTF8);
-            CFStringAppend(inputstr, tmp);
-        }
-        
-        if (ferror(stdin)) {
-            perror("error reading from stdin.");
-            exit(3);
-        }
-    }
-    else {
-        for (int i = 0; i < argc; i++) {
-            if (i > 1) CFStringAppendCString(inputstr, " ", kCFStringEncodingUTF8);
-            CFStringAppendCString(inputstr, argv[i], kCFStringEncodingUTF8);
-        }
-    }
-    
-    CFMessagePortRef port = CFMessagePortCreateRemote(NULL, CFSTR("hydra"));
-    
-    if (!port) {
-        fprintf(stderr, "error: can't access Hydra; is it running?\n");
-        return 1;
-    }
-    
-    CFDataRef data = CFStringCreateExternalRepresentation(NULL, inputstr, kCFStringEncodingUTF8, 0);
+static void hydra_setprefix(CFMutableStringRef inputstr, bool israw) {
+    CFStringAppendCString(inputstr, israw ? "r" : "x", kCFStringEncodingUTF8);
+}
+
+static void hydra_send(CFMessagePortRef port, CFMutableStringRef inputstr) {
+    CFDataRef inputData = CFStringCreateExternalRepresentation(NULL, inputstr, kCFStringEncodingUTF8, 0);
     
     CFDataRef returnedData;
-    SInt32 code = CFMessagePortSendRequest(port, 0, data, 2, 4, kCFRunLoopDefaultMode, &returnedData);
+    SInt32 code = CFMessagePortSendRequest(port, 0, inputData, 2, 4, kCFRunLoopDefaultMode, &returnedData);
+    CFRelease(inputData);
     
     if (code != kCFMessagePortSuccess) {
         const char* errstr = "unknown error";
@@ -73,20 +24,89 @@ int main(int argc, char * argv[]) {
         }
         
         fprintf(stderr, "error: %s\n", errstr);
-        return 2;
+        exit(2);
     }
     
     CFStringRef responseString = CFStringCreateFromExternalRepresentation(NULL, returnedData, kCFStringEncodingUTF8);
+    CFRelease(returnedData);
     
+    CFIndex maxSize = CFStringGetMaximumSizeForEncoding(CFStringGetLength(responseString), kCFStringEncodingUTF8);
     const char* responseCStringPtr = CFStringGetCStringPtr(responseString, kCFStringEncodingUTF8);
+    char responseCString[maxSize];
     
-    if (responseCStringPtr == NULL) {
-        CFIndex responseLength = CFStringGetLength(responseString);
-        CFIndex maxSize = CFStringGetMaximumSizeForEncoding(responseLength, kCFStringEncodingUTF8);
-        responseCStringPtr = malloc(maxSize + 1);
-        CFStringGetCString(responseString, (char *) responseCStringPtr, maxSize, kCFStringEncodingUTF8);
+    if (!responseCStringPtr)
+        CFStringGetCString(responseString, (char *) responseCString, maxSize, kCFStringEncodingUTF8);
+    
+    printf("%s\n", responseCStringPtr ? responseCStringPtr : responseCString);
+    
+    CFRelease(responseString);
+}
+
+int main(int argc, char * argv[]) {
+    bool israw = false;
+    bool readstdin = false;
+    char* code = NULL;
+    
+    int ch;
+    while ((ch = getopt(argc, argv, "irc:sh")) != -1) {
+        switch (ch) {
+            case 'i': break;
+            case 'r': israw = true; break;
+            case 'c': code = optarg; break;
+            case 's': readstdin = true; break;
+            case 'h': case '?': default:
+                printf("usage: %s [-i | -s | -c code] [-r]\n", argv[0]);
+                exit(0);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+    
+    CFMessagePortRef port = CFMessagePortCreateRemote(NULL, CFSTR("hydra"));
+    
+    if (!port) {
+        fprintf(stderr, "error: can't access Hydra; is it running?\n");
+        return 1;
     }
     
-    printf("%s\n", responseCStringPtr);
+    CFMutableStringRef str = CFStringCreateMutable(NULL, 0);
+    
+    if (readstdin) {
+        hydra_setprefix(str, israw);
+        
+        char buffer[BUFSIZ];
+        while (fgets(buffer, BUFSIZ, stdin))
+            CFStringAppendCString(str, buffer, kCFStringEncodingUTF8);
+        
+        if (ferror(stdin)) {
+            perror("error reading from stdin.");
+            exit(3);
+        }
+        
+        hydra_send(port, str);
+    }
+    else if (code) {
+        hydra_setprefix(str, israw);
+        CFStringAppendCString(str, code, kCFStringEncodingUTF8);
+        hydra_send(port, str);
+    }
+    else {
+        puts("Hydra interactive prompt.");
+        
+        while (1) {
+            char* input = readline("> ");
+            if (!input)
+                return 0;
+            add_history(input);
+            
+            hydra_setprefix(str, israw);
+            CFStringAppendCString(str, input, kCFStringEncodingUTF8);
+            hydra_send(port, str);
+            CFStringDelete(str, CFRangeMake(0, CFStringGetLength(str)));
+            
+            free(input);
+        }
+    }
+    
     return 0;
 }
