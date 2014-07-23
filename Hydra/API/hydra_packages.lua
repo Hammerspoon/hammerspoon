8a6cd65d6c7c2f190ece29c7e17fc6e294f25701
Steven Degutis <steven@cleancoders.com>
2014-07-23 16:52:15 -0500

    WIP code for hydra.packages; part of #192, kind of.

--- Hydra/API/hydra_packages.lua
+++ Hydra/API/hydra_packages.lua
@@ -8,27 +8,69 @@
 
 hydra.packages = {}
 
+--- hydra.packages.cachedirectory -> string
+--- Absolute path of packages cache, unquoted; defaults to ~/.hydra-ext
+hydra.packages.cachedirectory = os.getenv('HOME')..'/.hydra-ext'
+
 local function readpackages()
-  local f = io.open(os.getenv('HOME')..'/.hydra-ext/packages.json')
+  local f = io.open(hydra.packages.cachedirectory..'/packages.json')
   local contents = f:read("*a")
   f:close()
   return json.decode(contents)
 end
 
 --- hydra.packages.setup()
---- Clones https://github.com/sdegutis/hydra-ext into ~/.hydra-ext if it's not already there.
+--- Clones https://github.com/sdegutis/hydra-ext into hydra.packages.cachedirectory if it's not already there.
 function hydra.packages.setup()
-  os.execute('git clone https://github.com/sdegutis/hydra-ext.git ~/.hydra-ext')
+  os.execute('git clone https://github.com/sdegutis/hydra-ext.git "' .. hydra.packages.cachedirectory .. '"')
 end
 
 function hydra.packages.update()
-  os.execute('cd ~/.hydra-ext && git pull')
+  os.execute('cd "' .. hydra.packages.cachedirectory .. '" && git pull')
 end
 
 --- hydra.packages.list()
 --- Lists available and installed packages.
 function hydra.packages.list()
-  return readpackages()
+  local t = readpackages()
+  for k, pkg in ipairs(t) do
+    print(pkg.name, pkg.version, pkg.desc)
+  end
+end
+
+local function findpackage(name, version)
+  local matches = fnutils.filter(readpackages(), function(pkg) return pkg.name == name end)
+  table.sort(matches, function(a, b) return a.version < b.version end)
+
+  if version then
+    matches = fnutils.filter(matches, function(pkg) return pkg.version == version end)
+  end
+
+  if #matches == 0 then
+    return nil
+  elseif #matches > 1 then
+    print "oops. somehow we have more than one matching package for this name and version. please report this as a bug."
+  end
+
+  return matches[#matches]
+end
+
+--- hydra.packages.list(name[, version])
+--- Shows information about a given package.
+function hydra.packages.info(name)
+  local pkg = findpackage(name, version)
+  if not pkg then
+    print "No matching packages found"
+    return
+  end
+
+  print(pkg.name)
+  print(pkg.version)
+  print(pkg.desc)
+  print(pkg.license)
+  print(pkg.author)
+  print(pkg.homepage)
+  print(pkg.docspage)
 end
 
 --- hydra.packages.listinstalled()
@@ -43,19 +85,11 @@ end
 --- Changes take effect immediately, so that you may use `require "packagename"` without restarting Hydra.
 --- Multiple versions cannot be installed simultaneously; if another version of the same package is installed, this implies uninstalling it.
 function hydra.packages.install(name, version)
-  local matches = fnutils.filter(readpackages(), function(pkg) return pkg.name == name end)
-  table.sort(matches, function(a, b) return a.version < b.version end)
-
-  if version then
-    matches = fnutils.filter(matches, function(pkg) return pkg.version == version end)
-  end
-
-  if #matches == 0 then
+  local match = findpackage(name, version)
+  if not match then
     print "No matching packages found"
     return
   end
-
-  local match = matches[#matches]
 
   print(string.format("Installing:", inspect(match)))
 
