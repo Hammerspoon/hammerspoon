644acedfd61d7ee456f2ced6e2cb247009d9cb65
Steven Degutis <steven@cleancoders.com>
2014-07-24 01:54:30 -0500

    Starting on license requestion window.

--- Hydra/ThirdParty/HydraLicenseRequester.m
+++ Hydra/ThirdParty/HydraLicenseRequester.m
@@ -1,6 +1,9 @@
 #import "HydraLicenseRequester.h"
 
 @interface HydraLicenseRequester ()
+@property NSString* email;
+@property NSString* license;
+@property NSString* error;
 @end
 
 @implementation HydraLicenseRequester
@@ -16,7 +19,34 @@
 }
 
 - (void) request {
+    return; // leave commented during commits until its done.
     
+    if (![[self window] isVisible]) {
+        [[self window] center];
+    }
+    [[self window] orderFrontRegardless];
+    
+    
+    
+}
+
+- (IBAction) validate:(id)sender {
+    BOOL valid = [self.delegate tryingLicense:self.license forEmail:self.email];
+    if (valid) {
+        NSLog(@"woot");
+    }
+    else {
+        self.error = @"nope try again";
+    }
+}
+
+- (BOOL) enteredBothFields {
+    self.error = nil;
+    return [self.email length] > 0 && [self.license length] > 0;
+}
+
++ (NSSet*) keyPathsForValuesAffectingEnteredBothFields {
+    return [NSSet setWithArray:@[@"email", @"license"]];
 }
 
 @end
