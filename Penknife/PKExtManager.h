0ad1082a5f4b6566826721eaa5ab559153074ecf
Steven Degutis <steven@cleancoders.com>
2014-08-01 16:22:43 -0500

    Adding global ext manager class.

---
+++ Penknife/PKExtManager.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface PKExtManager : NSObject
+
++ (PKExtManager*) sharedExtManager;
+
+@end
---
+++ Penknife/PKExtManager.m
@@ -0,0 +1,14 @@
+#import "PKExtManager.h"
+
+@implementation PKExtManager
+
++ (PKExtManager*) sharedExtManager {
+    static PKExtManager* sharedExtManager;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedExtManager = [[PKExtManager alloc] init];
+    });
+    return sharedExtManager;
+}
+
+@end
