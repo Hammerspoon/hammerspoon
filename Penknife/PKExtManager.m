a1f8a425d50558830ae492f32809a989b6df8b6f
Steven Degutis <steven@cleancoders.com>
2014-08-01 16:37:32 -0500

    More boilerplate code.

--- Penknife/PKExtManager.m
+++ Penknife/PKExtManager.m
@@ -11,8 +11,45 @@
     return sharedExtManager;
 }
 
-- (void) setup {
+- (void) setupPaths {
+    NSError* __autoreleasing error;
+    BOOL success =
+    [[NSFileManager defaultManager] createDirectoryAtPath:[@"~/.penknife/.extcache/" stringByStandardizingPath]
+                              withIntermediateDirectories:YES
+                                               attributes:nil
+                                                    error:&error];
+    if (success) {
+        NSLog(@"could not create .extcache :'(");
+    }
+}
+
+- (void) createFilesIfNeeded {
     
+}
+
+- (void) loadFiles {
+}
+
+- (void) saveFiles {
+    
+}
+
+- (void) appIsQuitting:(NSNotification*)note {
+    [self saveFiles];
+}
+
+- (void) saveFilesOnQuit {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appIsQuitting:)
+                                                 name:NSApplicationWillTerminateNotification
+                                               object:nil];
+}
+
+- (void) setup {
+    [self setupPaths];
+    [self createFilesIfNeeded];
+    [self loadFiles];
+    [self saveFilesOnQuit];
 }
 
 - (NSArray*) availableExts {
