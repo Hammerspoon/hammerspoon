86e200d7e4c244de1a171412145cfe3bf1fb42b4
Steven Degutis <steven@cleancoders.com>
2014-08-02 12:05:54 -0500

    Accessibility timeout.

--- Penknife/core.m
+++ Penknife/core.m
@@ -12,22 +12,31 @@ static int core_exit(lua_State* L) {
     return 0; // lol
 }
 
-//static AXUIElementRef hydra_system_wide_element() {
-//    static AXUIElementRef element;
-//    static dispatch_once_t onceToken;
-//    dispatch_once(&onceToken, ^{
-//        element = AXUIElementCreateSystemWide();
-//    });
-//    return element;
-//}
+static AXUIElementRef hydra_system_wide_element() {
+    static AXUIElementRef element;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        element = AXUIElementCreateSystemWide();
+    });
+    return element;
+}
+
+/// core.setaccessibilitytimeout(sec)
+/// Change the timeout of accessibility operations; may reduce sluggishness.
+/// NOTE: this may be a dumb idea and might be removed before 1.0 is released.
+static int core_setaccessibilitytimeout(lua_State* L) {
+    float sec = luaL_checknumber(L, 1);
+    AXUIElementSetMessagingTimeout(hydra_system_wide_element(), sec);
+    return 0;
+}
 
 static luaL_Reg corelib[] = {
     {"exit", core_exit},
+    {"setaccessibilitytimeout", core_setaccessibilitytimeout},
     {}
 };
 
 //    hydra_setup_handler_storage(L); // TODO: turn into core.addhandler(), and set it up in setup.lua etc...
-//    AXUIElementSetMessagingTimeout(hydra_system_wide_element(), 1.0); // TODO: turn into core.setaccessibilitytimeout() and call with 1.0 in rawinit.lua
 
 int luaopen_core(lua_State* L) {
     luaL_newlib(L, corelib);
