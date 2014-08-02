f72117650d00adf5bbd7f6f69ef94b9992a89c0e
Steven Degutis <steven@cleancoders.com>
2014-08-02 09:55:07 -0500

    Filling out template for core module.

--- Penknife/core.m
+++ Penknife/core.m
@@ -1,5 +1,13 @@
 #import "lua/lauxlib.h"
 
+static int core_exit(lua_State* L) {
+    if (lua_toboolean(L, 2))
+        lua_close(L);
+    
+    [[NSApplication sharedApplication] terminate: nil];
+    return 0; // lol
+}
+
 static AXUIElementRef hydra_system_wide_element() {
     static AXUIElementRef element;
     static dispatch_once_t onceToken;
@@ -9,11 +17,15 @@ static AXUIElementRef hydra_system_wide_element() {
     return element;
 }
 
+static luaL_Reg corelib[] = {
+    {"exit", core_exit},
+    {}
+};
+
 //    hydra_setup_handler_storage(L); // TODO: turn into core.addhandler(), and set it up in setup.lua etc...
+//    AXUIElementSetMessagingTimeout(hydra_system_wide_element(), 1.0); // TODO: turn into core.setaccessibilitytimeout() and call with 1.0 in rawinit.lua
 
 int luaopen_core(lua_State* L) {
-    AXUIElementSetMessagingTimeout(hydra_system_wide_element(), 1.0); // TODO: turn into core.setaccessibilitytimeout() and call with 1.0 in rawinit.lua
-    
-    lua_newtable(L);
+    luaL_newlib(L, corelib);
     return 1;
 }
--- Penknife/setup.lua
+++ Penknife/setup.lua
@@ -1,1 +1,4 @@
+print(os.exit)
+os.exit = core.exit
 print "hi! lol"
+print(os.exit)
