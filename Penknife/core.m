611b32cd396a8b5b944029b15e702af25505ba58
Steven Degutis <steven@cleancoders.com>
2014-08-02 09:51:54 -0500

    Jotting down some ideas.

--- Penknife/core.m
+++ Penknife/core.m
@@ -9,6 +9,8 @@ static AXUIElementRef hydra_system_wide_element() {
     return element;
 }
 
+//    hydra_setup_handler_storage(L); // TODO: turn into core.addhandler(), and set it up in setup.lua etc...
+
 int luaopen_core(lua_State* L) {
     AXUIElementSetMessagingTimeout(hydra_system_wide_element(), 1.0); // TODO: turn into core.setaccessibilitytimeout() and call with 1.0 in rawinit.lua
     
--- Penknife/main.m
+++ Penknife/main.m
@@ -3,6 +3,7 @@
 #import "PKExtensionManager.h"
 #import "lua/lauxlib.h"
 #import "lua/lualib.h"
+int luaopen_core(lua_State* L);
 
 int main(int argc, const char * argv[]) {
     return NSApplicationMain(argc, argv);
@@ -23,15 +24,10 @@ lua_State* PKLuaState;
     lua_State* L = PKLuaState = luaL_newstate();
     luaL_openlibs(L);
     
-//    hydra_setup_handler_storage(L); // TODO: turn into core.addhandler(), etc...
-    
-    int luaopen_core(lua_State* L);
     luaopen_core(L);
-    lua_pushvalue(L, -1);
     lua_setglobal(L, "core");
     
-    NSString* initFile = [[NSBundle mainBundle] pathForResource:@"rawinit" ofType:@"lua"];
-    luaL_dofile(L, [initFile fileSystemRepresentation]);
+    luaL_dofile(L, [[[NSBundle mainBundle] pathForResource:@"setup" ofType:@"lua"] fileSystemRepresentation]);
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
