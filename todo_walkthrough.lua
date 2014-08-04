09f035ba43710d9d534bd8c4b9912efbebb803e6
Steven Degutis <steven@cleancoders.com>
2014-08-04 16:55:45 -0500

    Update todo_walkthrough.lua

--- todo_walkthrough.lua
+++ todo_walkthrough.lua
@@ -1,30 +1,8 @@
--- Hi!
--- Save this as ~/.hydra/init.lua and choose Reload Config from the menu (or press cmd-alt-ctrl R}
-
 -- show an alert to let you know Hydra's running
 hydra.alert("Hydra sample config loaded", 1.5)
 
 -- open a repl with mash-R; requires https://github.com/sdegutis/hydra-cli
 hotkey.bind({"cmd", "ctrl", "alt"}, "R", repl.open)
-
--- show a helpful menu
-hydra.menu.show(function()
-    local t = {
-      {title = "Reload Config", fn = hydra.reload},
-      {title = "Open REPL", fn = repl.open},
-      {title = "-"},
-      {title = "About Hydra", fn = hydra.showabout},
-      {title = "Check for Updates...", fn = function() hydra.updates.check(nil, true) end},
-      {title = "Quit", fn = os.exit},
-    }
-
-    if not hydra.license.haslicense() then
-      table.insert(t, 1, {title = "Buy or Enter License...", fn = hydra.license.enter})
-      table.insert(t, 2, {title = "-"})
-    end
-
-    return t
-end)
 
 -- move the window to the right half of the screen
 function movewindow_righthalf()
@@ -38,13 +16,3 @@ end
 -- bind your custom function to a convenient hotkey
 -- note: it's good practice to keep hotkey-bindings separate from their functions, like we're doing here
 hotkey.new({"cmd", "ctrl", "alt"}, "L", movewindow_righthalf):enable()
-
--- uncomment this line if you want Hydra to make sure it launches at login
--- hydra.autolaunch.set(true)
-
--- when the "update is available" notification is clicked, open the website
-notify.register("showupdate", function() os.execute('open https://github.com/sdegutis/Hydra/releases') end)
-
--- check for updates every week, and also right now (when first launching)
-timer.new(timer.weeks(1), hydra.updates.check):start()
-hydra.updates.check()
