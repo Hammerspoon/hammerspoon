8f0b40c8306e63831328ef4f377219111067d635
Chris Jones <cmsj@tenshu.net>
2014-10-22 08:26:24 +0100

    Only include Sparkle pod when building the Release target

--- Pods/Target Support Files/Pods-Hammerspoon/Pods-Hammerspoon-environment.h
+++ Pods/Target Support Files/Pods-Hammerspoon/Pods-Hammerspoon-environment.h
@@ -6,15 +6,19 @@
 // project.
 
 
-// Sparkle
-#define COCOAPODS_POD_AVAILABLE_Sparkle
-#define COCOAPODS_VERSION_MAJOR_Sparkle 1
-#define COCOAPODS_VERSION_MINOR_Sparkle 8
-#define COCOAPODS_VERSION_PATCH_Sparkle 0
-
 // lua
 #define COCOAPODS_POD_AVAILABLE_lua
 #define COCOAPODS_VERSION_MAJOR_lua 5
 #define COCOAPODS_VERSION_MINOR_lua 2
 #define COCOAPODS_VERSION_PATCH_lua 3
 
+// Release build configuration
+#ifdef RELEASE
+
+  // Sparkle
+  #define COCOAPODS_POD_AVAILABLE_Sparkle
+  #define COCOAPODS_VERSION_MAJOR_Sparkle 1
+  #define COCOAPODS_VERSION_MINOR_Sparkle 8
+  #define COCOAPODS_VERSION_PATCH_Sparkle 0
+
+#endif
