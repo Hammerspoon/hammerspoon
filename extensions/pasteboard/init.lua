2d2fe6602759a246031db3d25ac7f8b2f920e57b
asmagill <asmagill@users.noreply.github.com>
2014-10-28 19:34:34 -0500

    hs.pasteboard module

---
+++ extensions/pasteboard/init.lua
@@ -0,0 +1,15 @@
+--- === hs.pasteboard ===
+---
+--- This is a module provides access to the OS X clipboard from within Hammerspoon
+---
+--- This module is based partially on code from the previous incarnation of Mjolnir by [Steven Degutis](https://github.com/sdegutis/).
+
+local module = require("hs.pasteboard.internal")
+
+-- private variables and methods -----------------------------------------
+
+-- Public interface ------------------------------------------------------
+
+-- Return Module Object --------------------------------------------------
+
+return module
