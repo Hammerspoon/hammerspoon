53119274ecc0712d9ffccacad3a7838cf54ddb2d
Chris Jones <cmsj@tenshu.net>
2014-11-03 18:46:03 +0100

    Move TranformAnimation to window/internal.m to prevent duplicate declatarion with application/internal.m, which includes window/window.h

--- extensions/window/internal.m
+++ extensions/window/internal.m
@@ -8,6 +8,45 @@ extern AXError _AXUIElementGetWindow(AXUIElementRef, CGWindowID* out);
 
 #define get_window_arg(L, idx) *((AXUIElementRef*)luaL_checkudata(L, idx, "hs.window"))
 
+@interface TransformAnimation : NSAnimation
+
+@property NSPoint newTopLeft;
+@property NSPoint oldTopLeft;
+@property NSSize newSize;
+@property NSSize oldSize;
+
+@property AXUIElementRef window;
+
+@end
+
+@implementation TransformAnimation
+
+- (void)setCurrentProgress:(NSAnimationProgress)progress {
+	[super setCurrentProgress:progress];
+	float value = self.currentValue;
+
+	NSPoint thePoint = (NSPoint) {
+		_oldTopLeft.x + value * (_newTopLeft.x - _oldTopLeft.x),
+		_oldTopLeft.y + value * (_newTopLeft.y - _oldTopLeft.y)
+	};
+
+	NSSize theSize = (NSSize) {
+		_oldSize.width + value * (_newSize.width - _oldSize.width),
+		_oldSize.height + value * (_newSize.height - _oldSize.height)
+	};
+
+	CFTypeRef positionStorage = (CFTypeRef)(AXValueCreate(kAXValueCGPointType, (const void *)&thePoint));
+	CFTypeRef sizeStorage = (CFTypeRef)(AXValueCreate(kAXValueCGSizeType, (const void *)&theSize));
+
+	AXUIElementSetAttributeValue(_window, (CFStringRef)NSAccessibilityPositionAttribute, positionStorage);
+	AXUIElementSetAttributeValue(_window, (CFStringRef)NSAccessibilitySizeAttribute, sizeStorage);
+
+	if (sizeStorage) CFRelease(sizeStorage);
+	if (positionStorage) CFRelease(positionStorage);
+}
+
+@end
+
 static NSSize geom_tosize(lua_State* L, int idx) {
     luaL_checktype(L, idx, LUA_TTABLE);
     CGFloat w = (lua_getfield(L, idx, "w"), luaL_checknumber(L, -1));
--- extensions/window/window.h
+++ extensions/window/window.h
@@ -15,44 +15,5 @@ static void new_window(lua_State* L, AXUIElementRef win) {
     lua_setuservalue(L, -2);
 }
 
-@interface TransformAnimation : NSAnimation
-
-@property NSPoint newTopLeft;
-@property NSPoint oldTopLeft;
-@property NSSize newSize;
-@property NSSize oldSize;
-
-@property AXUIElementRef window;
-
-@end
-
-@implementation TransformAnimation
-
-- (void)setCurrentProgress:(NSAnimationProgress)progress {
-	[super setCurrentProgress:progress];
-	float value = self.currentValue;
-
-	NSPoint thePoint = (NSPoint) {
-		_oldTopLeft.x + value * (_newTopLeft.x - _oldTopLeft.x),
-		_oldTopLeft.y + value * (_newTopLeft.y - _oldTopLeft.y)
-	};
-
-	NSSize theSize = (NSSize) {
-		_oldSize.width + value * (_newSize.width - _oldSize.width),
-		_oldSize.height + value * (_newSize.height - _oldSize.height)
-	};
-
-	CFTypeRef positionStorage = (CFTypeRef)(AXValueCreate(kAXValueCGPointType, (const void *)&thePoint));
-	CFTypeRef sizeStorage = (CFTypeRef)(AXValueCreate(kAXValueCGSizeType, (const void *)&theSize));
-
-	AXUIElementSetAttributeValue(_window, (CFStringRef)NSAccessibilityPositionAttribute, positionStorage);
-	AXUIElementSetAttributeValue(_window, (CFStringRef)NSAccessibilitySizeAttribute, sizeStorage);
-
-	if (sizeStorage) CFRelease(sizeStorage);
-	if (positionStorage) CFRelease(positionStorage);
-}
-
-@end
-
 
 #endif
