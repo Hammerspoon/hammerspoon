aa9490005e9c9030782af504e48ae1a856ea6d26
Chris Jones <cmsj@tenshu.net>
2014-11-04 00:13:45 +0100

    Add menu discovery/activation to hs.application. Closes #52

--- extensions/application/internal.m
+++ extensions/application/internal.m
@@ -270,6 +270,167 @@ static int application_kind(lua_State* L) {
     return 1;
 }
 
+// Internal helper function to get an AXUIElementRef for a menu item in an app
+AXUIElementRef _findmenuitem(lua_State* L, AXUIElementRef app, NSString *name) {
+    AXUIElementRef foundItem = nil;
+    AXUIElementRef menuBar;
+    AXError error = AXUIElementCopyAttributeValue(app, kAXMenuBarAttribute, (CFTypeRef *)&menuBar);
+    if (error) {
+        return nil;
+    }
+
+    CFIndex count = -1;
+    error = AXUIElementGetAttributeValueCount(menuBar, kAXChildrenAttribute, &count);
+    if (error) {
+        CFRelease(menuBar);
+        return nil;
+    }
+
+    CFArrayRef cf_children;
+    error = AXUIElementCopyAttributeValues(menuBar, kAXChildrenAttribute, 0, count, &cf_children);
+    if (error) {
+        CFRelease(menuBar);
+        return nil;
+    }
+
+    NSMutableArray *toCheck = [[NSMutableArray alloc] init];
+    [toCheck addObjectsFromArray:(__bridge NSArray *)cf_children];
+
+    int i = 5000; // This acts as a guard against this loop mysteriously running away
+    while (i > 0) {
+        i--;
+        if ([toCheck count] == 0) {
+            break;
+        }
+
+        // Get the first item in our queue and pop it out of the queue
+        id firstObject = [toCheck firstObject];
+        AXUIElementRef element = (__bridge AXUIElementRef)firstObject;
+        [toCheck removeObjectIdenticalTo:firstObject];
+
+        CFTypeRef cf_title;
+        AXError error = AXUIElementCopyAttributeValue(element, kAXTitleAttribute, &cf_title);
+        NSString *title = (__bridge_transfer NSString *)cf_title;
+
+        // Check if this is a submenu, if so add its children to the toCheck array
+        CFIndex childcount = -1;
+        error = AXUIElementGetAttributeValueCount(element, kAXChildrenAttribute, &childcount);
+        if (error) {
+            NSLog(@"Got an error (%d) checking child count, skipping", (int)error);
+            continue;
+        }
+        if (childcount > 0) {
+            // This is a submenu. Collect its children into toCheck and continue iterating
+            CFArrayRef cf_menuchildren;
+            error = AXUIElementCopyAttributeValues(element, kAXChildrenAttribute, 0, childcount, &cf_menuchildren);
+            if (error) {
+                NSLog(@"Got an error (%d) fetching menu children, skipping", (int)error);
+                continue;
+            }
+            [toCheck addObjectsFromArray:(__bridge NSArray *)cf_menuchildren];
+        } else if (childcount == 0) {
+            // This doesn't seem to be a submenu, so see if it's a match
+            if ([name isEqualToString:title]) {
+                // It's a match. Store a reference to it and break out of the loop
+                foundItem = element;
+                break;
+            }
+        }
+    }
+    CFRelease(menuBar);
+
+    if (i == 0) {
+        NSLog(@"WARNING: _findmenuitem overflowed 5000 iteration guard. You have some crazy menus, or we have a bug!");
+        lua_getglobal(L, "print");
+        lua_pushstring(L, "WARNING: _findmenuitem() overflowed 5000 iteration guard. This is either a Hammerspoon bug, or you have some very deep menus");
+        lua_call(L, 1, 0);
+        return nil;
+    }
+
+    return foundItem;
+}
+
+/// hs.application:findmenuitem(name) -> table or nil
+/// Method
+/// Returns nil if the menu item 'name' cannot be found. If it does exist, returns a table with two keys:
+///  enabled - whether the menu item can be selected/ticked. This will always be false if the application is not currently focussed
+///  ticked - whether the menu item is ticked or not (obviously this value is meaningless for menu items that can't be ticked)
+/// NOTE: This can only search for menu items that don't have children - i.e. you can't search for the name of a submenu
+static int application_findmenuitem(lua_State* L) {
+    AXError error;
+    AXUIElementRef app = get_app(L, 1);
+    NSString *name = [NSString stringWithUTF8String: luaL_checkstring(L, 2)];
+
+    AXUIElementRef foundItem = _findmenuitem(L, app, name);
+    if (!foundItem) {
+        NSLog(@"Couldn't find %@", name);
+        lua_pushnil(L);
+        return 1;
+    }
+
+    CFTypeRef enabled;
+    error = AXUIElementCopyAttributeValue(foundItem, kAXEnabledAttribute, &enabled);
+    if (error) {
+        NSLog(@"AXEnabled Error: %d", error);
+        lua_pushnil(L);
+        return 1;
+    }
+    CFTypeRef markchar;
+    error = AXUIElementCopyAttributeValue(foundItem, kAXMenuItemMarkCharAttribute, &markchar);
+    if (error && error != kAXErrorNoValue) {
+        NSLog(@"AXMenuItemMarkChar: %d", error);
+        lua_pushnil(L);
+        return 1;
+    }
+    BOOL marked;
+    if (error == kAXErrorNoValue) {
+        // There's no value, which mean MarkChar is (null) and the menu item is not ticked
+        marked = false;
+    } else {
+        // We might want to explicitly check for ✓ here, but this seems to be reliable enough for now
+        marked = true;
+    }
+
+    lua_newtable(L);
+
+    lua_pushstring(L, "enabled");
+    lua_pushboolean(L, [(__bridge NSNumber *)enabled boolValue]);
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "ticked");
+    lua_pushboolean(L, marked);
+    lua_settable(L, -3);
+
+    return 1;
+}
+
+/// hs.appliation:selectmenuitem(name) -> true or nil
+/// Method
+/// Selects a menu item called 'name' if it exists in this application.
+/// Depending on the type of menu item involved, this will either activate or tick/untick the menu item
+/// Returns true if AXPress() was sent to the menu item, or nil if it wasn't (e.g. because the menu item couldn't be found)
+static int application_selectmenuitem(lua_State* L) {
+    AXUIElementRef app = get_app(L, 1);
+    NSString *name = [NSString stringWithUTF8String: luaL_checkstring(L, 2)];
+
+    AXUIElementRef foundItem = _findmenuitem(L, app, name);
+    if (!foundItem) {
+        NSLog(@"Couldn't find %@", name);
+        lua_pushnil(L);
+        return 1;
+    }
+
+    AXError error = AXUIElementPerformAction(foundItem, kAXPressAction);
+    if (error) {
+        NSLog(@"AXPress error: %d", (int)error);
+        lua_pushnil(L);
+        return 1;
+    }
+
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
 /// hs.application.launchorfocus(name) -> bool
 /// Function
 /// Launches the app with the given name, or activates it if it's already running.
@@ -285,7 +446,7 @@ static const luaL_Reg applicationlib[] = {
     {"runningapplications", application_runningapplications},
     {"applicationforpid", application_applicationforpid},
     {"applicationsforbundleid", application_applicationsforbundleid},
-    
+
     {"allwindows", application_allwindows},
     {"mainwindow", application_mainwindow},
     {"_activate", application__activate},
@@ -301,8 +462,10 @@ static const luaL_Reg applicationlib[] = {
     {"pid", application_pid},
     {"isunresponsive", application_isunresponsive},
     {"kind", application_kind},
+    {"findmenuitem", application_findmenuitem},
+    {"selectmenuitem", application_selectmenuitem},
     {"launchorfocus", application_launchorfocus},
-    
+
     {NULL, NULL}
 };
 
