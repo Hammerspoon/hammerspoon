bc1ad793928ed7ccd68d6e3f862fea5a3888d49b
Peter van Dijk <peter@7bits.nl>
2014-11-12 21:51:24 +0100

    load all extensions, see what breaks

--- Hammerspoon Tests/init.lua
+++ Hammerspoon Tests/init.lua
@@ -1,1 +1,12 @@
-hs.alert.show('testing init.lua loaded')
+-- hs.alert.show('testing init.lua loaded')
+for k,v in pairs(_extensions) do
+  print(string.format("checking extension '%s'", k))
+  res, ext = pcall(load(string.format("return hs.%s", k)))
+  if res then
+    if type(ext) ~= 'table' then
+      print(string.format("type of 'hs.%s' is '%s', was expecting 'table'", k, type(ext)))
+    end
+  else
+    print(string.format("failed to load 'hs.%s', error was '%s'", k, ext))
+  end
+end
