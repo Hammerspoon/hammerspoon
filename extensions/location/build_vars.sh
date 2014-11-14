e9135143b4b7ea18ae8c0eae7a8ab53b1c58c73c
Chris Jones <cmsj@tenshu.net>
2014-11-14 08:51:11 +0000

    Explicitly link CoreLocation and CoreWLAN in location/wifi extensions, respectively, which should allow them to work on <10.10. Closes #131

---
+++ extensions/location/build_vars.sh
@@ -0,0 +1,2 @@
+# Explicitly link CoreLocation for <10.10
+export EXTRA_CFLAGS="-fobjc-arc -framework CoreLocation"
---
+++ extensions/wifi/build_vars.sh
@@ -0,0 +1,2 @@
+# Explicitly link CoreWLAN for <10.10
+export EXTRA_CFLAGS="-fobjc-arc -framework CoreWLAN"
