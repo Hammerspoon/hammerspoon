6f19b01a3cc69f22f1c822edf35c0ee74a505b7d
Tyler Mandry <tmandry@gmail.com>
2014-12-20 23:57:55 -0600

    Add an application object parameter to watcher callback

--- extensions/application/watcher.m
+++ extensions/application/watcher.m
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 #import <lauxlib.h>
+#import "application.h"
 
 /// === hs.application.watcher ===
 ///
@@ -83,8 +84,9 @@ typedef enum _event_t {
 
     lua_pushstring(L, [appName UTF8String]); // Parameter 1: application name
     lua_pushnumber(L, event); // Parameter 2: the event type
+    new_application(L, [app processIdentifier]); // Paremeter 3: application object
 
-    if (lua_pcall(L, 2, 0, -4) != 0) {
+    if (lua_pcall(L, 3, 0, -5) != 0) {
         NSLog(@"%s", lua_tostring(L, -1));
         lua_getglobal(L, "hs");
         lua_getfield(L, -1, "showError");
@@ -126,8 +128,12 @@ typedef enum _event_t {
 /// hs.application.watcher.new(fn) -> watcher
 /// Function
 /// Creates an application watcher that is able to capture application events.
+///
 /// The parameter fn has to be a function accepting two parameters. The first parameter passed to
-/// the function is the application name as string and the second parameter is the event type.
+/// the function is the application name as string, the second parameter is the event type, and the
+/// third is an application object. Note that if the application has been terminated, the
+/// application object will only be good for getting the PID.
+///
 /// The event type parameter can be one of the following values:
 /// hs.application.watcher.launching    -- The application will launch.
 /// hs.application.watcher.launched     -- The application has launched.
