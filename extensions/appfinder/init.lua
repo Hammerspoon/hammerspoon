160f15085e42a7b13b8f1fc6dba8b3c581d4e955
Chris Jones <cmsj@tenshu.net>
2015-01-03 11:52:58 +0000

    Switch hs.appfinder to new docstrings format

--- extensions/appfinder/init.lua
+++ extensions/appfinder/init.lua
@@ -25,6 +25,12 @@ end
 --- hs.appfinder.appFromName(name) -> app or nil
 --- Function
 --- Finds an application by its name (e.g. "Safari")
+---
+--- Parameters:
+---  * name - A string containing the name of the application to search for
+---
+--- Returns:
+---  * An hs.application object if one can be found, otherwise nil
 function appfinder.appFromName(name)
     return fnutils.find(application.runningApplications(), function(app) return app:title() == name end)
 end
@@ -32,6 +38,12 @@ end
 --- hs.appfinder.appFromWindowTitle(title) -> app or nil
 --- Function
 --- Finds an application by its window title (e.g. "Activity Monitor (All Processes)")
+---
+--- Parameters:
+---  * title - A string containing a window title of the application to search for
+---
+--- Returns:
+---  * An hs.application object if one can be found, otherwise nil
 function appfinder.appFromWindowTitle(title)
     return find_application_from_window(function(win) return win:title() == title end)
 end
@@ -39,10 +51,15 @@ end
 --- hs.appfinder.appFromWindowTitlePattern(pattern) -> app or nil
 --- Function
 --- Finds an application by Lua pattern in its window title (e.g."Inbox %(%d+ messages.*)")
----  Notes:
----      For more about Lua patterns, see:
----       http://lua-users.org/wiki/PatternsTutorial
----       http://www.lua.org/manual/5.2/manual.html#6.4.1
+---
+--- Parameters:
+---  * pattern - a Lua pattern describing a window title of the application to search for
+---
+--- Returns:
+---  * An hs.application object if one can be found, otherwise nil
+--
+--- Notes:
+---  * For more about Lua patterns, see http://lua-users.org/wiki/PatternsTutorial and http://www.lua.org/manual/5.2/manual.html#6.4.1
 function appfinder.appFromWindowTitlePattern(pattern)
     return find_application_from_window(function(win) return string.match(win:title(), pattern) end)
 end
@@ -50,6 +67,12 @@ end
 --- hs.appfinder.windowFromWindowTitle(title) -> win or nil
 --- Function
 --- Finds a window by its title (e.g. "Activity Monitor (All Processes)")
+---
+--- Parameters:
+---  * title - A string containing the title of the window to search for
+---
+--- Returns:
+---  * An hs.window object if one can be found, otherwise nil
 function appfinder.windowFromWindowTitle(title)
     return find_window_from_function(function(win) return win:title() == title end)
 end
