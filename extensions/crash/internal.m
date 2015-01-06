798c25318ee5a0f79a50bd2620b55488bf640309
Peter van Dijk <peter@7bits.nl>
2015-01-06 19:37:04 +0100

    document isMainThread

--- extensions/crash/internal.m
+++ extensions/crash/internal.m
@@ -24,6 +24,27 @@ static int burnTheWorld(lua_State *L __unused) {
 
 extern pthread_t mainthreadid;
 
+/// hs.crash.isMainThread() -> bool
+/// Method
+/// Tells you whether you are executing Lua on the main thread.
+///
+/// Notes:
+/// * This is for testing purposes only, you are extremely unlikely to need this in normal Hammerspoon usage
+/// * When developing a new extension, especially one that involves handlers for outside events, this function
+///   can be used to make sure your event handling happens on the main thread. You can use this Lua code
+///   to make Hammerspoon abort if anything happens on a different thread:
+///   ```lua
+///   local function crashifnotmain(reason)
+///     print("crashifnotmain called with reason", reason) -- may want to remove this, very verbose otherwise
+///     if not hs.crash.isMainThread() then
+///       print("not in main thread, crashing")
+///       hs.crash.crash()
+///     end
+///   end
+///
+///   debug.sethook(crashifnotmain, 'c')
+///   ```
+
 static int isMainThread(lua_State *L)
 {
     pthread_t id = pthread_self();
