a17b22f0e8380b0663c0fb03fec525b236941019
Chris Jones <cmsj@tenshu.net>
2015-01-17 14:36:11 +0000

    Update hs.dockicon to new docstrings format

--- extensions/dockicon/internal.m
+++ extensions/dockicon/internal.m
@@ -7,7 +7,13 @@ extern void MJDockIconSetVisible(BOOL visible) ;
 
 /// hs.dockicon.visible() -> bool
 /// Function
-/// Returns whether Mjolnir has a Dock icon, and thus can be switched to via Cmd-Tab.
+/// Determine whether Hammerspoon's dock icon is visible
+///
+/// Parameters:
+///  * None
+///
+/// Returns:
+///  * A boolean, true if the dock icon is visible, false if not
 static int icon_visible(lua_State* L) {
     BOOL indock = MJDockIconVisible();
     lua_pushboolean(L, indock);
@@ -16,7 +22,13 @@ static int icon_visible(lua_State* L) {
 
 /// hs.dockicon.show()
 /// Function
-/// Shows Mjolnir's dock icon; Mjolnir can then be switched to via Cmd-Tab.
+/// Make Hammerspoon's dock icon visible
+///
+/// Parameters:
+///  * None
+///
+/// Returns:
+///  * None
 static int icon_show(lua_State* L __unused) {
     MJDockIconSetVisible(YES) ;
     return 0;
@@ -24,23 +36,35 @@ static int icon_show(lua_State* L __unused) {
 
 /// hs.dockicon.hide()
 /// Function
-/// Hides Mjolnir's dock icon; Mjolnir will no longer show up when you Cmd-Tab.
+/// Hide Hammerspoon's dock icon
+///
+/// Parameters:
+///  * None
+///
+/// Returns:
+///  * None
 static int icon_hide(lua_State* L __unused) {
     MJDockIconSetVisible(NO) ;
     return 0;
 }
 
-/// hs.dockicon.bounce(indefinitely = false)
+/// hs.dockicon.bounce(indefinitely)
 /// Function
-/// Bounces Mjolnir's dock icon; if indefinitely is true, won't stop until you click the dock icon.
+/// Bounce Hammerspoon's dock icon
+///
+/// Parameters:
+///  * indefinitely - A boolean value, true if the dock icon should bounce until the dock icon is clicked, false if the dock icon should only bounce briefly
 static int icon_bounce(lua_State* L) {
     [[NSApplication sharedApplication] requestUserAttention: lua_toboolean(L, 1) ? NSCriticalRequest : NSInformationalRequest];
     return 0;
 }
 
-/// hs.dockicon.setBadge(str)
+/// hs.dockicon.setBadge(badge)
 /// Function
-/// Set's Mjolnir's dock icon's badge to the given string; pass an empty string to clear it.
+/// Set Hammerspoon's dock icon badge
+///
+/// Parameters:
+///  * badge - A string containing the label to place inside the dock icon badge. If the string is empty, the badge will be cleared
 static int icon_setBadge(lua_State* L) {
     NSDockTile* tile = [[NSApplication sharedApplication] dockTile];
     [tile setBadgeLabel:[NSString stringWithUTF8String: luaL_checkstring(L, 1)]];
