38e9cbf0b871d3cbf2e21a5023897eabe26ed040
Chris Jones <cmsj@tenshu.net>
2015-01-17 14:16:43 +0000

    Update hs.brightness to new docstrings format

--- extensions/brightness/internal.m
+++ extensions/brightness/internal.m
@@ -4,9 +4,15 @@
 #import <lauxlib.h>
 #import "math.h"
 
-/// hs.brightness.set(number) -> boolean
+/// hs.brightness.set(brightness) -> boolean
 /// Function
-/// Sets the display brightness. Number should be between 0 and 100.
+/// Sets the display brightness
+///
+/// Parameters:
+///  * brightness - A number between 0 and 100
+///
+/// Returns:
+///  * True if the brightness was set, false if not
 static int brightness_set(lua_State* L) {
     double level = MIN(MAX(luaL_checknumber(L, 1) / 100.0, 0.0), 1.0);
     bool found = false;
@@ -32,7 +38,13 @@ static int brightness_set(lua_State* L) {
 
 /// hs.brightness.get() -> number
 /// Function
-/// Returns the current brightness of the display.
+/// Returns the current brightness of the display
+///
+/// Parameters:
+///  * None
+///
+/// Returns:
+///  * A number containing the brightness of the display, between 0 and 100
 static int brightness_get(lua_State *L) {
     io_iterator_t iterator;
     kern_return_t result = IOServiceGetMatchingServices(kIOMasterPortDefault,
