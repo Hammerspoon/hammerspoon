57546e31f3579a5b2655f6da7c70f39adba7de0f
Chris Jones <cmsj@tenshu.net>
2015-01-30 18:04:30 +0000

    Add hs.mouse.getAbsolutePosition, hs.mouse.setAbsolutePosition, hs.mouse.getRelativePosition, hs.mouse.setRelativePosition. Fix docstring for hs.mouse.getCurrentScreen. Closes #206

--- extensions/mouse/init.lua
+++ extensions/mouse/init.lua
@@ -12,7 +12,96 @@ local check_list = {}
 
 -- Public interface ------------------------------------------------------
 
---- hs.mouse.getCurrentScreen() -> screen
+--- hs.mouse.getAbsolutePosition() -> point
+--- Function
+--- Gets the absolute co-ordinates of the mouse pointer
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * A point-table containing the absolute x and y co-ordinates of the mouse pointer
+---
+--- Notes:
+---  * The co-ordinates returned by this function are in relation to the full size of your desktop. If you have multiple monitors, the desktop is a large virtual rectangle that contains them all (e.g. if you have two 1920x1080 monitors and the mouse is in the middle of the second monitor, the returned table would be `{ x=2879, y=540 }`)
+---  * Multiple monitors of different sizes can cause the co-ordinates of some areas of the desktop to be negative. This is perfectly normal. 0,0 in the co-ordinates of the desktop is the top left of the primary monitor
+function module.getAbsolutePosition()
+    return module.get()
+end
+
+--- hs.mouse.setAbsolutePosition(point)
+--- Function
+--- Sets the absolute co-ordinates of the mouse pointer
+---
+--- Parameters:
+---  * point - A point-table containing the absolute x and y co-ordinates to move the mouse pointer to
+---
+--- Returns:
+---  * None
+---
+--- Notes:
+---  * The co-ordinates given to this function must be in relation to the full size of your desktop. See the notes for `hs.mouse.get` for more information
+function module.getAbsolutePosition()
+    return module.get()
+end
+
+--- hs.mouse.getRelativePosition() -> point or nil
+--- Function
+--- Gets the co-ordinates of the mouse pointer, relative to the screen it is on
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * A point-table containing the relative x and y co-ordinates of the mouse pointer, or nil if an error occured
+---
+--- Notes:
+---  * The co-ordinates returned by this function are relative to the top left pixel of the screen the mouse is on (see `hs.mouse.getAbsolutePosition` if you need the location in the full desktop space)
+function module.getRelativePosition()
+    local screen = hs.mouse.getCurrentScreen()
+    if screen == nil then
+        return nil
+    end
+
+    local frame = screen:fullFrame()
+    local point = hs.mouse.getAbsolutePosition()
+    local rel = {}
+
+    rel["x"] = point["x"] - frame["x"]
+    rel["y"] = point["y"] - frame["y"]
+
+    return rel
+end
+
+--- hs.mouse.setRelativePosition(point[, screen])
+--- Function
+--- Sets the co-ordinates of the mouse pointer, relative to a screen
+---
+--- Parameters:
+---  * point - A point-table containing the relative x and y co-ordinates to move the mouse pointer to
+---  * screen - An optional `hs.screen` object. Defaults to the screen the mouse pointer is currently on
+---
+--- Returns:
+---  * None
+function module.setRelativePosition(point, screen)
+    if screen == nil then
+        screen = hs.mouse.getCurrentScreen()
+        if screen == nil then
+            print("ERROR: Unable to find the current screen")
+            return nil
+        end
+    end
+
+    local frame = screen:fullFrame()
+    local abs = {}
+
+    abs["x"] = frame["x"] + point["x"]
+    abs["y"] = frame["y"] + point["y"]
+
+    return module.set(abs)
+end
+
+--- hs.mouse.getCurrentScreen() -> screen or nil
 --- Function
 --- Gets the screen the mouse pointer is on
 ---
@@ -23,7 +112,7 @@ local check_list = {}
 ---  * An `hs.screen` object that the mouse pointer is on, or nil if an error occurred
 function module.getCurrentScreen()
     local point = hs.mouse.get()
-    return hs.fnutils.find(hs.screen.allScreens(), function(screen) return isPointInRect(point, screen:fullFrame()) end)
+    return hs.fnutils.find(hs.screen.allScreens(), function(screen) return hs.geometry.isPointInRect(point, screen:fullFrame()) end)
 end
 
 -- Return Module Object --------------------------------------------------
--- extensions/mouse/internal.m
+++ extensions/mouse/internal.m
@@ -15,19 +15,6 @@ static void hammerspoon_pushpoint(lua_State* L, NSPoint point) {
     lua_pushnumber(L, point.y); lua_setfield(L, -2, "y");
 }
 
-/// hs.mouse.get() -> point
-/// Function
-/// Get the current location of the mouse pointer
-///
-/// Parameters:
-///  * None
-///
-/// Returns:
-///  * A point-table containing the x and y co-ordinates of the mouse pointer
-///
-/// Notes:
-///  * The co-ordinates returned by this function are in relation to the full size of your desktop. If you have multiple monitors, the desktop is a large virtual rectangle that contains them all (e.g. if you have two 1920x1080 monitors and the mouse is in the middle of the second monitor, the returned table would be `{ x=2879, y=540 }`)
-///  * Multiple monitors of different sizes can cause the co-ordinates of some areas of the desktop to be negative. This is perfectly normal. 0,0 in the co-ordinates of the desktop is the top left of the primary monitor
 static int mouse_get(lua_State* L) {
     CGEventRef ourEvent = CGEventCreate(NULL);
     hammerspoon_pushpoint(L, CGEventGetLocation(ourEvent));
@@ -35,24 +22,13 @@ static int mouse_get(lua_State* L) {
     return 1;
 }
 
-/// hs.mouse.set(point)
-/// Function
-/// Move the mouse pointer
-///
-/// Parameters:
-///  * point - A point-table containing the x and y co-ordinates to move the mouse pointer to
-///
-/// Returns:
-///  * None
-///
-/// Notes:
-///  * The co-ordinates given to this function must be in relation to the full size of your desktop. See the notes for `hs.mouse.get` for more information
 static int mouse_set(lua_State* L) {
     CGWarpMouseCursorPosition(hammerspoon_topoint(L, 1));
     return 0;
 }
 
 static const luaL_Reg mouseLib[] = {
+// Note that .get and .set are no longer documented. They should stick around for now, as they are used by our init.lua
     {"get", mouse_get},
     {"set", mouse_set},
     {NULL, NULL}
