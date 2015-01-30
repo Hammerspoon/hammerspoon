c45b97a2b838192b73b5fb031dbfb52d0be0c624
Chris Jones <cmsj@tenshu.net>
2015-01-30 17:26:57 +0000

    Add hs.mouse.getCurrentScreen

--- extensions/mouse/init.lua
+++ extensions/mouse/init.lua
@@ -12,6 +12,20 @@ local check_list = {}
 
 -- Public interface ------------------------------------------------------
 
+--- hs.mouse.getCurrentScreen() -> screen
+--- Function
+--- Gets the screen the mouse pointer is on
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * An `hs.screen` object that the mouse pointer is on, or nil if an error occurred
+function module.getCurrentScreen()
+    local point = hs.mouse.get()
+    return hs.fnutils.find(hs.screen.allScreens(), function(screen) return isPointInRect(point, screen:fullFrame()) end)
+end
+
 -- Return Module Object --------------------------------------------------
 
 return module
