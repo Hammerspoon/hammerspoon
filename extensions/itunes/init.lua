c438fb9dfc59a25cfa606071b456956974c5e51e
Chris Jones <cmsj@tenshu.net>
2015-02-05 11:17:13 +0000

    Add hs.itunes.getCurrentArtist(), hs.itunes.getCurrentAlbum() and hs.itunes.getCurrentTrack(). Also update hs.itunes docstrings to new format

--- extensions/itunes/init.lua
+++ extensions/itunes/init.lua
@@ -14,46 +14,115 @@ local function tell(cmd)
   return result
 end
 
---- hs.itunes.play() -> nil
+--- hs.itunes.play()
 --- Function
---- Toggles play/pause of current itunes track
+--- Toggles play/pause of current iTunes track
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function itunes.play()
   tell('playpause')
   alert.show(' ▶', 0.5)
 end
 
---- hs.itunes.pause() -> nil
+--- hs.itunes.pause()
 --- Function
---- Pauses of current itunes track
+--- Pauses of current iTunes track
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function itunes.pause()
   tell('pause')
   alert.show(' ◼', 0.5)
 end
 
---- hs.itunes.next() -> nil
+--- hs.itunes.next()
 --- Function
 --- Skips to the next itunes track
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function itunes.next()
   tell('next track')
   alert.show(' ⇥', 0.5)
 end
 
---- hs.itunes.previous() -> nil
+--- hs.itunes.previous()
 --- Function
 --- Skips to previous itunes track
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function itunes.previous()
   tell('previous track')
   alert.show(' ⇤', 0.5)
 end
 
---- hs.itunes.displayCurrentTrack() -> nil
+--- hs.itunes.displayCurrentTrack()
 --- Function
---- Displays information for current track
+--- Displays information for current track on screen
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
 function itunes.displayCurrentTrack()
   artist = tell('artist of the current track as string')
   album  = tell('album of the current track as string')
   track  = tell('name of the current track as string')
   alert.show(track .."\n".. album .."\n".. artist, 1.75)
+end
+
+--- hs.itunes.getCurrentArtist()
+--- Function
+--- Gets the name of the current Artist
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * A string containing the Artist of the current track
+function itunes.getCurrentArtist()
+    return tell('artist of the current track as string')
+end
+
+--- hs.itunes.getCurrentAlbum()
+--- Function
+--- Gets the name of the current Album
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * A string containing the Album of the current track
+function itunes.getCurrentAlbum()
+    return tell('album of the current track as string')
+end
+
+--- hs.itunes.getCurrentTrack()
+--- Function
+--- Gets the name of the current track
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * A string containing the name of the current track
+function itunes.getCurrentTrack()
+    return tell('name of the current track as string')
 end
 
 return itunes
