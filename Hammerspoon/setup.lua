66743d2f7aa1ec923f379a4f0c6908836668dd0d
Chris Jones <cmsj@tenshu.net>
2015-02-07 15:11:20 +0000

    Update core hs to use new docstrings format

--- Hammerspoon/setup.lua
+++ Hammerspoon/setup.lua
@@ -16,35 +16,32 @@ local function runstring(s)
   return str
 end
 
---- hs.configdir = "~/.hammerspoon/"
+--- hs.configdir
 --- Constant
---- The user's Hammerspoon config directory. Modules may use it, assuming
---- they've worked out a contract with the user about how to use it.
+--- A string containing Hammerspoon's configuration directory. Typically `~/.hammerspoon/`
 hs.configdir = configdir
 
 --- hs.docstrings_json_file
 --- Constant
---- This is the full path to the docs.json file shipped with Hammerspoon.
---- It contains the same documentation used to generate the full Hammerspoon
---- API documentation, in JSON form.
---- You can load, parse and access this information using the hs.doc extension
+--- A string containing the full path to the `docs.json` file inside Hammerspoon's app bundle. This contains the full Hammerspoon API documentation and can be accessed in the Console using `help("someAPI")`. It can also be loaded and processed by the `hs.doc` extension
 hs.docstrings_json_file = docstringspath
 
 --- hs.showError(err)
 --- Function
+--- Shows an error to the user, using Hammerspoon's Console
 ---
---- Presents an error to the user via Hammerspoon's GUI. The default
---- implementation prints the error, focuses Hammerspoon, and opens
---- Hammerspoon's console.
+--- Parameters:
+---  * err - A string containing an error message
 ---
---- Users can override this with a new function that shows errors in a
---- custom way.
+--- Returns:
+---  * None
 ---
---- Modules can call this in the event of an error, e.g. in callbacks
---- from the user:
+--- Notes:
+---  * You can override this function if you wish to route errors differently
+---  * Modules can call this in the event of an error, e.g. in callbacks from the user:
 ---
----     local ok, err = xpcall(callbackfn, debug.traceback)
----     if not ok then hs.showError(err) end
+---     ```local ok, err = xpcall(callbackfn, debug.traceback)
+---     if not ok then hs.showError(err) end```
 function hs.showError(err)
   hs._notify("Hammerspoon config error") -- undecided on this line
   print(err)
@@ -56,14 +53,15 @@ end
 --- Function
 --- Toggles the visibility of the console
 ---
---- If the console is not currently open, it will be opened. If it is open and not the focused window, it will be brought forward and focused.
---- If the console is focused, it will be closed.
----
 --- Parameters:
 ---  * None
 ---
 --- Returns:
 ---  * None
+---
+--- Notes:
+---  * If the console is not currently open, it will be opened. If it is open and not the focused window, it will be brought forward and focused.
+---  * If the console is focused, it will be closed.
 function hs.toggleConsole()
     local console = hs.appfinder.windowFromWindowTitle("Hammerspoon Console")
     if console and (console ~= hs.window.focusedWindow()) then
@@ -75,9 +73,18 @@ function hs.toggleConsole()
     end
 end
 
---- hs.rawprint = print
+--- hs.rawprint(aString)
 --- Function
---- The original print function, before hammerspoon overrides it.
+--- The original Lua print() function
+---
+--- Parameters:
+---  * aString - A string to be printed
+---
+--- Returns:
+---  * None
+---
+--- Notes:
+---  * Hammerspoon overrides Lua's print() function, but this is a reference we retain to is, should you need it for any reason
 local rawprint = print
 hs.rawprint = rawprint
 function print(...)
@@ -134,7 +141,20 @@ if autoload_extensions then
   })
 end
 
-function help(identifier)
+--- hs.help(identifier)
+--- Function
+--- Prints the documentation for some part of Hammerspoon's API
+---
+--- Parameters:
+---  * identifier - A string containing the signature of some part of Hammerspoon's API (e.g. `"hs.reload"`)
+---
+--- Returns:
+---  * None
+---
+--- Notes:
+---  * This function is mainly for runtime API help while using Hammerspoon's Console
+---  * You can also just use `help()` directly
+function hs.help(identifier)
   local doc = require "hs.doc"
   local tree = doc.fromJSONFile(hs.docstrings_json_file)
   local result = tree
@@ -145,6 +165,7 @@ function help(identifier)
 
   print(result)
 end
+help = hs.help
 
 if not hasinitfile then
   hs.notify.register("__noinitfile", function() os.execute("open http://www.hammerspoon.org/go/") end)
