c2bbc95bf4fc083f9c49062f7a74bc4377e67968
Chris Jones <cmsj@tenshu.net>
2015-02-17 15:26:38 +0000

    Add release script to repo

---
+++ scripts/librelease.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+# Helper functions for Hammerspoon release.sh
+
+############################## UTILITY FUNCTIONS ##############################
+
+function fail() {
+  echo "ERROR: $*"
+  exit 1
+}
+
+############################### SANITY CHECKERS ###############################
+
+function assert_github_release_token() {
+  if [ ! -f "${GITHUB_TOKEN_FILE}" ]; then
+    fail "ERROR: You do not have a github token in ${GITHUB_TOKEN_FILE}"
+  fi
+}
+
+function assert_codesign_authority_token() {
+  if [ ! -f "${CODESIGN_AUTHORITY_TOKEN_FILE}" ]; then
+    fail "ERROR: You do not have a code signing authority token in ${CODESIGN_AUTHORITY_TOKEN_FILE} (hint, it should look like 'Authority=Developer ID Application: Foo Bar (ABC123)'"
+  fi
+}
+
+function assert_version_in_xcode() {
+  XCODEVER="$(defaults read "${HAMMERSPOON_HOME}/Hammerspoon/Hammerspoon-Info" CFBundleVersion)"
+
+  if [ "$VERSION" != "$XCODEVER" ]; then
+      fail "ERROR: You asked for $VERSION to be released, but Xcode will build $XCODEVER"
+  fi
+}
+
+function assert_version_in_git_tags() {
+  pushd "${HAMMERSPOON_HOME}" >/dev/null
+  local GITVER="$(git tag | grep "$VERSION")"
+  popd >/dev/null
+
+  if [ "$VERSION" != "$GITVER" ]; then
+      pushd "${HAMMERSPOON_HOME}" >/dev/null
+      git tag
+      popd >/dev/null
+      fail "ERROR: You asked for $VERSION to be released, but git does not know about it"
+  fi
+}
+
+function assert_version_not_in_github_releases() {
+  local RELEASE_INFO="$(github-release info -t "$VERSION" 2>&1)"
+  if [ "$?" == "0" ]; then
+      echo "${RELEASE_INFO}"
+      fail "ERROR: github already seems to have version $VERSION"
+  fi
+}
+
+function assert_docs_bundle_complete() {
+  pushd "${HAMMERSPOON_HOME}/scripts/docs" >/dev/null
+  bundle check
+  if [ "$?" != "0" ]; then
+    fail "ERROR: docs bundle is incomplete. Ensure 'bundle' is installed and run 'sudo bundle install' in hammerspoon/scripts/docs/"
+  fi
+  popd >/dev/null
+}
+
+function assert_cocoapods_state() {
+  pushd "${HAMERSPOON_HOME}" >/dev/null
+  pod outdated >/dev/null 2>&1
+  if [ "$?" != "0" ]; then
+    fail "ERROR: cocoapods installation does not seem sane"
+  fi
+  popd >/dev/null
+}
+
+function assert_website_repo() {
+  pushd "${HAMMERSPOON_HOME}" >/dev/null
+  if [ ! -d website/.git ]; then
+    fail "ERROR: website repo does not exist. git clone git@github.com:Hammerspoon/hammerspoon.github.io.git"
+  fi
+  pushd website >/dev/null
+  git diff-index --quiet HEAD --
+  if [ "$?" != "0" ]; then
+    fail "ERROR: website repo has uncommitted changes"
+  fi
+  pushd "website" >/dev/null
+  git fetch origin
+  local DESYNC=$(git rev-list --left-right "@{upstream}"...HEAD)
+  if [ "${DESYNC}" != "" ]; then
+    echo "$DESYNC"
+    fail "ERROR: website repo is not in sync with upstream"
+  fi
+  popd >/dev/null
+}
+
+function assert_valid_code_signature() {
+  echo "Ensuring valid code signature..."
+  codesign --verify --verbose=4 "${HAMERSPOON_HOME}/build/Hammerspoon.app"
+  if [ "$?" != "0" ]; then
+      codesign -dvv "${HAMMERSPOON_HOME}/build/Hammerspoon.app"
+      fail "ERROR: Invalid signature"
+  fi
+}
+
+function assert_valid_code_signing_entity() {
+  echo "Ensuring valid signing entity..."
+  local SIGNER=$(codesign --display --verbose=4 "${HAMMERSPOON_HOME}/build/Hammerspoon.app" 2>&1 | grep ^Authority | head -1)
+  if [ "$SIGNER" != "$CODESIGN_AUTHORITY_TOKEN" ]; then
+      fail "ERROR: App is signed with the wrong key: $SIGNER"
+      exit 1
+  fi
+}
+
+function assert_gatekeeper_acceptance() {
+  echo "Ensuring Gatekeeper acceptance..."
+  spctl --verbose=4 --assess --type execute "${HAMMERSPOON_HOME}/build/Hammerspoon.app"
+  if [ "$?" != "0" ]; then
+      fail "ERROR: Gatekeeper rejection"
+      exit 1
+  fi
+}
+
+############################### BUILD FUNCTIONS ###############################
+
+function build_hammerspoon_app() {
+  pushd "${HAMMERSPOON_HOME}" >/dev/null
+  make clean
+  make
+  rm build/docs.json
+  make docs
+  popd >/dev/null
+  if [ ! -e "${HAMMERSPOON_HOME}"/build/Hammerspoon.app ]; then
+      fail "ERROR: Looks like the build failed. sorry!"
+  fi
+}
+
+############################ POST-BUILD FUNCTIONS #############################
+
+function compress_hammerspoon_app() {
+  echo "Compressing release..."
+  pushd "${HAMMERSPOON_HOME}/build" >/dev/null
+  zip -yqr "Hammerspoon-${VERSION}.zip" Hammerspoon.app/
+  export ZIPLEN=$(ls -l Hammerspoon-"${VERSION}".zip | awk '{ print $5 }')
+  popd >/dev/null
+}
+
+function archive_hammerspoon_app() {
+  echo "Archiving binary..."
+  pushd "${HAMMERSPOON_HOME}/../" >/dev/null
+  mkdir -p "archive/${VERSION}"
+  cp -a "${HAMMERSPOON_HOME}/build/Hammerspoon-${VERSION}.zip" "archive/${VERSION}/"
+  popd >/dev/null
+}
+
+function archive_dSYMs() {
+  echo "Archiving extension .dSYM files..."
+  pushd "${HAMMERSPOON_HOME}/../" >/dev/null
+  mkdir -p "archive/${VERSION}/dSYM"
+  rsync -arx --stats --include '*/' --include='*.dSYM/**' --exclude='*' "${HAMMERSPOON_HOME}/extensions/" "archive/${VERSION}/dSYM/"
+  popd >/dev/null
+}
+
+function archive_docs() {
+  echo "Archiving docs..."
+  pushd "${HAMMERSPOON_HOME}/../" >/dev/null
+  mkdir -p "archive/${VERSION}/docs"
+  cp -a "${HAMMERSPOON_HOME}/build/html" "archive/${VERSION}/docs/"
+  popd >/dev/null
+}
+
+############################# RELEASE FUNCTIONS ###############################
+
+function release_add_to_github() {
+  echo "Adding release..."
+  github-release release --tag "$VERSION"
+}
+
+function release_upload_binary() {
+  echo "Uploading binary..."
+  github-release upload --tag "$VERSION" -n "Hammerspoon-${VERSION}.zip" -f "${HAMMERSPOON_HOME}/build/Hammerspoon-${VERSION}.zip"
+}
+
+function release_upload_docs() {
+  echo "Uploading docs to github..."
+  pushd "${HAMMERSPOON_HOME}/../" >/dev/null
+  mv "${HAMMERSPOON_HOME}/build/html" "website/docs/${VERSION}"
+  rm "website/docs/*.html"
+  cp "website/docs/${VERSION}/*.html" website/docs/
+  pushd website >/dev/null
+  git add --all "docs/"
+  git commit -am "Add docs for ${VERSION}"
+  git push
+  popd >/dev/null
+  popd >/dev/null
+}
+
+function release_submit_dash_docs() {
+  echo "Uploading docs to Dash..."
+  pushd "${HAMMERSPOON_HOME}/../" >/dev/null
+  rm -rf dash
+  git clone git@github.com:Kapeli/Dash-User-Contributions.git dash
+  cp "${HAMMERSPOON_HOME}/build/Hammerspoon.tgz" dash/docsets/Hammerspoon/
+  pushd dash >/dev/null
+  git remote add hammerspoon git@github.com:hammerspoon/Dash-User-Contributions.git
+  cat > docsets/Hammerspoon/docset.json <<EOF
+  {
+      "name": "Hammerspoon",
+      "version": "${VERSION}",
+      "archive": "Hammerspoon.tgz",
+      "author": {
+          "name": "Hammerspoon Team",
+          "link": "http://www.hammerspoon.org/"
+      },
+      "aliases": [],
+
+      "specific_versions": [
+      ]
+  }
+EOF
+  git add docsets/Hammerspoon/Hammerspoon.tgz
+  git commit -am "Update Hammerspoon docset to ${VERSION}"
+  git push hammerspoon -f
+  popd >/dev/null
+
+  echo "To generate a PR, cd dash ; hub pull-request -m \"Update Hammerspoon docset to ${VERSION}\" -h hammerspoon:master"
+  popd >/dev/null
+}
+
+function release_update_appcast() {
+  echo "TBC (ziplen: ${ZIPLEN})"
+}
+
+function release_tweet() {
+  local CURRENT=$(t accounts | grep -B1 active | head -1)
+  t set active hammerspoon1
+  t update "Just release ${VERSION} - http://www.hammerspoon.org/"
+  t set active $CURRENT
+}
+
---
+++ scripts/release.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Usage: $0 VERSION"
+    exit 1
+fi
+
+# Early sanity check that we have everything we need
+if [ "$(which greadlink)" == "" ]; then
+    echo "ERROR: Unable to find greadlink. Maybe 'brew install coreutils'?"
+    exit 1
+fi
+
+# Store some variables for later
+export VERSION="$1"
+export CWD=$PWD
+export SCRIPT_NAME="$(basename "$0")"
+export SCRIPT_HOME="$(dirname "$(greadlink -f "$0")")"
+export HAMMERSPOON_HOME="$(greadlink -f "${SCRIPT_HOME}/../")"
+
+export CODESIGN_AUTHORITY_TOKEN_FILE="${HAMMERSPOON_HOME}/../token-codesign-authority"
+export GITHUB_TOKEN_FILE="${HAMMERSPOON_HOME}/../token-github-release"
+export GITHUB_USER="hammerspoon"
+export GITHUB_REPO="hammerspoon"
+
+# Import our function library
+source "${SCRIPT_HOME}/librelease.sh"
+
+echo "******** CHECKING SANITY:"
+
+assert_github_release_token && export GITHUB_TOKEN="$(cat "${GITHUB_TOKEN_FILE}")"
+assert_codesign_authority_token && export CODESIGN_AUTHORITY_TOKEN="$(cat "${CODESIGN_AUTHORITY_TOKEN_FILE}")"
+assert_version_in_xcode
+assert_version_in_git_tags
+assert_version_not_in_github_releases
+assert_docs_bundle_completeA
+assert_cocoapods_state
+assert_website_repo
+
+exit 0
+echo "******** BUILDING:"
+
+build_hammerspoon_app
+
+echo "******** VALIDATING:"
+
+assert_valid_code_signature
+assert_valid_code_signing_entity
+assert_gatekeeper_acceptance
+
+echo "******** PREPARING FOR UPLOAD:"
+
+compress_hammerspoon_app
+
+echo "******** ARCHIVING MATERIALS:"
+
+archive_hammerspoon_app
+archive_dSYMs
+archive_docs
+
+echo "******** UPLOADING:"
+
+release_add_to_github
+release_upload_binary
+release_upload_docs
+release_submit_dash_docs
+
+echo "Appcast zip length is: ${ZIPLEN}"
+
+echo "******** TWEETING:"
+
+release_tweet
