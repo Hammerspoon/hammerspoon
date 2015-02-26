04eb89e7f6053b196f5ec967e1716d5b1ab55371
Chris Jones <cmsj@tenshu.net>
2015-02-26 11:42:45 +0000

    Teach hs.crash to leave Crashlytics breadcrumbs. Closes #180

--- extensions/crash/init.lua
+++ extensions/crash/init.lua
@@ -1,6 +1,6 @@
 --- === hs.crash ===
 ---
---- Force Hammerspoon to crash (for development/testing purposes)
+--- Various features/facilities for developers who are working on Hammerspoon itself, or writing extensions for it. It is extremely unlikely that you should need any part of this extension, in a normal user configuration.
 
 local crash = require "hs.crash.internal"
 return crash
