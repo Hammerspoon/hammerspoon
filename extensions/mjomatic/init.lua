fc772db450ff5e51180098b903f3e49c62ac66c0
Joseph Ferano <feranodesign@hotmail.com>
2015-04-05 00:37:19 -0400

    Caught error in mjolmatic when the windowgram contains a program that is not open at the time. Now it's checking for a nil window

--- extensions/mjomatic/init.lua
+++ extensions/mjomatic/init.lua
@@ -142,13 +142,15 @@ function mjomatic.go(cfg)
             error(string.format('no window found for application %s (%s)', title, key))
         end
         local app = appfinder.appFromName(title)
-        local appWindows = app:allWindows()
-        -- alert.show(string.format('application title for %q is %q, main window %q', title, app:title(), window:title()))
-        for char, window in pairs(appWindows) do
-          if window then
-              resizetogrid(window, windows[key])
-          else
-              alert.show(string.format('application %s has no main window', app:title()))
+        if app ~= nil then
+          local appWindows = app:allWindows()
+          -- alert.show(string.format('application title for %q is %q, main window %q', title, app:title(), window:title()))
+          for char, window in pairs(appWindows) do
+            if window then
+                resizetogrid(window, windows[key])
+            else
+                alert.show(string.format('application %s has no main window', app:title()))
+            end
           end
         end
     end
