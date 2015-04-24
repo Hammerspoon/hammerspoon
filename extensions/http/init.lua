32e4b187bade05035c7f84d82b18d62d927d23a9
Till Klocke <till.klocke@gmail.com>
2015-04-24 18:12:37 +0200

    Added documentation to http get and post convenience methods

--- extensions/http/init.lua
+++ extensions/http/init.lua
@@ -1,10 +1,35 @@
 -- Simple loader for the Objective C code
 local http = require("hs.http.internal")
 
+--- hs.http.get(url, headers) -> int, string, table
+--- Function
+--- This is a simple wrapper function for GET requests
+---
+--- Parameters
+---  * url - A string representing the URL
+---  * headers - A table containing string keys and values representing the request headers
+---
+--- Returns:
+---  * An int representing the http response status
+---  * a string containing the response body
+---  * a table representing the respinse headers
 http.get = function(url,headers)
 	return http.doRequest(url,"GET",nil,headers)
 end
 
+--- hs.http.post(url, data, headers) -> int, string, table
+--- Function
+--- This is a simple wrapper function for POST requests
+---
+--- Parameters
+---  * url - A string representing the URL
+---  * data - A string representing the request body
+---  * headers - A table containing string keys and values representing the request headers
+---
+--- Returns:
+---  * An int representing the http response status
+---  * a string containing the response body
+---  * a table representing the respinse headers
 http.post = function(url,data,headers)
 	return http.doRequest(url,"POST",data,headers)
 end
