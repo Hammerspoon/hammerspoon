87594499c177caffa09e7365a749b99e69f2963b
Till Klocke <till.klocke@gmail.com>
2015-04-27 20:52:38 +0200

    Added documentation for the async helper functions

--- extensions/http/init.lua
+++ extensions/http/init.lua
@@ -34,10 +34,35 @@ http.post = function(url,data,headers)
 	return http.doRequest(url,"POST",data,headers)
 end
 
+--- hs.http.asyncGet(url, headers, callback)
+--- Function
+--- Simple wrapper to create an async GET request
+---
+--- Parameters:
+---  * url - A string representing the URL
+---  * headers - A table containing the request headers
+---  * callback - the callback to be called when the request succeeds or fails
+---
+--- Notes:
+---  * In case of a failure the callback will only be called with two parameters.
+---    first a negative integer indicating, there is an error. The second parameter is the error message
+---  * In case of success the callback will be called with the parameter status, data, headers
 http.asyncGet = function(url, headers, callback)
 	http.doAsyncRequest(url,"GET",nil,headers,callback)
 end
 
+--- hs.http.asyncPost(url, data, headers, callback)
+--- Function
+--- Simple wrapper to create an async POST request
+---
+--- Parameters:
+---  * url - A string representing the URL
+---  * data - String representing the request body
+---  * headers - A table containing the request headers
+---  * callback - Function to be called when the request succeeds or fails
+---
+--- Notes:
+---  * See hs.http.asyncGet
 http.asyncPost = function(url, data, headers, callback)
 	http.doAsyncRequest(url, "POST", data, headers, callback)
 end
