699fec0d4128fffee22d7037c3c698a9add32002
Chris Jones <cmsj@tenshu.net>
2015-05-13 18:05:57 +0100

    Formatting and docs fixes for hs.http. Also a few code tweaks for correctness. Closes #285"

--- extensions/http/init.lua
+++ extensions/http/init.lua
@@ -1,70 +1,84 @@
 -- Simple loader for the Objective C code
 local http = require("hs.http.internal")
 
+--- === hs.http ===
+---
+--- Perform HTTP requests
+
 --- hs.http.get(url, headers) -> int, string, table
 --- Function
---- This is a simple wrapper function for GET requests
+--- Sends an HTTP GET request to a URL
 ---
 --- Parameters
----  * url - A string representing the URL
----  * headers - A table containing string keys and values representing the request headers
+---  * url - A string containing the URL to retrieve
+---  * headers - A table containing string keys and values representing the request headers, or nil to add no headers
 ---
 --- Returns:
----  * An int representing the http response status
----  * a string containing the response body
----  * a table representing the respinse headers
-http.get = function(url,headers)
-	return http.doRequest(url,"GET",nil,headers)
+---  * A number containing the HTTP response status
+---  * A string containing the response body
+---  * A table containing the response headers
+---
+--- Notes:
+---  * This function is synchronous and will therefore block all other Lua execution while the request is in progress, you are encouraged to use the asynchronous functions
+http.get = function(url, headers)
+    return http.doRequest(url, "GET", nil, headers)
 end
 
 --- hs.http.post(url, data, headers) -> int, string, table
 --- Function
---- This is a simple wrapper function for POST requests
+--- Sends an HTTP POST request to a URL
 ---
 --- Parameters
----  * url - A string representing the URL
----  * data - A string representing the request body
----  * headers - A table containing string keys and values representing the request headers
+---  * url - A string containing the URL to submit to
+---  * data - A string containing the request body, or nil to send no body
+---  * headers - A table containing string keys and values representing the request headers, or nil to add no headers
 ---
 --- Returns:
----  * An int representing the http response status
----  * a string containing the response body
----  * a table representing the respinse headers
-http.post = function(url,data,headers)
-	return http.doRequest(url,"POST",data,headers)
+---  * A number containing the HTTP response status
+---  * A string containing the response body
+---  * A table containing the response headers
+---
+--- Notes:
+---  * This function is synchronous and will therefore block all other Lua execution while the request is in progress, you are encouraged to use the asynchronous functions
+http.post = function(url, data, headers)
+    return http.doRequest(url, "POST", data,headers)
 end
 
 --- hs.http.asyncGet(url, headers, callback)
 --- Function
---- Simple wrapper to create an async GET request
+--- Sends an HTTP GET request asynchronously
 ---
 --- Parameters:
----  * url - A string representing the URL
----  * headers - A table containing the request headers
----  * callback - the callback to be called when the request succeeds or fails
+---  * url - A string containing the URL to retrieve
+---  * headers - A table containing string keys and values representing the request headers, or nil to add no headers
+---  * callback - A function to be called when the request succeeds or fails. The function will be passed three parameters:
+---   * A number containing the HTTP response status
+---   * A string containing the response body
+---   * A table containing the response headers
 ---
 --- Notes:
----  * In case of a failure the callback will only be called with two parameters.
----    first a negative integer indicating, there is an error. The second parameter is the error message
----  * In case of success the callback will be called with the parameter status, data, headers
+---  * If the request fails, the callback function's first parameter will be negative and the second parameter will contain an error message. The third parameter will be nil
 http.asyncGet = function(url, headers, callback)
-	http.doAsyncRequest(url,"GET",nil,headers,callback)
+    http.doAsyncRequest(url, "GET", nil, headers, callback)
 end
 
 --- hs.http.asyncPost(url, data, headers, callback)
 --- Function
---- Simple wrapper to create an async POST request
+--- Sends an HTTP POST request asynchronously
 ---
 --- Parameters:
----  * url - A string representing the URL
----  * data - String representing the request body
----  * headers - A table containing the request headers
----  * callback - Function to be called when the request succeeds or fails
+---  * url - A string containing the URL to submit to
+---  * data - A string containing the request body, or nil to send no body
+---  * headers - A table containing string keys and values representing the request headers, or nil to add no headers
+---  * callback - A function to be called when the request succeeds or fails. The function will be passed three parameters:
+---   * A number containing the HTTP response status
+---   * A string containing the response body
+---   * A table containing the response headers
 ---
 --- Notes:
----  * See hs.http.asyncGet
+---  * If the request fails, the callback function's first parameter will be negative and the second parameter will contain an error message. The third parameter will be nil
 http.asyncPost = function(url, data, headers, callback)
-	http.doAsyncRequest(url, "POST", data, headers, callback)
+    http.doAsyncRequest(url, "POST", data, headers, callback)
 end
 
 return http
--- extensions/http/internal.m
+++ extensions/http/internal.m
@@ -7,20 +7,20 @@ static NSMutableArray* delegates;
 
 // Create a new Lua table and add all response header keys and values from the response
 static void createResponseHeaderTable(lua_State* L, NSHTTPURLResponse* httpResponse){
-	NSDictionary *responseHeaders = [httpResponse allHeaderFields];
-	lua_newtable(L);
-    for(id key in responseHeaders){
-    	NSString *value = [responseHeaders objectForKey:key];
-    	lua_pushstring(L, [value UTF8String]);
-    	lua_setfield(L,-2, [key UTF8String]);
+    NSDictionary *responseHeaders = [httpResponse allHeaderFields];
+    lua_newtable(L);
+    for (id key in responseHeaders) {
+        NSString *value = [responseHeaders objectForKey:key];
+        lua_pushstring(L, [value UTF8String]);
+        lua_setfield(L, -2, [key UTF8String]);
     }
 }
 
 // Show an error message via hs.showError
 static void showError(lua_State* L, NSString* error) {
-	lua_getglobal(L, "hs"); 
-	lua_getfield(L, -1, "showError"); 
-	lua_remove(L, -2);
+    lua_getglobal(L, "hs");
+    lua_getfield(L, -1, "showError");
+    lua_remove(L, -2);
     lua_pushstring(L, [error UTF8String]);
     lua_pcall(L, 1, 0, 0);
 }
@@ -29,191 +29,200 @@ static void showError(lua_State* L, NSString* error) {
 @interface connectionDelegate : NSObject<NSURLConnectionDelegate>
 @property lua_State* L;
 @property int fn;
-@property NSMutableData* receivedData;
-@property NSHTTPURLResponse* httpResponse;
-@property NSURLConnection* connection;
+@property(nonatomic, retain) NSMutableData* receivedData;
+@property(nonatomic, retain) NSHTTPURLResponse* httpResponse;
+@property(nonatomic, retain) NSURLConnection* connection;
 @end
 
 // Store a created delegate so we can cancel it on garbage collection
 static void store_delegate(connectionDelegate* delegate) {
-	[delegates addObject:delegate];
+    [delegates addObject:delegate];
 }
 
 // Remove a delegate either if loading has finished or if it needs to be
 // garbage collected. This unreferences the lua callback and sets the callback
 // reference in the delegate to LUA_NOREF.
 static void remove_delegate(lua_State* L, connectionDelegate* delegate) {
-	[delegate.connection cancel];
-	luaL_unref(L, LUA_REGISTRYINDEX, delegate.fn);
-	delegate.fn = LUA_NOREF;
-	[delegates removeObject:delegate];
+    [delegate.connection cancel];
+    luaL_unref(L, LUA_REGISTRYINDEX, delegate.fn);
+    delegate.fn = LUA_NOREF;
+    [delegates removeObject:delegate];
 }
 
 // Implementation of the connectionDelegate. If the property fn equals LUA_NOREF
 // no lua operations will be performed in the callbacks
 @implementation connectionDelegate
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-	self.httpResponse = (NSHTTPURLResponse *)response;
+- (void)connection:(NSURLConnection * __unused)connection didReceiveResponse:(NSURLResponse *)response {
+    self.httpResponse = (NSHTTPURLResponse *)response;
+}
 
+- (void)connection:(NSURLConnection * __unused)connection didReceiveData:(NSData *)data {
+    [self.receivedData appendData:data];
 }
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-	[self.receivedData appendData:data];
 
-}
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-	lua_State* L = self.L;
-	if(self.fn == LUA_NOREF){
-    	return;
+- (void)connectionDidFinishLoading:(NSURLConnection * __unused)connection {
+    lua_State* L = self.L;
+    if (self.fn == LUA_NOREF) {
+        return;
     }
-	NSString* stringReply = (NSString *)[[NSString alloc] initWithData:self.receivedData encoding:NSUTF8StringEncoding];
-	int statusCode = [self.httpResponse statusCode];
 
-	lua_rawgeti(L, LUA_REGISTRYINDEX, self.fn);
-    lua_pushinteger(L,statusCode);
-    lua_pushstring(L,[stringReply UTF8String]);
-    createResponseHeaderTable(L,self.httpResponse);
-    int cbRes = lua_pcall(L,3,0,0);
-    if (cbRes != LUA_OK){
-    	NSString* message = [NSString stringWithFormat:@"%s Code: %d", @"Can't call callback", cbRes];
-    	showError(L, message);
+    NSString* stringReply = (NSString *)[[NSString alloc] initWithData:self.receivedData encoding:NSUTF8StringEncoding];
+    int statusCode = [self.httpResponse statusCode];
+
+    lua_rawgeti(L, LUA_REGISTRYINDEX, self.fn);
+    lua_pushinteger(L, statusCode);
+    lua_pushstring(L, [stringReply UTF8String]);
+    createResponseHeaderTable(L, self.httpResponse);
+    int cbRes = lua_pcall(L, 3, 0, 0);
+    if (cbRes != LUA_OK) {
+        // FIXME: traceback shirley?
+        NSString* message = [NSString stringWithFormat:@"%@ Code: %d", @"Can't call callback", cbRes];
+        showError(L, message);
     }
-    remove_delegate(L,self);
+    remove_delegate(L, self);
 }
-- (void)connection:(NSURLConnection *)connection
-  didFailWithError:(NSError *)error {
-  	if(self.fn == LUA_NOREF){
-    	return;
+
+- (void)connection:(NSURLConnection * __unused)connection didFailWithError:(NSError *)error {
+    if (self.fn == LUA_NOREF){
+        return;
     }
-  	NSString* errorMessage = [NSString stringWithFormat:@"Connection failed: %@ - %@", [error localizedDescription], [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]];
-  	lua_rawgeti(self.L, LUA_REGISTRYINDEX, self.fn);
-  	lua_pushinteger(self.L,-1);
-  	lua_pushstring(self.L,[errorMessage UTF8String]);
-  	lua_pcall(self.L,2,0,0);
-  	remove_delegate(self.L,self);
+    NSString* errorMessage = [NSString stringWithFormat:@"Connection failed: %@ - %@", [error localizedDescription], [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]];
+    lua_rawgeti(self.L, LUA_REGISTRYINDEX, self.fn);
+    lua_pushinteger(self.L, -1);
+    lua_pushstring(self.L, [errorMessage UTF8String]);
+    lua_pcall(self.L, 2, 0, 0);
+    remove_delegate(self.L, self);
 }
+
 @end
 
-// If the user specified a request body, get it from stack, 
+// If the user specified a request body, get it from stack,
 // add it to the request and add the content length header field
 static void getBodyFromStack(lua_State* L, int index, NSMutableURLRequest* request){
-	if(!lua_isnil(L,index)){
-		NSString* body = [NSString stringWithCString:lua_tostring(L,3) encoding:NSASCIIStringEncoding];
-		NSData *postData = [body dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-		NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+    if (!lua_isnil(L, index)) {
+        NSString* body = [NSString stringWithCString:lua_tostring(L, 3) encoding:NSASCIIStringEncoding];
+        NSData *postData = [body dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+        NSString *postLength = [NSString stringWithFormat:@"%lu", [postData length]];
 
-		[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-		[request setHTTPBody:postData];
-	}
+        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+        [request setHTTPBody:postData];
+    }
 }
 
 // Gets all information for the request from the stack and creates a request
 static NSMutableURLRequest* getRequestFromStack(lua_State* L){
-	NSString* url = [NSString stringWithCString:luaL_checklstring(L,1,NULL) encoding:NSASCIIStringEncoding];
-	NSString* method = [NSString stringWithCString:luaL_checklstring(L,2,NULL) encoding:NSASCIIStringEncoding];
+    NSString* url = [NSString stringWithCString:luaL_checklstring(L, 1, NULL) encoding:NSASCIIStringEncoding];
+    NSString* method = [NSString stringWithCString:luaL_checklstring(L, 2, NULL) encoding:NSASCIIStringEncoding];
 
-	NSMutableURLRequest *request;
+    NSMutableURLRequest *request;
 
-	request = [NSMutableURLRequest requestWithURL: [NSURL URLWithString:url]];
-	[request setHTTPMethod:method];
-	return request;
+    request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    [request setHTTPMethod:method];
+    return request;
 }
 
 // Gets the table for the headers from stack and adds the key value pairs to the request object
 static void extractHeadersFromStack(lua_State* L, int index, NSMutableURLRequest* request){
-	if(!lua_isnil(L,index)){
-		lua_pushnil(L);
-		while (lua_next(L, index) != 0) {
-			// TODO check key and value for string type
-			NSString* key = [NSString stringWithCString:luaL_checkstring(L,-2) encoding:NSASCIIStringEncoding];
-			NSString* value = [NSString stringWithCString:luaL_checkstring(L,-1) encoding:NSASCIIStringEncoding];
+    if (!lua_isnil(L, index)) {
+        lua_pushnil(L);
+        while (lua_next(L, index) != 0) {
+            // TODO check key and value for string type
+            NSString* key = [NSString stringWithCString:luaL_checkstring(L, -2) encoding:NSASCIIStringEncoding];
+            NSString* value = [NSString stringWithCString:luaL_checkstring(L, -1) encoding:NSASCIIStringEncoding];
 
-			[request setValue:value forHTTPHeaderField:key];
+            [request setValue:value forHTTPHeaderField:key];
 
-			lua_pop(L, 1);
-		}
-	}
+            lua_pop(L, 1);
+        }
+    }
 }
 
-/// hs.http.doAsyncRequest(url, method, [data, headers,] callback)
+/// hs.http.doAsyncRequest(url, method, data, headers, callback)
 /// Function
-/// Creates a http request and executes it asynchronously, calling the callback upon success
+/// Creates an HTTP request and executes it asynchronously
 ///
 /// Parameters:
-///  * url - String representing the full URL
-///  * method - String representing the HTTP method
-///  * data - String presenting the request body
-///  * headers - Table containing string keys and values representing request header keys and values
-///  * callback - Callback function to called when the response is received
+///  * url - A string containing the URL
+///  * method - A string containing the HTTP method to use (e.g. "GET", "POST", etc)
+///  * data - A string containing the request body, or nil to send no body
+///  * headers - A table containing string keys and values representing request header keys and values, or nil to add no headers
+///  * callback - A function to called when the response is received
+///
+/// Returns:
+///  * None
 static int http_doAsyncRequest(lua_State* L){
-	NSMutableURLRequest* request = getRequestFromStack(L);
-	getBodyFromStack(L,3,request);
-	extractHeadersFromStack(L,4,request);
+    NSMutableURLRequest* request = getRequestFromStack(L);
+    getBodyFromStack(L, 3, request);
+    extractHeadersFromStack(L, 4, request);
 
-	luaL_checktype(L, 5, LUA_TFUNCTION);
-	lua_pushvalue(L, 5);
+    luaL_checktype(L, 5, LUA_TFUNCTION);
+    lua_pushvalue(L, 5);
 
-	connectionDelegate* delegate = [[connectionDelegate alloc] init];
-	delegate.L = L;
-	delegate.receivedData = [[NSMutableData alloc] init];
-	delegate.fn = luaL_ref(L, LUA_REGISTRYINDEX);
+    connectionDelegate* delegate = [[connectionDelegate alloc] init];
+    delegate.L = L;
+    delegate.receivedData = [[NSMutableData alloc] init];
+    delegate.fn = luaL_ref(L, LUA_REGISTRYINDEX);
 
-	store_delegate(delegate);
+    store_delegate(delegate);
 
-	NSURLConnection* connection = [[NSURLConnection alloc] initWithRequest:request delegate:delegate];
+    NSURLConnection* connection = [[NSURLConnection alloc] initWithRequest:request delegate:delegate];
 
-	delegate.connection = connection;
+    delegate.connection = connection;
 
-	return 0;
+    return 0;
 }
 
 /// hs.http.doRequest(url, method, [data, headers]) -> int, string, table
 /// Function
-/// Creates a http request and sends it to the specified URL
+/// Creates an HTTP request and executes it synchronously
 ///
 /// Parameters:
-///  * url - String representing the full URL
-///  * method - String, the HTTP method,i.e. GET, POST, PUT, DELETE
-///  * data - String representing the data to post to the URL
-///  * headers - Table of String keys and values used as headers for the request
+///  * url - A string containing the URL
+///  * method - A string containing the HTTP method to use (e.g. "GET", "POST", etc)
+///  * data - A string containing the data to POST to the URL, or nil to send no data
+///  * headers - A table of string keys and values used as headers for the request, or nil to add no headers
 ///
 /// Returns:
-///  * the response status code
-///  * the response body
-///  * the response headers as a table
+///  * A number containing the HTTP response status code
+///  * A string containing the response body
+///  * A table containing the response headers
+///
+/// Notes:
+///  * This function is synchronous and will therefore block all Lua execution until it completes. You are encouraged to use the asynchronous functions.
 static int http_doRequest(lua_State* L) {
-	NSMutableURLRequest *request = getRequestFromStack(L);
-	getBodyFromStack(L,3,request);
-	extractHeadersFromStack(L,4,request);
+    NSMutableURLRequest *request = getRequestFromStack(L);
+    getBodyFromStack(L, 3, request);
+    extractHeadersFromStack(L, 4, request);
 
-	NSData *dataReply;
-	NSURLResponse *response;
-	NSError *error;
+    NSData *dataReply;
+    NSURLResponse *response;
+    NSError *error;
 
-	dataReply = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    dataReply = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
 
-	NSString* stringReply = (NSString *)[[NSString alloc] initWithData:dataReply encoding:NSUTF8StringEncoding];
+    NSString* stringReply = (NSString *)[[NSString alloc] initWithData:dataReply encoding:NSUTF8StringEncoding];
 
-	NSHTTPURLResponse *httpResponse;
-	httpResponse = (NSHTTPURLResponse *)response;
+    NSHTTPURLResponse *httpResponse;
+    httpResponse = (NSHTTPURLResponse *)response;
     int statusCode = [httpResponse statusCode];
 
-    lua_pushinteger(L,statusCode);
+    lua_pushinteger(L, statusCode);
     lua_pushstring(L, [stringReply UTF8String]);
 
-    createResponseHeaderTable(L,httpResponse);
+    createResponseHeaderTable(L, httpResponse);
 
     return 3;
 }
 
 static int http_gc(lua_State* L){
-	NSMutableArray* delegatesCopy = [[NSMutableArray alloc] init];
-	[delegatesCopy addObjectsFromArray:delegates];
+    NSMutableArray* delegatesCopy = [[NSMutableArray alloc] init];
+    [delegatesCopy addObjectsFromArray:delegates];
 
-	for(connectionDelegate* delegate in delegatesCopy){
-		remove_delegate(L, delegate);
-	}
+    for (connectionDelegate* delegate in delegatesCopy){
+        remove_delegate(L, delegate);
+    }
 
-	return 0;
+    return 0;
 }
 
 static const luaL_Reg httplib[] = {
@@ -230,7 +239,7 @@ static const luaL_Reg metalib[] = {
 };
 
 int luaopen_hs_http_internal(lua_State* L) {
-	delegates = [[NSMutableArray alloc] init];
+    delegates = [[NSMutableArray alloc] init];
     luaL_newlib(L, httplib);
 
     luaL_newlib(L, metalib);
