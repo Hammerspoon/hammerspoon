1e02f44fcc78f8952d6b7a8ba4d9bf47f11d6191
asmagill <asmagill@users.noreply.github.com>
2015-06-22 01:53:45 -0500

    add a few more codepoints, add tostring metamethod, detect 5.3 versus 5.2 and uses builtin utf8 library, if available

--- extensions/utf8_53/init.lua
+++ extensions/utf8_53/init.lua
@@ -1,9 +1,14 @@
 --- === hs.utf8_53 ===
 ---
 --- Functions providing basic support for UTF-8 encodings
+---
+--- For Hammerspoon versions running with the Lua 5.2 core, this module will include a compatibility library to provide the same functionality as the Lua 5.3.1 utf8 library. For Hammerspoon versions running with the Lua 5.3 core, this compatibility library is left out, but a metatable index will pass through the same function names directly to the builtin Lua 5.3 utf8 library, allowing code which relies on these functions to work in either environment.  The additional functions providing codepoint to UTF8 sequence conversion and the registering of labels for common codepoints is provided in both environments.
+---
+--- Compatibility library notes:
+---
 --- These functions are from the UTF-8 Library as provided by the [Lua 5.3.1 programming language](http://www.lua.org/). This is primarily this a wrapper to allow easy inclusion within the Hammerspoon environment.
 ---
---- The following text is from the preliminary [reference documentation](http://www.lua.org/docs.html) for the Lua 5.3.1 programming language.
+--- The following text is from the [reference documentation](http://www.lua.org/docs.html) for the Lua 5.3.1 programming language.
 ---
 --- > This library provides basic support for UTF-8 encoding. It provides all its functions inside the table utf8. This library does not provide any support for Unicode other than the handling of the encoding. Any operation that needs the meaning of a character, such as character classification, is outside its scope.
 --- >
@@ -68,7 +73,22 @@
 --- Notes:
 ---  * This variable is ported from the Lua 5.3.1 source code.
 
-local module = require("hs.utf8_53.internal-utf8")
+local module = {}
+if string.match(_VERSION,"5.3") then
+    module = setmetatable(module, {
+        __index = function(object, key)
+            for i,v in pairs(_G.utf8) do
+                if string.lower(key) == i then return v end
+            end
+            return nil
+        end
+    })
+else
+    print("-- loading utf8_53 compatibility library")
+    module = require("hs.utf8_53.internal-utf8")
+end
+
+local fnutils = require("hs.fnutils")
 
 -- private variables and methods -----------------------------------------
 
@@ -141,7 +161,14 @@ end
 
 -- see below -- we need it defined for the registration function, but documentation will be
 -- given where we add the predefined keys.
-module.registeredKeys = {}
+module.registeredKeys = setmetatable({}, { __tostring = function(object)
+            local output = ""
+            for i,v in fnutils.sortByKeys(object) do
+                output = output..string.format("(U+%04X) %-15s  %s\n", module.codepoint(v), i, v)
+            end
+            return output
+    end
+})
 
 --- hs.utf8_53.registerCodepoint(label, codepoint) -> string
 --- Function
@@ -170,88 +197,108 @@ end
 ---
 --- Predefined keys include:
 ---
----     (U+2325) alt            ⌥
----     (U+F8FF) apple          
----     (U+21E4) backtab        ⇤
----     (U+21EA) capslock       ⇪
----     (U+2318) cmd            ⌘
----     (U+2303) ctrl           ⌃
----     (U+232B) delete         ⌫
----     (U+2193) down           ↓
----     (U+21E3) down2          ⇣
----     (U+23CF) eject          ⏏
----     (U+21F2) end            ⇲
----     (U+2198) end2           ↘
----     (U+238B) escape         ⎋
----     (U+2326) forwarddelete  ⌦
----     (U+FE56) help           ﹖
----     (U+21F1) home           ⇱
----     (U+2196) home2          ↖
----     (U+21B8) home3          ↸
----     (U+2190) left           ←
----     (U+21E0) left2          ⇠
----     (U+21ED) numlock        ⇭
----     (U+2325) option         ⌥
----     (U+2327) padclear       ⌧
----     (U+2324) padenter       ⌤
----     (U+2386) padenter2      ⎆
----     (U+21A9) padenter3      ↩
----     (U+21DF) pagedown       ⇟
----     (U+21DE) pageup         ⇞
----     (U+233D) power          ⌽
----     (U+23CE) return         ⏎
----     (U+21A9) return2        ↩
----     (U+2192) right          →
----     (U+21E2) right2         ⇢
----     (U+21E7) shift          ⇧
----     (U+2423) space          ␣
----     (U+21E5) tab            ⇥
----     (U+2191) up             ↑
----     (U+21E1) up2            ⇡
+---     (U+2325) alt              ⌥
+---     (U+F8FF) apple            
+---     (U+21E4) backtab          ⇤
+---     (U+21EA) capslock         ⇪
+---     (U+2713) checkMark        ✓
+---     (U+2318) cmd              ⌘
+---     (U+00A9) copyrightSign    ©
+---     (U+2303) ctrl             ⌃
+---     (U+232B) delete           ⌫
+---     (U+2193) down             ↓
+---     (U+21E3) down2            ⇣
+---     (U+23CF) eject            ⏏
+---     (U+21F2) end              ⇲
+---     (U+2198) end2             ↘
+---     (U+238B) escape           ⎋
+---     (U+2326) forwarddelete    ⌦
+---     (U+FE56) help             ﹖
+---     (U+21F1) home             ⇱
+---     (U+2196) home2            ↖
+---     (U+21B8) home3            ↸
+---     (U+2190) left             ←
+---     (U+21E0) left2            ⇠
+---     (U+201C) leftDoubleQuote  “
+---     (U+2018) leftSingleQuote  ‘
+---     (U+00B7) middleDot        ·
+---     (U+21ED) numlock          ⇭
+---     (U+2325) option           ⌥
+---     (U+2327) padclear         ⌧
+---     (U+2324) padenter         ⌤
+---     (U+2386) padenter2        ⎆
+---     (U+21A9) padenter3        ↩
+---     (U+21DF) pagedown         ⇟
+---     (U+21DE) pageup           ⇞
+---     (U+233D) power            ⌽
+---     (U+00AE) registeredSign   ®
+---     (U+23CE) return           ⏎
+---     (U+21A9) return2          ↩
+---     (U+2192) right            →
+---     (U+21E2) right2           ⇢
+---     (U+201D) rightDoubleQuote  ”
+---     (U+2019) rightSingleQuote  ’
+---     (U+00A7) sectionSign      §
+---     (U+21E7) shift            ⇧
+---     (U+2423) space            ␣
+---     (U+21E5) tab              ⇥
+---     (U+2191) up               ↑
+---     (U+21E1) up2              ⇡
 ---
 --- Notes:
 ---  * This variable is *NOT* part of the Lua 5.3.1 source code, and is provided for convenience within Hammerspoon.
----  * To see a list of the currently defined characters and labels, enter the following code in the Hammerspoon console:
----    * for i,v in pairs(hs.utf8_53.registeredKeys) do print(string.format("(U+%0X) %-15s  %s", hs.utf8_53.codepoint(v), i, v)) end
+---  * To see a list of the currently defined characters and labels, a __tostring meta-method is included so that referencing the table directly as a string will return the current definitions.
+---    * For reference, this meta-method is essentially the following:
+---
+---      for i,v in hs.fnutils.sortByKeys(hs.utf8_53.registeredKeys) do print(string.format("(U+%04X) %-15s  %s", hs.utf8_53.codepoint(v), i, v)) end
 
-module.registerCodepoint("alt",           0x2325)
-module.registerCodepoint("apple",         0xF8FF)
-module.registerCodepoint("backtab",       0x21E4)
-module.registerCodepoint("capslock",      0x21EA)
-module.registerCodepoint("cmd",           0x2318)
-module.registerCodepoint("ctrl",          0x2303)
-module.registerCodepoint("delete",        0x232B)
-module.registerCodepoint("down",          0x2193)
-module.registerCodepoint("down2",         0x21E3)
-module.registerCodepoint("eject",         0x23CF)
-module.registerCodepoint("end",           0x21F2)
-module.registerCodepoint("end2",          0x2198)
-module.registerCodepoint("escape",        0x238B)
-module.registerCodepoint("forwarddelete", 0x2326)
-module.registerCodepoint("help",          0xFE56)
-module.registerCodepoint("home",          0x21F1)
-module.registerCodepoint("home2",         0x2196)
-module.registerCodepoint("home3",         0x21B8)
-module.registerCodepoint("left",          0x2190)
-module.registerCodepoint("left2",         0x21E0)
-module.registerCodepoint("numlock",       0x21ED)
-module.registerCodepoint("option",        0x2325)
-module.registerCodepoint("padclear",      0x2327)
-module.registerCodepoint("padenter",      0x2324)
-module.registerCodepoint("padenter2",     0x2386)
-module.registerCodepoint("padenter3",     0x21A9)
-module.registerCodepoint("pagedown",      0x21DF)
-module.registerCodepoint("pageup",        0x21DE)
-module.registerCodepoint("power",         0x233D)
-module.registerCodepoint("return",        0x23CE)
-module.registerCodepoint("return2",       0x21A9)
-module.registerCodepoint("right",         0x2192)
-module.registerCodepoint("right2",        0x21E2)
-module.registerCodepoint("shift",         0x21E7)
-module.registerCodepoint("space",         0x2423)
-module.registerCodepoint("tab",           0x21E5)
-module.registerCodepoint("up",            0x2191)
-module.registerCodepoint("up2",           0x21E1)
+module.registerCodepoint("alt",              0x2325)
+module.registerCodepoint("apple",            0xF8FF)
+module.registerCodepoint("backtab",          0x21E4)
+module.registerCodepoint("capslock",         0x21EA)
+module.registerCodepoint("cmd",              0x2318)
+module.registerCodepoint("ctrl",             0x2303)
+module.registerCodepoint("delete",           0x232B)
+module.registerCodepoint("down",             0x2193)
+module.registerCodepoint("down2",            0x21E3)
+module.registerCodepoint("eject",            0x23CF)
+module.registerCodepoint("end",              0x21F2)
+module.registerCodepoint("end2",             0x2198)
+module.registerCodepoint("escape",           0x238B)
+module.registerCodepoint("forwarddelete",    0x2326)
+module.registerCodepoint("help",             0xFE56)
+module.registerCodepoint("home",             0x21F1)
+module.registerCodepoint("home2",            0x2196)
+module.registerCodepoint("home3",            0x21B8)
+module.registerCodepoint("left",             0x2190)
+module.registerCodepoint("left2",            0x21E0)
+module.registerCodepoint("numlock",          0x21ED)
+module.registerCodepoint("option",           0x2325)
+module.registerCodepoint("padclear",         0x2327)
+module.registerCodepoint("padenter",         0x2324)
+module.registerCodepoint("padenter2",        0x2386)
+module.registerCodepoint("padenter3",        0x21A9)
+module.registerCodepoint("pagedown",         0x21DF)
+module.registerCodepoint("pageup",           0x21DE)
+module.registerCodepoint("power",            0x233D)
+module.registerCodepoint("return",           0x23CE)
+module.registerCodepoint("return2",          0x21A9)
+module.registerCodepoint("right",            0x2192)
+module.registerCodepoint("right2",           0x21E2)
+module.registerCodepoint("shift",            0x21E7)
+module.registerCodepoint("space",            0x2423)
+module.registerCodepoint("tab",              0x21E5)
+module.registerCodepoint("up",               0x2191)
+module.registerCodepoint("up2",              0x21E1)
+module.registerCodepoint("middleDot",        0x00B7)
+module.registerCodepoint("leftSingleQuote",  0x2018)
+module.registerCodepoint("rightSingleQuote", 0x2019)
+module.registerCodepoint("leftDoubleQuote",  0x201C)
+module.registerCodepoint("rightDoubleQuote", 0x201D)
+module.registerCodepoint("sectionSign",      0x00A7)
+module.registerCodepoint("copyrightSign",    0x00A9)
+module.registerCodepoint("registeredSign",   0x00AE)
+module.registerCodepoint("checkMark",        0x2713)
 
 -- Return Module Object --------------------------------------------------
 
