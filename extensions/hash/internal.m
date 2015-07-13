d13c61d803c0e70faf9b7da8ed8362a885cfa074
Chris Jones <cmsj@tenshu.net>
2015-07-13 17:29:55 +0100

    Rename previous hs.hash functions to uppercase, add HMAC variants. Relates to #375

--- extensions/hash/internal.m
+++ extensions/hash/internal.m
@@ -1,5 +1,6 @@
 #import <Cocoa/Cocoa.h>
 #import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonHMAC.h>
 #import <LuaSkin/LuaSkin.h>
 #import "../hammerspoon.h"
 
@@ -23,7 +24,29 @@ static int doHash(lua_State *L, CC_LONG length, unsigned char *(*hashFunc)(const
     return 1;
 }
 
-/// hs.hash.sha1(data) -> string
+static int doHashHMAC(lua_State *L, CCHmacAlgorithm algorithm, CC_LONG resultLength) {
+    unsigned char digest[resultLength + 1];
+    size_t keyLength;
+    size_t dataLength;
+    const char *key = luaL_checklstring(L, 1, &keyLength);
+    const char *data = luaL_checklstring(L, 2, &dataLength);
+    NSMutableString *conversionSink = [NSMutableString string];
+
+    CCHmac(algorithm, key, keyLength, data, dataLength, digest);
+    digest[resultLength] = 0;
+
+    for (unsigned int i = 0; i < resultLength; i++) {
+        [conversionSink appendFormat:@"%02x", digest[i]];
+    }
+
+    //NSLog(@"HMAC Hashed '%s' with key '%s' into '%@'", data, key, conversionSink);
+
+    lua_pushstring(L, [conversionSink UTF8String]);
+
+    return 1;
+}
+
+/// hs.hash.SHA1(data) -> string
 /// Function
 /// Calculates an SHA1 hash
 ///
@@ -31,12 +54,12 @@ static int doHash(lua_State *L, CC_LONG length, unsigned char *(*hashFunc)(const
 ///  * data - A string containing some data to hash
 ///
 /// Returns:
-///  * A string containing the SHA1 hash of the supplied data
+///  * A string containing the hash of the supplied data
 static int hash_sha1(lua_State *L) {
     return doHash(L, CC_SHA1_DIGEST_LENGTH, CC_SHA1);
 }
 
-/// hs.hash.sha256(data) -> string
+/// hs.hash.SHA256(data) -> string
 /// Function
 /// Calculates an SHA256 hash
 ///
@@ -44,12 +67,12 @@ static int hash_sha1(lua_State *L) {
 ///  * data - A string containing some data to hash
 ///
 /// Returns:
-///  * A string containing the SHA256 hash of the supplied data
+///  * A string containing the hash of the supplied data
 static int hash_sha256(lua_State *L) {
     return doHash(L, CC_SHA256_DIGEST_LENGTH, CC_SHA256);
 }
 
-/// hs.hash.sha512(data) -> string
+/// hs.hash.SHA512(data) -> string
 /// Function
 /// Calculates an SHA512 hash
 ///
@@ -57,12 +80,12 @@ static int hash_sha256(lua_State *L) {
 ///  * data - A string containing some data to hash
 ///
 /// Returns:
-///  * A string containing the SHA512 hash of the supplied data
+///  * A string containing the hash of the supplied data
 static int hash_sha512(lua_State *L) {
     return doHash(L, CC_SHA512_DIGEST_LENGTH, CC_SHA512);
 }
 
-/// hs.hash.md5(data) -> string
+/// hs.hash.MD5(data) -> string
 /// Function
 /// Calculates an MD5 hash
 ///
@@ -70,16 +93,77 @@ static int hash_sha512(lua_State *L) {
 ///  * data - A string containing some data to hash
 ///
 /// Returns:
-///  * A string containing the MD5 hash of the supplied data
+///  * A string containing the hash of the supplied data
 static int hash_md5(lua_State *L) {
     return doHash(L, CC_MD5_DIGEST_LENGTH, CC_MD5);
 }
 
+/// hs.hash.hmacSHA1(key, data) -> string
+/// Function
+/// Calculates an HMAC using a key and a SHA1 hash
+///
+/// Parameters:
+///  * key - A string containing a secret key to use
+///  * data - A string containing the data to hash
+///
+/// Returns:
+///  * A string containing the hash of the supplied data
+static int hash_sha1_hmac(lua_State *L) {
+    return doHashHMAC(L, kCCHmacAlgSHA1, CC_SHA1_DIGEST_LENGTH);
+}
+
+/// hs.hash.hmacSHA256(key, data) -> string
+/// Function
+/// Calculates an HMAC using a key and a SHA256 hash
+///
+/// Parameters:
+///  * key - A string containing a secret key to use
+///  * data - A string containing the data to hash
+///
+/// Returns:
+///  * A string containing the hash of the supplied data
+static int hash_sha256_hmac(lua_State *L) {
+    return doHashHMAC(L, kCCHmacAlgSHA256, CC_SHA256_DIGEST_LENGTH);
+}
+
+/// hs.hash.hmacSHA512(key, data) -> string
+/// Function
+/// Calculates an HMAC using a key and a SHA512 hash
+///
+/// Parameters:
+///  * key - A string containing a secret key to use
+///  * data - A string containing the data to hash
+///
+/// Returns:
+///  * A string containing the hash of the supplied data
+static int hash_sha512_hmac(lua_State *L) {
+    return doHashHMAC(L, kCCHmacAlgSHA512, CC_SHA512_DIGEST_LENGTH);
+}
+
+/// hs.hash.hmacMD5(key, data) -> string
+/// Function
+/// Calculates an HMAC using a key and an MD5 hash
+///
+/// Parameters:
+///  * key - A string containing a secret key to use
+///  * data - A string containing the data to hash
+///
+/// Returns:
+///  * A string containing the hash of the supplied data
+static int hash_md5_hmac(lua_State *L) {
+    return doHashHMAC(L, kCCHmacAlgMD5, CC_MD5_DIGEST_LENGTH);
+}
+
 static const luaL_Reg hashlib[] = {
-    {"sha1", hash_sha1},
-    {"sha256", hash_sha256},
-    {"sha512", hash_sha512},
-    {"md5", hash_md5},
+    {"SHA1", hash_sha1},
+    {"SHA256", hash_sha256},
+    {"SHA512", hash_sha512},
+    {"MD5", hash_md5},
+
+    {"hmacSHA1", hash_sha1_hmac},
+    {"hmacSHA256", hash_sha256_hmac},
+    {"hmacSHA512", hash_sha512_hmac},
+    {"hmacMD5", hash_md5_hmac},
 
     {}
 };
