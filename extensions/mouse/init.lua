50e690adb6815d69a5ec53a8731d2a7bc0fb9b59
asmagill <asmagill@users.noreply.github.com>
2015-08-04 18:19:40 -0500

    mouse

--- extensions/mouse/init.lua
+++ extensions/mouse/init.lua
@@ -11,41 +11,30 @@ local screen = require("hs.screen")
 
 -- private variables and methods -----------------------------------------
 
-local check_list = {}
+local check_list = {} -- what is this for?
+
+local deprecation_warnings = {}
 
 -- Public interface ------------------------------------------------------
 
---- hs.mouse.getAbsolutePosition() -> point
---- Function
---- Gets the absolute co-ordinates of the mouse pointer
----
---- Parameters:
----  * None
----
---- Returns:
----  * A point-table containing the absolute x and y co-ordinates of the mouse pointer
----
---- Notes:
----  * The co-ordinates returned by this function are in relation to the full size of your desktop. If you have multiple monitors, the desktop is a large virtual rectangle that contains them all (e.g. if you have two 1920x1080 monitors and the mouse is in the middle of the second monitor, the returned table would be `{ x=2879, y=540 }`)
----  * Multiple monitors of different sizes can cause the co-ordinates of some areas of the desktop to be negative. This is perfectly normal. 0,0 in the co-ordinates of the desktop is the top left of the primary monitor
-function module.getAbsolutePosition()
-    return module.get()
+function module.get(...)
+    local state = debug.getinfo(2)
+    local tag = state.short_src..":"..state.currentline
+    if not deprecation_warnings[tag] then
+        print(tag..": hs.mouse.get is deprecated.  Please update your code to use hs.mouse.getAbsolutePosition or hs.mouse.getRelativePosition")
+        deprecation_warnings[tag] = true
+    end
+    return module.getAbsolutePosition(...)
 end
 
---- hs.mouse.setAbsolutePosition(point)
---- Function
---- Sets the absolute co-ordinates of the mouse pointer
----
---- Parameters:
----  * point - A point-table containing the absolute x and y co-ordinates to move the mouse pointer to
----
---- Returns:
----  * None
----
---- Notes:
----  * The co-ordinates given to this function must be in relation to the full size of your desktop. See the notes for `hs.mouse.get` for more information
-function module.setAbsolutePosition(point)
-    return module.set(point)
+function module.set(...)
+    local state = debug.getinfo(2)
+    local tag = state.short_src..":"..state.currentline
+    if not deprecation_warnings[tag] then
+        print(tag..": hs.mouse.set is deprecated.  Please update your code to use hs.mouse.setAbsolutePosition or hs.mouse.setRelativePosition")
+        deprecation_warnings[tag] = true
+    end
+    return module.setAbsolutePosition(...)
 end
 
 --- hs.mouse.getRelativePosition() -> point or nil
@@ -101,7 +90,7 @@ function module.setRelativePosition(point, screen)
     abs["x"] = frame["x"] + point["x"]
     abs["y"] = frame["y"] + point["y"]
 
-    return module.set(abs)
+    return module.setAbsolutePosition(abs)
 end
 
 --- hs.mouse.getCurrentScreen() -> screen or nil
@@ -114,8 +103,26 @@ end
 --- Returns:
 ---  * An `hs.screen` object that the mouse pointer is on, or nil if an error occurred
 function module.getCurrentScreen()
-    local point = module.get()
+    local point = module.getAbsolutePosition()
     return fnutils.find(screen.allScreens(), function(aScreen) return geometry.isPointInRect(point, aScreen:fullFrame()) end)
+end
+
+--- hs.mouse.getButtons() -> table
+--- Function
+--- Returns a table containing the current mouse buttons being pressed *at this instant*.
+---
+--- Parameters:
+---  None
+---
+--- Returns:
+---  * Returns an array containing indicies starting from 1 up to the highest numbered button currently being pressed where the index is `true` if the button is currently pressed or `false` if it is not.
+---  * Special hash tag synonyms for `left` (button 1), `right` (button 2), and `middle` (button 3) are also set to true if these buttons are currently being pressed.
+---
+--- Notes:
+---  * This function is a wrapper to `hs.eventtap.checkMouseButtons`
+---  * This is an instantaneous poll of the current mouse buttons, not a callback.
+function module.getButtons()
+    return require("hs.eventtap").checkMouseButtons()
 end
 
 -- Return Module Object --------------------------------------------------
