372a0f8108e92ca3400e029b6b32eac362593b28
Chris Jones <cmsj@tenshu.net>
2015-08-06 12:49:07 +0200

    Remove a useless xcode build script. Enforce set -eu safety in all xcode build scripts. Add support for Lua-only modules to the Xcode script for installing modules. Refactor build_extensions.sh to entirely skip migrated modules

--- extensions/build_extensions.sh
+++ extensions/build_extensions.sh
@@ -41,66 +41,69 @@ fi
 mkdir -p "${T}"
 
 for dir in $(find . -type d -mindepth 1 -maxdepth 1 ! -name '.build') ; do
+    MIGRATED=0
     dir=$(basename "$dir")
-
-    # Reset variables that may have been set by a previous iteration's build_vars.sh
-    unset EXTRA_CFLAGS DEBUG_CFLAGS EXTRA_LDFLAGS
-
-    # Check if this module has a Makefile already
-    if [ ! -e "${dir}/Makefile" ]; then
-        cp build_extensions.Makefile "${dir}/Makefile"
-        LASTMAKEFILE="${dir}/Makefile"
-    fi
-
-    # Check if this module is Lua-only
-    if [ -e "${dir}/internal.m" ]; then
-        LUAONLY=0
-    else
-        LUAONLY=1
-    fi
 
     # Check if this module has been migrated to Xcode building
     case "${dir}" in
-        "alert"|"applescript"|"application"|"audiodevice"|"base64"|"battery"|"brightness"|"caffeinate")
-            LUAONLY=1
+        "alert"|"appfinder"|"applescript"|"application"|"audiodevice"|"base64"|"battery"|"brightness"|"caffeinate")
+            MIGRATED=1
             ;;
     esac
 
-    # Set environment variables
-    export PREFIX="${T}"
-    if [ "${CONFIGURATION:-''}" != "Debug" ]; then
-        export DEBUG_CFLAGS=""
-    fi
+    if [ "${MIGRATED}" == "0" ]; then
+        # Reset variables that may have been set by a previous iteration's build_vars.sh
+        unset EXTRA_CFLAGS DEBUG_CFLAGS EXTRA_LDFLAGS
 
-    # Import any environment variables the module wants to set
-    if [ -e "${dir}/build_vars.sh" ]; then
-        . "${dir}/build_vars.sh"
-    fi
-
-    # Do the build
-    pushd "${dir}"
-
-    if [ "${LUAONLY}" == "1" ]; then
-        make install-lua
-    else
-        make install
-        if [ -n "${ISXCODE}" ]; then
-            for dsymfile in *.dSYM ; do
-                DSYM_DEST="${BUILT_PRODUCTS_DIR}/${dir}-${dsymfile}"
-                if [ -d "${DSYM_DEST}" ]; then
-                    rm -rf "${DSYM_DEST}"
-                fi
-                cp -a "$dsymfile" "${DSYM_DEST}"
-            done
+        # Check if this module has a Makefile already
+        if [ ! -e "${dir}/Makefile" ]; then
+            cp build_extensions.Makefile "${dir}/Makefile"
+            LASTMAKEFILE="${dir}/Makefile"
         fi
-        make clean
-    fi
 
-    popd
+        # Check if this module is Lua-only
+        if [ -e "${dir}/internal.m" ]; then
+            LUAONLY=0
+        else
+            LUAONLY=1
+        fi
 
-    if [ "${LASTMAKEFILE}" != "" ]; then
-        rm "${LASTMAKEFILE}"
-        LASTMAKEFILE=""
+        # Set environment variables
+        export PREFIX="${T}"
+        if [ "${CONFIGURATION:-''}" != "Debug" ]; then
+            export DEBUG_CFLAGS=""
+        fi
+
+        # Import any environment variables the module wants to set
+        if [ -e "${dir}/build_vars.sh" ]; then
+            . "${dir}/build_vars.sh"
+        fi
+
+        # Do the build
+        pushd "${dir}"
+
+        if [ "${LUAONLY}" == "1" ]; then
+            make install-lua
+        else
+            make install
+            if [ -n "${ISXCODE}" ]; then
+                for dsymfile in *.dSYM ; do
+                    DSYM_DEST="${BUILT_PRODUCTS_DIR}/${dir}-${dsymfile}"
+                    if [ -d "${DSYM_DEST}" ]; then
+                        rm -rf "${DSYM_DEST}"
+                    fi
+                    cp -a "$dsymfile" "${DSYM_DEST}"
+                done
+            fi
+            make clean
+        fi
+
+        popd
+
+        if [ "${LASTMAKEFILE}" != "" ]; then
+            rm "${LASTMAKEFILE}"
+            LASTMAKEFILE=""
+        fi
     fi
 done
 
