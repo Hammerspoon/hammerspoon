2aea5744233e41d20aaec1f6c3c1d505151dbb54
Mark Lowne <marklowne@gmail.com>
2015-08-13 15:18:01 +0200

    add hs.func iterators

--- extensions/func/init.lua
+++ extensions/func/init.lua
@@ -17,6 +17,7 @@
 local getmetatable,setmetatable=getmetatable,setmetatable
 local pairs,ipairs,next,type=pairs,ipairs,next,type
 local tpack,tunpack=table.pack,table.unpack
+local tostring=tostring
 
 local f={} -- module/class
 
@@ -36,12 +37,14 @@ local f={} -- module/class
 ---    that hs.func methods don't shadow your table object methods
 ---  * for the same reason, if you need to set the metatable to an already existing hs.func table, you can use
 ---    the `hs.func:setmetatable()` method, it will *insert* the new metatable at the top of the chain
-f._isFuncTable = true
+f._isFuncCollection = true
 local function new(t)
   if not t then t={} end
   local mt,nt=t,t
   while mt do
-    if mt._isFuncTable then return t end
+    if mt._isFuncCollection then return t end
+    -- not checking __index to see that it actually points to the metatable;
+    -- let's assume it does in almost every case; if it doesn't, issues will be filed
     nt,mt=mt,getmetatable(mt)
   end
   setmetatable(nt,{__index=f}) -- at the end of the metatable chain
@@ -49,6 +52,20 @@ local function new(t)
 end
 f.collection=new
 
+--- hs.func:setmetatable(metatable) -> collection
+--- Method
+--- Sets a custom metatable on a hs.func collection
+---
+--- Parameters:
+---  * metatable - the metatable
+---
+--- Returns:
+---  * this collection, with the specified metatable inserted at the top of the chain
+function f.setmetatable(t,mt)
+  setmetatable(t,mt)
+  t=new(t)
+  return t
+end
 
 --- hs.func:flatten() -> list
 --- Method
@@ -139,10 +156,9 @@ end
 ---  * a map containing the results of calling the function on every element in this map
 ---
 --- Notes:
----  * you must use `hs.func:imap()` if this table is a list (without holes) and you need guaranteed in-order
----  processing
----  * if `fn` doesn't return keys, the transformed values returned by it will be assigned to the respective original keys
----  * if `fn` does returns keys, and they are not unique, the previous element with the same key will be overwritten;
+---  * if this table is a list (without holes) and you need guaranteed in-order processing you must use `hs.func:imap()`
+---  * if `fn` doesn't return keys, the transformed values returned by it will be assigned to their respective original keys
+---  * if `fn` *does* return keys, and they are not unique, the previous element with the same key will be overwritten;
 ---    keep in mind that the iteration order, and therefore which value will ultimately be associated to a
 ---    conflicted key, is arbitrary
 ---  * if `fn` returns `nil`, the respective key in the result map won't have any associated element
@@ -179,39 +195,6 @@ end
 ---  * None
 function f.each(t,fn)
   for k,v in pairs(t) do fn(v,k) end
-end
-
---- hs.func:ipairs(fn)
---- Method
---- Executes a function with side effects across a list in order, discarding any results
----
---- Parameters:
----  * fn - a function that accepts two parameters, a list index and its associated value
----
---- Returns:
----  * None
----
---- Notes:
----  * this method is like `hs.func:ieach`, but `fn` is passed `key,value` instead of `value,key` to mirror
----    the Lua global `ipairs`
-function f.ipairs(t,fn)
-  for k,v in ipairs(t) do fn(k,v) end
-end
---- hs.func:pairs(fn)
---- Method
---- Executes a function with side effects across a map, in arbitrary order, discarding any results
----
---- Parameters:
----  * fn - a function that accepts two parameters, a map key and its associated value
----
---- Returns:
----  * None
----
---- Notes:
----  * this method is like `hs.func:each`, but `fn` is passed `key,value` instead of `value,key` to mirror
----    the Lua global `pairs`
-function f.pairs(t,fn)
-  for k,v in pairs(t) do fn(k,v) end
 end
 
 --- hs.func:ifilter(fn) -> list
@@ -468,7 +451,7 @@ end
 ---  * fn - a function that accepts two parameters, a list element and its index, and returns a boolean
 ---
 --- Returns:
----  * The first element of this list that caused `fn` to return `true`, amd its index; `nil` if not found
+---  * The first element of this list that caused `fn` to return `true`, and its index; `nil` if not found
 function f.ifind(t,fn)
   for k,v in ipairs(t) do if fn(v,k) then return v,k end end
 end
@@ -486,5 +469,329 @@ function f.find(t,fn)
   for k,v in pairs(t) do if fn(v,k) then return v,k end end
 end
 
-return setmetatable(f,{__call=new})
+--- hs.func:cycle() -> function()->value,key
+--- Constructor
+--- Creates a function that repeatedly iterates over a map, in arbitrary order
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * a function that will return the next element (`value,key`) of this map every time it is called, cycling back
+---    to the start after the last element
+---
+--- Notes:
+---  * An example usage:
+---     ```lua
+---     f = hs.func.cycle({a=1, b=2, c=3})
+---     t = {f(), f(), f(), f(), f(), f(), f()} -- {3, 2, 1, 3, 2, 1, 3, 'c'}
+---     ```
+function f.cycle(t)
+  local k,v
+  return function()
+    k,v = next(t,k)
+    if not k then k,v=next(t) end
+    return v,k
+  end
+end
+
+--- hs.func:icycle() -> function()->value,index
+--- Constructor
+--- Creates a function that repeatedly iterates over a list, in order
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * a function that will return the next element (`value,index`) of this list every time it is called, cycling back
+---    to the start after the last element
+---
+--- Notes:
+---  * An example usage:
+---     ```lua
+---     f = hs.func.icycle({4, 5, 6})
+---     t= {f(), f(), f(), f(), f(), f(), f()} -- {4, 5, 6, 4, 5, 6, 4}
+---     ```
+function f.icycle(t)
+  local i = 0
+  return function()
+    i=i+1
+    if i>#t then i=1 end
+    return t[i],i
+  end
+end
+
+--- hs.func:every(fn) -> boolean
+--- Method
+--- Checks if a predicate function is satisfied by every element of a collection
+---
+--- Parameters:
+---  * fn - a function that accepts two parameters, a table value and its key, and returns a boolean
+---
+--- Returns:
+---  * `true` if `fn` returns `true` for every element; `false` otherwise
+function f.every(t,fn)
+  for k,v in pairs(t) do
+    if not fn(v,k) then return false end
+  end
+  return true
+end
+
+--- hs.func:some(fn) -> boolean
+--- Method
+--- Checks if a predicate function is satisfied by at least one element of a collection
+---
+--- Parameters:
+---  * fn - a function that accepts two parameters, a table value and its key, and returns a boolean
+---
+--- Returns:
+---  * `true` if `fn` returns `true` for at least one element; `false` otherwise
+function f.some(t,fn)
+  return f.find(t,fn)~=nil
+end
+
+-- wrappers for table module functions
+--- hs.func.defaultComparator
+--- Variable
+--- A function that takes two values and returns `true` if the first value should come before the second.
+---
+--- It is used in methods that perform sorting and in sorted iterators, when no custom comparator is
+--- passed as argument; you can set this variable to your own comparator function for these cases.
+---
+--- The default implementation simply uses `value1<value2` (less than); if value1 and value2 are different
+--- types, then it does `tostring(value1)<tostring(value2)`.
+f.defaultComparator = function(a,b)
+  if type(a)~=type(b) then return tostring(a)<tostring(b) else return a<b end
+end
+
+local tsort,tinsert,tremove = table.sort,table.insert,table.remove
+--- hs.func:sort([fn, removeDuplicates]) -> list
+--- Method
+--- Sorts a list
+---
+--- Parameters:
+---  * `fn` - a function that accepts two list elements, and returns `true` if the first should come
+---    before the second in the soerted return list; if `nil`, `hs.func.defaultComparator` will be used
+---  * `removeDuplicates` - (optional) a boolean, if `true` the result list won't have any duplicate elements (the
+---    `==` (equality) operator will determine duplicates)
+---
+--- Returns:
+---  * a list with the same elements of this list, sorted according to `fn`
+---
+--- Notes:
+---  * this method is basically a wrapper for `table.sort`; the only difference is that a new table is returned
+---    (in other words, the original list is left untouched)
+function f:sort(fn,removeDuplicates)
+  local nt=removeDuplicates and f.toSet(self):toList() or copy(self)
+  tsort(nt,fn~=nil and fn or f.defaultComparator)
+  return nt
+end
+
+-- the following methods work on both maps and lists
+local floor=math.floor
+local function isListIndex(k)
+  return type(k)=='number' and k>=1 and floor(k)==k -- not using 5.3 syntax (k//1==k, or math.type), as you never know
+end
+
+--- hs.func:insert(value[, key]) -> collection
+--- Method
+--- Inserts an element into a collection
+---
+--- Parameters:
+---  * value - the element to insert
+---  * key - the key for the element:
+---    - if omitted, this is assumed to be a list, and `value` will be appended at the end
+---    - if a positive integer, this is assumed to be a list, and `value` will be inserted in that position; subsequent
+---      elements will be shifted up to make room
+---    - otherwise, this method simply executes `self[key]=value`
+---
+--- Returns:
+---  * this collection, for method chaining
+function f:insert(v,k)
+  if k==nil then k=#self+1 end
+  if isListIndex(k) then tinsert(self,k,v)
+  else self[k]=v end
+  return self
+end
+
+--- hs.func:remove(key) -> collection
+--- Method
+--- Removes the element associated with a given key or index from this collection
+---
+--- Parameters:
+---  * key - the key for the element to remove:
+---    - if a positive integer, this is assumed to be a list; the element in that position will be removed, and subsequent
+---      elements will be shifted down to fill the hole
+---    - otherwise, this method simply executes `self[key]=nil`
+---
+--- Returns:
+---  * this collection, for method chaining
+function f:remove(k)
+  if k==nil then return self end
+  if isListIndex(k) then tremove(self,k)
+  else self[k]=nil end
+  return self
+end
+
+--- hs.func:removeElement(value) -> collection
+--- Method
+--- Removes all occurrences of a given element from this collection
+---
+--- Parameters:
+---  * value - the element to remove
+---
+--- Returns:
+---  * this collection, for method chaining
+function f:removeElement(v)
+  local k=f.key(self,v)
+  while k~=nil do
+    f.remove(self,k)
+    k=f.key(self,v)
+  end
+  return self
+end
+
+
+-- iterators
+
+--- hs.func:values() -> iterator
+--- Constructor
+--- Returns an iterator that returns `value,key` at every iteration, in arbitrary order
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * an iterator function and this collection, meant for "for" loops: `for v,k in my_coll:values() do...`
+function f:values()
+  local k,v
+  return function(t)
+    k,v=next(t,k) return v,k
+  end,self
+end
+
+--- hs.func:keys() -> iterator, collection
+--- Constructor
+--- Returns an iterator that returns `key,value` at every iteration, in arbitrary order
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * an iterator function and this collection, meant for "for" loops: `for k,v in my_coll:keys() do...`
+function f:keys()return next,self end
+
+--- hs.func:pairs() -> iterator
+--- Constructor
+--- Alias for `hs.func:keys()`
+f.pairs=f.keys
+
+--- hs.func:ipairs() -> iterator, collection
+--- Constructor
+--- Returns an iterator that returns `key,value` at every iteration, in order (for a list)
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * an iterator function and this collection, meant for "for" loops: `for k,v in my_coll:ipairs() do...`
+---
+--- Notes:
+---  * for pure lists, this method is equivalent to `hs.func:byKeys()`, but more efficient
+function f:ipairs() return ipairs(self) end
+
+local function sortedpos(t,fn,v)
+  local i=1
+  while t[i] and fn(t[i],v) do i=i+1 end
+  return i
+end
+--- hs.func:byValues([comparator]) -> iterator
+--- Constructor
+--- Returns an iterator that returns `value,key` at every iteration, sorted by values
+---
+--- Parameters:
+---  * comparator - (optional) a comparator function to determine the sorting order
+---
+--- Returns:
+---  * an iterator function and a helper table, meant for "for" loops: `for v,k in my_coll:byValues() do...`
+function f:byValues(fn)
+  if fn==nil then fn=f.defaultComparator end
+  local vl,kl={},{}
+  local k,v=next(self)
+  while k~=nil do
+    local i=sortedpos(vl,fn,v)
+    tinsert(vl,i,v) tinsert(kl,i,k)
+    k,v=next(self,k)
+  end
+  local i=0
+  return function()
+    i=i+1 return vl[i],kl[i]
+  end,vl,0
+end
+
+--- hs.func:keysByValues([comparator]) -> iterator
+--- Constructor
+--- Returns an iterator that returns `key,value` at every iteration, sorted by values
+---
+--- Parameters:
+---  * comparator - (optional) a comparator function to determine the sorting order
+---
+--- Returns:
+---  * an iterator function and a helper table, meant for "for" loops: `for k,v in my_coll:keysByValues() do...`
+function f:keysByValues(fn)
+  if fn==nil then fn=f.defaultComparator end
+  local vl,kl={},{}
+  local k,v=next(self)
+  while k~=nil do
+    local i=sortedpos(vl,fn,v)
+    tinsert(vl,i,v) tinsert(kl,i,k)
+    k,v=next(self,k)
+  end
+  local i=0
+  return function()
+    i=i+1 return kl[i],vl[i]
+  end,vl,0
+end
+
+--- hs.func:byKeys([comparator]) -> iterator, collection
+--- Constructor
+--- Returns an iterator that returns `key,value` at every iteration, sorted by keys
+---
+--- Parameters:
+---  * comparator - (optional) a comparator function to determine the sorting order
+---
+--- Returns:
+---  * an iterator function and this collection, meant for "for" loops: `for k,v in my_coll:byKeys() do...`
+function f:byKeys(fn)
+  local kl=f.toList(self)
+  tsort(kl,fn~=nil and fn or f.defaultComparator)
+  local i=0
+  return function(t)
+    i=i+1
+    local k=kl[i]
+    return k,k and t[k] or nil
+  end,self,0
+end
+
+--- hs.func:valuesByKeys([comparator]) -> iterator, collection
+--- Constructor
+--- Returns an iterator that returns `value,key` at every iteration, sorted by keys
+---
+--- Parameters:
+---  * comparator - (optional) a comparator function to determine the sorting order
+---
+--- Returns:
+---  * an iterator function and this collection, meant for "for" loops: `for v,k in my_coll:valuesByKeys() do...`
+function f:valuesByKeys(fn)
+  local kl=f.toList(self)
+  tsort(kl,fn~=nil and fn or f.defaultComparator)
+  local i=0
+  return function(t)
+    i=i+1
+    local k=kl[i]
+    return k and t[k] or nil,k
+  end,self,0
+end
+
+return setmetatable(f,{__call=function(_,t)return new(t)end})
 
