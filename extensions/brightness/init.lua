6224706e01022ac7a9505aeb455c962a7e8bca52
Mark Lowne <marklowne@gmail.com>
2015-12-16 16:22:56 +0100

    add hs.brightness.DDC* documentation

--- extensions/brightness/init.lua
+++ extensions/brightness/init.lua
@@ -2,9 +2,16 @@
 ---
 --- Inspect/manipulate display brightness
 ---
---- Home: https://github.com/asmagill/mjolnir_asm.sys
+--- The functions `hs.brightness.set()` and `hs.brightness.get()` should work on Apple-made screens (including
+--- laptop screens).
 ---
---- This module is based primarily on code from the previous incarnation of Mjolnir by [Steven Degutis](https://github.com/sdegutis/).
+--- If you have external non-Apple screens, you can use the `hs.brightness.DDCset()` and `hs.brightness.DDCget()`
+--- functions; these (along with `hs.brightness.DDCauto()`) require an external utility called `ddcctl`
+--- (https://github.com/kfix/ddcctl).
+--- **WARNING:** before trying to use the `DDC\*` functions, make sure you understand the caveats listed in the
+--- ddcctl project page; namely: **your Mac might crash and/or your screen might freeze or just not work with ddcctl**.
+--- Make sure that your screens work properly with ddcctl *before* attempting to use the `DDC\*` functions in this module.
+
 local screen=require'hs.screen'
 local timer=require'hs.timer'
 local windowfilter=require'hs.window.filter'
@@ -16,13 +23,30 @@ local log=require'hs.logger'.new('brightness')
 bri.setLogLevel=log.setLogLevel
 local type,next,ipairs,pairs,tonumber,min,max,floor,abs=type,next,ipairs,pairs,tonumber,math.min,math.max,math.floor,math.abs
 local tinsert,tsort,sformat,sfind,ssub,exec=table.insert,table.sort,string.format,string.find,string.sub,hs.execute
--- private variables and methods -----------------------------------------
 
--- Public interface ------------------------------------------------------
-
-bri.ddcctl=nil--'/usr/local/bin/ddcctl'
+--- hs.brightness.ddcctl
+--- Variable
+--- In order to use the `hs.brightness.DDC\*` functions, set this to the full path of the `ddcctl` utility (e.g. "/usr/local/bin/ddcctl")
+bri.ddcctl=nil
+--- hs.brightness.DDCautoSmoothness
+--- Variable
+--- A number from 0 to 0.95 indicating the smoothness for brightness/contrast changes by `hs.brighntess.DDCauto()`;
+--- if 0, changes will be abrupt; default value is 0.8
 bri.DDCautoSmoothness=0.8
+--- hs.brightness.DDCautoDisableWindowfilter
+--- Variable
+--- An `hs.window.filter` object that will disable automatic brightness/contrast control - i.e., set brightness and contrast
+--- to the maximum values provided - whenever any window is allowed; alternatively, you can set this to a list of application
+--- names (typically media apps and/or apps for color-sensitive work) and a windowfilter will be created
+--- for you that disables brightness/contrast control whenever one of these apps is focused
 bri.DDCautoDisableWindowfilter=nil
+--- hs.brightness.DDCautoInvertBelowLux
+--- Variable
+--- A number indicating the ambient light intensity in lux (as returned by `hs.brightness.ambient()`) below which
+--- `hs.brightness.DDCauto()` will apply color inversion (on all screens) via `hs.redshift`; default value is 0 (disabled)
+---
+--- Notes:
+---  * color inversion requires `hs.redshift` to be started
 bri.DDCautoInvertBelowLux=0
 
 local screenCache={}
@@ -47,6 +71,16 @@ local function ddcctl(scr,args)
   if sfind(s,'DDC send command failed!',1,true) then log.ef('ddcctl error: %s',s) return end
   return s
 end
+
+--- hs.brightness.DDCget(screen) -> number, number
+--- Function
+--- Returns the current brightness and contrast of the given external screen
+---
+--- Parameters:
+---  * screen - an `hs.screen` object, or argument for `hs.screen.find()`, indicating which screen to query
+---
+--- Returns
+---  * the current brightness and contrast of the screen, or nil if an error occurred
 function bri.DDCget(scr)
   local s=ddcctl(scr,'-b \\? -c \\?')
   if not s then return end
@@ -65,6 +99,17 @@ function bri.DDCget(scr)
   return bri,con
 end
 
+--- hs.brightness.DDCset(screen,brightness,contrast) -> boolean
+--- Function
+--- Sets the brightness and/or contrast of the given external screen
+---
+--- Parameters:
+---  * screen - an `hs.screen` object, or argument for `hs.screen.find()`, indicating which screen to affect
+---  * brightness - a number between 0 and 100 indicating the desired brightness; if nil, brightness won't be changed
+---  * contrast - a number between 0 and 100 indicating the desired contrast; if nil, contrast won't be changed
+---
+--- Returns
+---  * true if the operation succeeded, false otherwise
 function bri.DDCset(scr,bri,con)
   if type(bri)~='number' or bri<0 or bri>100 then bri=nil end
   if type(con)~='number' or con<0 or con>100 then con=nil end
@@ -84,6 +129,7 @@ local function getValuesForLux(lux,args)
     local v2=args[i]
     if lux<=v2.lux then
       local v1=args[i-1]
+      --TODO this probably needs to be logarithmic
       local p=ilerp(lux,v1.lux,v2.lux)
       return lerpr(p,v1.bri,v2.bri),lerpr(p,v1.con,v2.con)
     end
@@ -116,13 +162,11 @@ local function getAmbient()
       end
     end
   end
-  --  print(actualLux^0.5,abs(lux-actualLux))
-  --  print(max(0,20*actualLux^0.5/(1+(abs(lux-actualLux)))))
   local nextt=max(1,min(30,20*(bri.DDCautoSmoothness+0.1)*actualLux^0.5/(1+(abs(lux-actualLux)))))
   log.vf('next readout in %.2fs',nextt)
   tmr:start(nextt)
 end
---function bri.force(lux) forcedLux=lux getAmbient()end
+
 tmr=timer.delayed.new(30,getAmbient)
 local wfDisable,modulewfDisable
 local function pause() forcedLux=100000 getAmbient() end
@@ -144,8 +188,8 @@ local function getScreens()
       local id=s:id()
       args.screen=s
       args.screenName=s:name()
-      args.lastBri=settings.get(SETTING_DDCSET..id..'.bri') or 50--floor((args.minBri+args.maxBri)/2)
-      args.lastCon=settings.get(SETTING_DDCSET..id..'.con') or 50--floor((args.minCon+args.maxCon)/2)
+      args.lastBri=settings.get(SETTING_DDCSET..id..'.bri') or floor((minv.bri+maxv.bri)/2)
+      args.lastCon=settings.get(SETTING_DDCSET..id..'.con') or floor((minv.con+maxv.con)/2)
       t[id]=args
       if not autoScreens[id] then log.f('autobrightness started for screen %s',args.screenName or '???') end
       autoScreens[id]=nil
@@ -185,15 +229,50 @@ local function getScreens()
   end
 end
 local tmrScreens=timer.delayed.new(8,getScreens)
+
+--- hs.brightness.DDCauto(screen,values)
+--- Function
+--- Starts or stops automatic control of brightness/contrast for the given external screen
+---
+--- Parameters:
+---  * screen - an `hs.screen` object, or argument for `hs.screen.find()`, indicating which screen to affect
+---  * values - if false or nil, stops automatic control for the screen; otherwise it must be a list of
+---    (at least two) entries; each entry must be a table with the following keys/values:
+---    * lux - ambient light intensity in lux (0 to 10000) (as returned by `hs.brightness.ambient()`)
+---    * bri - desired brightness value (0 to 100) at the given lux
+---    * con - desired contrast value (0 to 100) at the given lux
+---
+--- Returns:
+---  * None
+---
+--- Notes:
+---  * Automatic brightness/contrast control requires a functional ambient light sensor; make sure that
+---    `hs.brightness.ambient()` returns correct values before using this function
+---  * You don't need to worry about whether the screen is currently connected when calling this function;
+---    automatic control will be started/stopped as needed as the screen is connected/disconnected
+---  * When automatic control is started for at least one screen, this module will query the ambient
+---    light sensor periodically, and apply appropriate brightness/contrast to the screen by interpolating
+---    among the entries in `values`
+---
+--- Usage:
+--- ```
+--- hs.brightness.ddcctl = '/path/to/ddcctl'
+--- hs.brightness.DDCautoInvertBelowLux = 10
+--- hs.brightness.DDCautoDisableWindowfilter = {'VLC','Plex'}
+--- hs.brightness.DDCauto('phl',{{lux=1,bri=10,con=20},{lux=14,bri=20,con=20},{lux=120,bri=70,con=40},{lux=200,bri=100,con=50}})
+--- hs.brightness.DDCauto('dell',{{lux=1,bri=10,con=40},{lux=14,bri=20,con=40},{lux=120,bri=70,con=70},{lux=200,bri=100,con=75}})
+--- ```
 function bri.DDCauto(scr,args)
   local _=screen.find(scr) -- check argument
   if args then
-    if type(args)~='table' then error('settings must be a table',2) end
+    if type(args)~='table' or #args<2 then error('values must be a list of at least two entries',2) end
     log.f('DDCauto enabled for %s',scr)
     for _,values in ipairs(args) do
-      if type(values)~='table' or not values.bri or not values.con then error('invalid entry in settings',2) end
-      if type(values.lux)~='number' or values.lux<0 or values.lux>10000 then error('invalid lux value in settings',2) end
-      log.df('@ %dlux: bri:%d, con:%d%s',values.lux,values.bri,values.con,values.invert and ', invert' or '')
+      if type(values)~='table' or not values.bri or not values.con then error('invalid entry in values',2) end
+      if type(values.lux)~='number' or values.lux<0 or values.lux>10000 then error('invalid lux value in values',2) end
+      if type(values.bri)~='number' or values.bri<0 or values.bri>100 then error('invalid bri value in values',2) end
+      if type(values.con)~='number' or values.con<0 or values.con>100 then error('invalid con value in values',2) end
+      log.df('@ %dlux: bri:%d, con:%d',values.lux,values.bri,values.con)
     end
   else log.i('DDCauto disabled for %s',scr) end
   autoScreensRequested[scr]=args or nil
@@ -204,22 +283,5 @@ function bri.DDCauto(scr,args)
     if screenWatcher then screenWatcher:stop() screenWatcher=nil end
   end
 end
-function bri.oldDDCauto(scr,minLux,minBri,minCon,maxLux,maxBri,maxCon)--,customArg,noRead)
-  if minLux then log.f('DDCauto enabled for %s: %d/%d @ %dlux, %d/%d at %dlux',scr,minBri,minCon,minLux,maxBri,maxCon,maxLux)
-  else log.f('DDCauto disabled for %s',scr) end
-  autoScreensRequested[scr]=minLux and
-    {minLux=minLux,minBri=minBri,minCon=minCon,maxLux=maxLux,maxBri=maxBri,maxCon=maxCon,} or nil
-  --      lastBri=floor((minBri+maxBri)/2),lastCon=floor((minCon+maxCon)/2)} or nil
-  getScreens()
-  if next(autoScreensRequested) then
-    if not screenWatcher then screenWatcher=screen.watcher.new(function()tmr:stop()tmrScreens:start()end):start() end
-  else
-    if screenWatcher then screenWatcher:stop() screenWatcher=nil end
-  end
-end
--- Return Module Object --------------------------------------------------
 
 return bri
-
-
-
