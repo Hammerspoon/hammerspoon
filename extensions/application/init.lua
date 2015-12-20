799d10587bed5b25971c31351eba897bd33bbc3c
Mark Lowne <marklowne@gmail.com>
2015-12-20 12:49:34 +0100

    implement lazy loading for window submodules

--- extensions/application/init.lua
+++ extensions/application/init.lua
@@ -188,8 +188,9 @@ end
 
 do
   local mt=getmetatable(application)
-  if not mt.__call then mt.__call=function(t,...)if t.find then return t.find(...) else error('cannot call uielement',2) end end end
+  -- whoever gets it first (window vs application)
+  if not mt.__call then mt.__call=function(t,...) return t.find(...) end end
 end
---getmetatable(application).__call=function(_,...)return application.find(...)end
+
 return application
 
