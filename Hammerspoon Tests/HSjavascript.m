6667c1807311919996eaafb3e2f933f133ba014b
Michael Bujol <bujolm@gmail.com>
2016-02-05 17:38:02 -0600

    add some tests for hs.javascript

---
+++ Hammerspoon Tests/HSjavascript.m
@@ -0,0 +1,50 @@
+//
+//  HSjavascript.m
+//
+//  Created by Michael Bujol on 01/04/2016.
+//
+
+#import "HSTestcase.h"
+#pragma GCC diagnostic ignored "-Wgnu-statement-expression"
+
+@interface HSjavascript : HSTestCase
+
+@end
+
+@implementation HSjavascript
+
+- (void)setUp {
+    [super setUpWithRequire:@"test_javascript"];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testAddition {
+    RUN_LUA_TEST()
+}
+
+- (void)testDestructuring {
+    RUN_LUA_TEST()
+}
+
+- (void)testString {
+    RUN_LUA_TEST()
+}
+
+- (void)testJsonStringify {
+    RUN_LUA_TEST()
+}
+
+// - (void)testJsonParse {
+//     RUN_LUA_TEST()
+// }
+
+- (void)testJsonParseError {
+    RUN_LUA_TEST()
+}
+
+@end
---
+++ extensions/javascript/test_javascript.lua
@@ -0,0 +1,70 @@
+function testAddition()
+  local js = "2+2"
+  local status, result = hs.javascript(js)
+  assertIsEqual(status, true)
+  assertIsEqual(result, 4)
+  return success()
+end
+
+function testDestructuring()
+  local js = [[
+    var obj = { cat: 1, dog: 2 };
+    var { cat, dog } = obj;
+    cat + dog;
+]]
+  local status, result = hs.javascript(js)
+  assertIsEqual(status, true)
+  assertIsEqual(result, 3)
+  return success()
+end
+
+function testString()
+  local js = [[
+    var str1 = "Hello", str2 = "World";
+    str1 + ", " + str2 + "!";
+]]
+  local status, result = hs.javascript(js)
+  assertIsEqual(status, true)
+  assertIsEqual(result, "Hello, World!")
+  return success()
+end
+
+function testJsonStringify()
+  local js = [[
+    var obj = {
+      a: 1, 
+      "b": "two", 
+      "c": true
+    };
+    json = JSON.stringify(obj);
+    json;
+]]
+  local status, result = hs.javascript(js)
+  assertIsEqual(status, true)
+  assertIsEqual(result, '{\\"a\\":1,\\"b\\":\\"two\\",\\"c\\":true}')
+  return success()
+end
+
+-- function testJsonParse()
+--   local js = [[
+--     var json = '{"a":1, "b": "two", "c": true}';
+--     obj = JSON.parse(json);
+--     obj;
+-- ]]
+--   local status, result = hs.javascript(js)
+--   assertIsEqual(status, true)
+--   assertIsEqual(result, ) -- need to parse NSAppleEventDescriptor nonsense 
+--   return success()
+-- end
+
+function testJsonParseError()
+  local js = [[
+    var json = '{"a":1, "b": "two", c: true}';
+    obj = JSON.parse(json);
+    obj;
+]]
+  local status, result = hs.javascript(js)
+  assertIsEqual(status, false)
+  assertIsEqual(result.OSAScriptErrorBriefMessageKey, "Error on line 2: SyntaxError: JSON Parse error: Property name must be a string literal")
+  return success()
+end
