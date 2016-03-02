37cfa5983487c377fd252057ec66783bd1dd0f53
Chris Jones <cmsj@tenshu.net>
2016-03-02 17:33:46 +0000

    Fix my blundering on @heptal's commit

--- extensions/fs/test_fs.lua
+++ extensions/fs/test_fs.lua
@@ -1,6 +1,6 @@
 -- hs.fs tests
 
-testDir = "/tmp/hs_fs_test_dir/"
+testDir = "/private/tmp/hs_fs_test_dir/"
 
 function writeFile(filename, contents)
   io.open(filename, "w"):write(contents):close()
