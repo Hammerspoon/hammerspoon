a6934ad1a6016023c42d4248bff3a26036743f2d
Chris Jones <cmsj@tenshu.net>
2016-04-24 15:42:53 +0100

    Add basic testing for hs.distributednotifications

---
+++ Hammerspoon Tests/HSdistributednotifications.m
@@ -0,0 +1,31 @@
+//
+//  HSdistributednotifications.m
+//  Hammerspoon
+//
+//  Created by Chris Jones on 24/04/2016.
+//  Copyright © 2016 Hammerspoon. All rights reserved.
+//
+
+#import "HSTestCase.h"
+
+@interface HSdistributednotifications : HSTestCase
+
+@end
+
+@implementation HSdistributednotifications
+
+- (void)setUp {
+    [super setUpWithRequire:@"test_distributednotifications"];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testdistributednotifications {
+    XCTAssertTrue([self luaTestWithCheckAndTimeOut:5 setupCode:@"testDistributedNotifications()" checkCode:@"testDistNotValueCheck()"], @"hs.distributednotifications test failed");
+}
+
+@end
