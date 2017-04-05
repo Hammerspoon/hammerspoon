9852447f8820befaacdc613bbca399603e883fdf
Chris Hocking <chris@latenitefilms.com>
2017-04-05 10:22:53 +1000

    Added Execute AppleScript Command

---
+++ Hammerspoon/AppleScript/executeAppleScript.h
@@ -0,0 +1,13 @@
+//
+//  executeAppleScript.h
+//  Hammerspoon
+//
+//  Created by Chris Hocking on 5/4/17.
+//  Copyright © 2017 Hammerspoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface executeAppleScript : NSScriptCommand
+
+@end
