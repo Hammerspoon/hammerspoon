fdcb64e9533c09fc4b697c217e800d12ea47fad4
Chris Hocking <latenitefilms@users.noreply.github.com>
2017-09-03 11:04:18 +1000

    #1324
    - Work in progress

--- Hammerspoon/HSAppleScript.h
+++ Hammerspoon/HSAppleScript.h
@@ -9,6 +9,11 @@
 #import <Foundation/Foundation.h>
 
 //
+// Run String:
+//
+//NSString* HSAppleScriptRunString(NSString* command);
+
+//
 // Enable & Disable AppleScript Support:
 //
 BOOL HSAppleScriptEnabled(void);
--- Hammerspoon/HSAppleScript.m
+++ Hammerspoon/HSAppleScript.m
@@ -14,6 +14,57 @@
 #import "MJDockIcon.h"
 
 //
+// Run String:
+//
+static LuaSkin* MJLuaState;
+static int evalfn;
+int asRefTable;
+
+static int applescript_cleanUTF8(lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    [skin checkArgs:LS_TANY, LS_TBREAK] ;
+    [skin pushNSObject:[skin getValidUTF8AtIndex:1]] ;
+    return 1 ;
+}
+
+NSString* HSAppleScriptRunString(NSString* command) {
+    lua_State* L = MJLuaState.L;
+    
+    [MJLuaState pushLuaRef:asRefTable ref:evalfn];
+    if (!lua_isfunction(L, -1)) {
+        HSNSLOG(@"ERROR: MJLuaRunString doesn't seem to have an evalfn");
+        if (lua_isstring(L, -1)) {
+            HSNSLOG(@"evalfn appears to be a string: %s", lua_tostring(L, -1));
+        }
+        return @"";
+    }
+    lua_pushstring(L, [command UTF8String]);
+    if ([MJLuaState protectedCallAndTraceback:1 nresults:1] == NO) {
+        const char *errorMsg = lua_tostring(L, -1);
+        [MJLuaState logError:[NSString stringWithUTF8String:errorMsg]];
+    }
+    
+    size_t len;
+    const char* s = lua_tolstring(L, -1, &len);
+    NSString* str = [[NSString alloc] initWithData:[NSData dataWithBytes:s length:len] encoding:NSUTF8StringEncoding];
+    if (str == nil) {
+        lua_pushcfunction(L, applescript_cleanUTF8) ;
+        lua_pushvalue(L, -2) ;
+        if (lua_pcall(L, 1, 1, 0) == LUA_OK) {
+            s = lua_tolstring(L, -1, &len);
+            str = [[NSString alloc] initWithData:[NSData dataWithBytes:s length:len] encoding:NSUTF8StringEncoding];
+            lua_pop(L, 1) ;
+        } else {
+            str = [[NSString alloc] initWithFormat:@"-- unable to clean for utf8 output: %s", lua_tostring(L, -1)] ;
+            lua_pop(L, 1) ;
+        }
+    }
+    lua_pop(L, 1);
+    
+    return str;
+}
+
+//
 // Error Message:
 //
 NSString *appleScriptErrorMessage = @"Hammerspoon's AppleScript support is currently disabled. Please enable it in Hammerspoon by using the hs.allowAppleScript(true) command.";
@@ -51,7 +102,7 @@ void HSAppleScriptSetEnabled(BOOL enabled) {
 
     if (HSAppleScriptEnabled()) {
         // Execute Lua Code:
-        return MJLuaRunString(stringToExecute);
+        return HSAppleScriptRunString(stringToExecute);
     } else {
         // Raise Error:
         [self setScriptErrorNumber:-50];
