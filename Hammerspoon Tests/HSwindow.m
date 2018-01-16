90ec88b46f9d9a299b649b785c785af404e9f7bf
Chris Jones <cmsj@tenshu.net>
2018-01-16 22:49:34 +0000

    Add some tests for hs.window

---
+++ Hammerspoon Tests/HSwindow.m
@@ -0,0 +1,38 @@
+//
+//  HSwindow.m
+//  Hammerspoon Tests
+//
+//  Created by Chris Jones on 15/01/2018.
+//  Copyright © 2018 Hammerspoon. All rights reserved.
+//
+
+#import "HSTestCase.h"
+
+@interface HSwindow : HSTestCase
+
+@end
+
+@implementation HSwindow
+
+- (void)setUp {
+    [super setUpWithRequire:@"test_window"];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testAllWindows {
+    RUN_LUA_TEST()
+}
+
+- (void)testDesktop {
+    RUN_LUA_TEST()
+}
+
+- (void)testOrderedWindows {
+    RUN_LUA_TEST()
+}
+@end
