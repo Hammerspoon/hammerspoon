6754e4beb28f9b5a90b96c706ef3c4abd9a61b7e
Chris Jones <cmsj@tenshu.net>
2018-02-14 09:22:13 +0000

    More tweaks to Crashlytics logging, since it seemingly still isn't working in 0.9.60

--- Hammerspoon/HSLogger.h
+++ Hammerspoon/HSLogger.h
@@ -8,18 +8,25 @@
 
 #import <LuaSkin/LuaSkin.h>
 #import <Crashlytics/Crashlytics.h>
+#import "secrets.h"
 
 #ifdef CRASHLYTICS_API_KEY
+#   pragma message "BUILD NOTE: Crashlytics API key available"
 #   ifdef CLS_LOG
+#       pragma message "BUILD NOTE: CLS_LOG defined"
 #       define HSNSLOG(__FORMAT__, ...) CLSNSLog(__FORMAT__, ##__VA_ARGS__)
+#       pragma message "BUILD NOTE: HSNSLOG DEFINED AS CLSNSLog()"
+#   else
+#       pragma message "BUILD NOTE: CLS_LOG undefined"
 #   endif
+#else
+#   pragma message "BUILD NOTE: Crashlytics API key unavailable"
 #endif
 
 #ifndef HSNSLOG
 #   define HSNSLOG(__FORMAT__, ...) NSLog(__FORMAT__, ##__VA_ARGS__)
+#   pragma message "BUILD NOTE: HSNSLOG DEFINED AS NSLog()"
 #endif
-
-#define HSLOG HSNSLOG
 
 @interface HSLogger : NSObject <LuaSkinDelegate> {
     lua_State *_L;
