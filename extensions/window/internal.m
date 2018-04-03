Hammerspoon/hammerspoon

commit bf75146c471f82f33031301215bf64775bb304e1

Remove dedicated functions for converting to/from NSPoint/NSSize objects in hs.mouse and hs.window, using LuaSkin's methods instead

extensions/window/internal.m
@@ -12,35 +12,6 @@
 #define kCGSDebugOptionNoShadows 16384
 void CGSSetDebugOptions(int);
 
-
-static NSSize geom_tosize(lua_State* L, int idx) {
-    luaL_checktype(L, idx, LUA_TTABLE);
-    CGFloat w = (lua_getfield(L, idx, "w"), luaL_checknumber(L, -1));
-    CGFloat h = (lua_getfield(L, idx, "h"), luaL_checknumber(L, -1));
-    lua_pop(L, 2);
-    return NSMakeSize(w, h);
-}
-
-static NSPoint geom_topoint(lua_State* L, int idx) {
-    luaL_checktype(L, idx, LUA_TTABLE);
-    CGFloat x = (lua_getfield(L, idx, "x"), luaL_checknumber(L, -1));
-    CGFloat y = (lua_getfield(L, idx, "y"), luaL_checknumber(L, -1));
-    lua_pop(L, 2);
-    return NSMakePoint(x, y);
-}
-
-static void geom_pushsize(lua_State* L, NSSize size) {
-    lua_newtable(L);
-    lua_pushnumber(L, size.width);  lua_setfield(L, -2, "w");
-    lua_pushnumber(L, size.height); lua_setfield(L, -2, "h");
-}
-
-static void geom_pushpoint(lua_State* L, NSPoint point) {
-    lua_newtable(L);
-    lua_pushnumber(L, point.x); lua_setfield(L, -2, "x");
-    lua_pushnumber(L, point.y); lua_setfield(L, -2, "y");
-}
-
 static NSPoint get_window_topleft(AXUIElementRef win) {
     CFTypeRef positionStorage;
     AXError result = AXUIElementCopyAttributeValue(win, (CFStringRef)NSAccessibilityPositionAttribute, &positionStorage);
@@ -297,9 +268,11 @@ static int window_isstandard(lua_State* L) {
 /// Returns:
 ///  * A point-table containing the absolute co-ordinates of the top left corner of the window
 static int window__topleft(lua_State* L) {
+    LuaSkin *skin = [LuaSkin shared];
+    [skin checkArgs:LS_TBREAK];
+
     AXUIElementRef win = get_window_arg(L, 1);
-    CGPoint topLeft = get_window_topleft(win);
-    geom_pushpoint(L, topLeft);
+    [skin pushNSPoint:get_window_topleft(win)];
     return 1;
 }
 
@@ -313,9 +286,11 @@ static int window__topleft(lua_State* L) {
 /// Returns:
 ///  * A size-table containing the width and height of the window
 static int window__size(lua_State* L) {
+    LuaSkin *skin = [LuaSkin shared];
+    [skin checkArgs:LS_TBREAK];
+
     AXUIElementRef win = get_window_arg(L, 1);
-    CGSize size = get_window_size(win);
-    geom_pushsize(L, size);
+    [skin pushNSSize:get_window_size(win)];
     return 1;
 }
 
@@ -329,8 +304,11 @@ static int window__size(lua_State* L) {
 /// Returns:
 ///  * The `hs.window` object
 static int window__settopleft(lua_State* L) {
+    LuaSkin *skin = [LuaSkin shared];
+    [skin checkArgs:LS_TTABLE, LS_TBREAK];
+
     AXUIElementRef win = get_window_arg(L, 1);
-    NSPoint thePoint = geom_topoint(L, 2);
+    NSPoint thePoint = [skin tableToPointAtIndex:2];
 
     CFTypeRef positionStorage = (CFTypeRef)(AXValueCreate(kAXValueCGPointType, (const void *)&thePoint));
     AXUIElementSetAttributeValue(win, (CFStringRef)NSAccessibilityPositionAttribute, positionStorage);
@@ -354,8 +332,11 @@ static int window__settopleft(lua_State* L) {
 /// Returns:
 ///  * The `hs.window` object
 static int window__setsize(lua_State* L) {
+    LuaSkin *skin = [LuaSkin shared];
+    [skin checkArgs:LS_TTABLE, LS_TBREAK];
+
     AXUIElementRef win = get_window_arg(L, 1);
-    NSSize theSize = geom_tosize(L, 2);
+    NSSize theSize = [skin tableToSizeAtIndex:2];
 
     CFTypeRef sizeStorage = (CFTypeRef)(AXValueCreate(kAXValueCGSizeType, (const void *)&theSize));
     AXUIElementSetAttributeValue(win, (CFStringRef)NSAccessibilitySizeAttribute, sizeStorage);

extensions/mouse/internal.m
@@ -5,20 +5,6 @@
 #import <IOKit/hidsystem/IOHIDParameter.h>
 #import <IOKit/hidsystem/IOHIDLib.h>
 
-static NSPoint hammerspoon_topoint(lua_State* L, int idx) {
-    luaL_checktype(L, idx, LUA_TTABLE);
-    CGFloat x = (lua_getfield(L, idx, "x"), luaL_checknumber(L, -1));
-    CGFloat y = (lua_getfield(L, idx, "y"), luaL_checknumber(L, -1));
-    lua_pop(L, 2);
-    return NSMakePoint(x, y);
-}
-
-static void hammerspoon_pushpoint(lua_State* L, NSPoint point) {
-    lua_newtable(L);
-    lua_pushnumber(L, point.x); lua_setfield(L, -2, "x");
-    lua_pushnumber(L, point.y); lua_setfield(L, -2, "y");
-}
-
 /// hs.mouse.getAbsolutePosition() -> point
 /// Function
 /// Gets the absolute co-ordinates of the mouse pointer
@@ -33,8 +19,11 @@ static void hammerspoon_pushpoint(lua_State* L, NSPoint point) {
 ///  * The co-ordinates returned by this function are in relation to the full size of your desktop. If you have multiple monitors, the desktop is a large virtual rectangle that contains them all (e.g. if you have two 1920x1080 monitors and the mouse is in the middle of the second monitor, the returned table would be `{ x=2879, y=540 }`)
 ///  * Multiple monitors of different sizes can cause the co-ordinates of some areas of the desktop to be negative. This is perfectly normal. 0,0 in the co-ordinates of the desktop is the top left of the primary monitor
 static int mouse_get(lua_State* L) {
+    LuaSkin *skin = [LuaSkin shared];
+    [skin checkArgs:LS_TBREAK];
+
     CGEventRef ourEvent = CGEventCreate(NULL);
-    hammerspoon_pushpoint(L, CGEventGetLocation(ourEvent));
+    [skin pushNSPoint:CGEventGetLocation(ourEvent)];
     CFRelease(ourEvent);
     return 1;
 }
@@ -52,7 +41,10 @@ static int mouse_get(lua_State* L) {
 /// Notes:
 ///  * The co-ordinates given to this function must be in relation to the full size of your desktop. See the notes for `hs.mouse.getAbsolutePosition` for more information
 static int mouse_set(lua_State* L) {
-    CGWarpMouseCursorPosition(hammerspoon_topoint(L, 1));
+    LuaSkin *skin = [LuaSkin shared];
+    [skin checkArgs:LS_TTABLE, LS_TBREAK];
+
+    CGWarpMouseCursorPosition([skin tableToPointAtIndex:1]);
     return 0;
 }
 

commit 9877805fbd19fd0f4eb00dc00194ad3a33fec00d

Fix LuaSkin argument checking in some hs.window calls, which were broken by bf75146c471f82f33031301215bf64775bb304e1

extensions/window/internal.m
@@ -269,7 +269,7 @@ static int window_isstandard(lua_State* L) {
 ///  * A point-table containing the absolute co-ordinates of the top left corner of the window
 static int window__topleft(lua_State* L) {
     LuaSkin *skin = [LuaSkin shared];
-    [skin checkArgs:LS_TBREAK];
+    [skin checkArgs:LS_TUSERDATA, "hs.window", LS_TBREAK];
 
     AXUIElementRef win = get_window_arg(L, 1);
     [skin pushNSPoint:get_window_topleft(win)];
@@ -287,7 +287,7 @@ static int window__topleft(lua_State* L) {
 ///  * A size-table containing the width and height of the window
 static int window__size(lua_State* L) {
     LuaSkin *skin = [LuaSkin shared];
-    [skin checkArgs:LS_TBREAK];
+    [skin checkArgs:LS_TUSERDATA, "hs.window", LS_TBREAK];
 
     AXUIElementRef win = get_window_arg(L, 1);
     [skin pushNSSize:get_window_size(win)];
@@ -305,7 +305,7 @@ static int window__size(lua_State* L) {
 ///  * The `hs.window` object
 static int window__settopleft(lua_State* L) {
     LuaSkin *skin = [LuaSkin shared];
-    [skin checkArgs:LS_TTABLE, LS_TBREAK];
+    [skin checkArgs:LS_TUSERDATA, "hs.window", LS_TTABLE, LS_TBREAK];
 
     AXUIElementRef win = get_window_arg(L, 1);
     NSPoint thePoint = [skin tableToPointAtIndex:2];
@@ -333,7 +333,7 @@ static int window__settopleft(lua_State* L) {
 ///  * The `hs.window` object
 static int window__setsize(lua_State* L) {
     LuaSkin *skin = [LuaSkin shared];
-    [skin checkArgs:LS_TTABLE, LS_TBREAK];
+    [skin checkArgs:LS_TUSERDATA, "hs.window", LS_TTABLE, LS_TBREAK];
 
     AXUIElementRef win = get_window_arg(L, 1);
     NSSize theSize = [skin tableToSizeAtIndex:2];