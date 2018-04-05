8c09321510243a624f046805ab466bfb2de1a528
Chris Jones <cmsj@tenshu.net>
2018-04-05 19:08:50 +0100

    Fix typo in previous commit

--- LuaSkin/LuaSkin/Skin.m
+++ LuaSkin/LuaSkin/Skin.m
@@ -931,7 +931,7 @@ nextarg:
 }
 
 - (void)growStack:(int)slots withMessage:(const char *)message {
-#ifdef 0
+#if 0
     NSLog(@"growStack: %03d:%s", slots, message);
 #endif
     luaL_checkstack(self.L, slots, message);
