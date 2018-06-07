79ed50c8c1fc8a0eeb288e285dd7110cb9ddb2d5
Chris Jones <cmsj@tenshu.net>
2018-06-07 13:19:18 +0100

    Add a useful note to hs.execute docstrings, related to #1810

--- extensions/_coresetup/init.lua
+++ extensions/_coresetup/init.lua
@@ -181,6 +181,7 @@ hs.fileDroppedToDockIconCallback = nil
 ---  * Setting `with_user_env` to true does incur noticeable overhead, so it should only be used if necessary (to set the path or other environment variables).
 ---  * Because this function returns the stdout as it's first return value, it is not quite a drop-in replacement for `os.execute`.  In most cases, it is probable that `stdout` will be the empty string when `status` is nil, but this is not guaranteed, so this trade off of shifting os.execute's results was deemed acceptable.
 ---  * This particular function is most useful when you're more interested in the command's output then a simple check for completion and result codes.  If you only require the result codes or verification of command completion, then `os.execute` will be slightly more efficient.
+---  * If you need to execute commands that have spaces in their paths, use a form like: `hs.execute [["/Some/Path To/An/Executable" "--first-arg" "second-arg"]]`
   hs.execute = function(command, user_env)
     local f
     if user_env then
