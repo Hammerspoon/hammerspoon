d07506eecca657a916dc7a5789dbc29884a2bced
David Peterson <david@randombits.org>
2018-07-19 23:40:47 +1000

    #1909 * Fixed bug with inspecting complex tables.

---
+++ extensions/inspect/test_inspect.lua
@@ -0,0 +1,23 @@
+hs.inspect = require("hs.inspect")
+
+-- tests the case where a custom __init always returns a new table instance as a key/value
+function testInspectAlwaysNewTableKeyValue()
+  local t = setmetatable({}, {
+    __init = function(_, key)
+      return {}
+    end,
+    __pairs = function(self)
+      local function stateless_iter(_, k)
+        if k ~= "a" then
+          return "a", "b"
+        end
+      end
+      -- Return an iterator function, the table, starting point
+    return stateless_iter, self, nil
+    end,
+  })
+
+  assertIsEqual('{a = "b"}', hs.inspect(t))
+
+  return success()
+end
