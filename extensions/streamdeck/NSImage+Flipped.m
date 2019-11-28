cf97f6638bdbdde5993976f0678950b055da481e
Chris Jones <cmsj@tenshu.net>
2019-11-28 17:30:01 +0000

    Comment out a log message

--- extensions/streamdeck/NSImage+Flipped.m
+++ extensions/streamdeck/NSImage+Flipped.m
@@ -27,7 +27,7 @@
     CGFloat xScale = horiz ? -1.0 : 1.0;
     CGFloat yScale = vert ? -1.0 : 1.0;
 
-    NSLog(@"Flipping with xTrans,yTrans: %.1f,%.1f. xScale,yScale: %.1f,%.1f", xTrans, yTrans, xScale, yScale);
+    //NSLog(@"Flipping with xTrans,yTrans: %.1f,%.1f. xScale,yScale: %.1f,%.1f", xTrans, yTrans, xScale, yScale);
 
     [t translateXBy:xTrans yBy:yTrans];
     [t scaleXBy:xScale yBy:yScale];
