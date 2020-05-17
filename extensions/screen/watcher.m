6a484e4c9cde880aea2384a0758217b8473ab76d
Danny Guo <danny@dannyguo.com>
2020-05-17 15:12:05 -0400

    Fix a typo in the hs.screen.watcher.newWithActiveScreen docstrings

--- extensions/screen/watcher.m
+++ extensions/screen/watcher.m
@@ -120,7 +120,7 @@ static int screen_watcher_new(lua_State* L) {
 ///
 /// Notes:
 ///  * A screen layout change usually involves a change that is made from the Displays Preferences Panel or when a monitor is attached or removed.
-///    * `nil` was chosen instead of `falase` for the argument type when this type of change occurs to more closely match the previous behavior of having no argument passed to the callback function.
+///    * `nil` was chosen instead of `false` for the argument type when this type of change occurs to more closely match the previous behavior of having no argument passed to the callback function.
 ///  * An active screen change indicates that the focused or main screen has changed when the user has "Displays have separate spaces" checked in the Mission Control Preferences Panel (the focused display is the display which has the active window and active menubar).
 ///    * Detecting a change in the active display relies on watching for the `NSWorkspaceActiveDisplayDidChangeNotification` message which is not documented by Apple.  While this message has been around at least since OS X 10.9, because it is undocumented, we cannot be positive that Apple won't remove it in a future OS X update.  Because this watcher works by listening for posted messages, should Apple remove this notification, your callback function will no longer receive messages about this change -- it won't crash or change behavior in any other way.  This documentation will be updated if this status changes.
 static int screen_watcher_new_with_active_screen(lua_State* L) {
