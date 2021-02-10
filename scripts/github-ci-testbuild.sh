7b770a99a07604088e7bc2a909d6f76371d80cf8
Chris Jones <cmsj@tenshu.net>
2021-02-10 00:11:07 +0000

    Capture more logs to try and figure out why we're seeing a build failure at the moment in CI, and also try to make the test output uploader, work

--- scripts/github-ci-testbuild.sh
+++ scripts/github-ci-testbuild.sh
@@ -5,6 +5,8 @@ set -o pipefail
 
 make docs
 
-xcodebuild -workspace Hammerspoon.xcworkspace -scheme Release build-for-testing GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty -f `xcpretty-actions-formatter`
+mkdir -p artifacts
+
+xcodebuild -workspace Hammerspoon.xcworkspace -scheme Release build-for-testing GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | tee artifacts/build.log | xcpretty -f `xcpretty-actions-formatter`
 
 make build/html
