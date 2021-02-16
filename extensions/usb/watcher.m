965e8aa65a91b452b4cccb20c09587c4d21deb68
Chris Jones <cmsj@tenshu.net>
2021-02-16 17:50:07 +0000

    Guard hs.usb.watcher DeviceAdded calls to Lua against the callback function somehow being unref'd since it seems to happen in the world.
    Closes #2686
    Fixes HAMMERSPOON-1B

--- extensions/usb/watcher.m
+++ extensions/usb/watcher.m
@@ -144,7 +144,7 @@ void DeviceAdded(void *refCon, io_iterator_t iterator) {
         IOObjectRelease(usbDevice);
 
         // We don't want to trigger callbacks for every device attached before the watcher starts, but we needed to enumerate them to get private device data cached
-        if (!watcher->isFirstRun) {
+        if (!watcher->isFirstRun && watcher->fn != LUA_REFNIL && watcher->fn != LUA_NOREF) {
             [skin pushLuaRef:refTable ref:watcher->fn];
 
             lua_newtable(L);
