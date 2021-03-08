98806e1a1c78daabadbd93e05b7055b5a1cc8788
Chris Jones <cmsj@tenshu.net>
2021-03-08 22:09:19 +0000

    Switch from returning false to returning nil

--- extensions/window/init.lua
+++ extensions/window/init.lua
@@ -38,7 +38,7 @@ window.animationDuration = 0.2
 ---  * None
 ---
 --- Returns:
----  * An `hs.window` object representing the desktop
+---  * An `hs.window` object representing the desktop, or nil if Finder is not running
 ---
 --- Notes:
 ---  * The desktop belongs to Finder.app: when Finder is the active application, you can focus the desktop by cycling
@@ -47,7 +47,7 @@ window.animationDuration = 0.2
 ---  * The desktop is filtered out from `hs.window.allWindows()` (and downstream uses)
 function window.desktop()
   local finder = application.get('com.apple.finder')
-  if not finder then return false end
+  if not finder then return nil end
   for _,w in ipairs(finder:allWindows()) do if w:role()=='AXScrollArea' then return w end end
 end
 
