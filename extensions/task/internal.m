233b96f25ffca403a4a335cd3b1e8e497983757c
Chris Jones <cmsj@tenshu.net>
2021-03-22 12:40:48 +0000

    Wrap hs.task file handle writing in a @try/@catch block to attempt to avoid crashing when a SIGPIPE is raised. Fixes HAMMERSPOON-9E

--- extensions/task/internal.m
+++ extensions/task/internal.m
@@ -88,10 +88,15 @@ void (^writerBlock)(NSFileHandle *) = ^(NSFileHandle *stdInFH) {
         id inputData = (__bridge_transfer id)userData->inputData;
         userData->inputData = nil;
 
-        if ([inputData isKindOfClass:[NSData class]]) {
-            [stdInFH writeData:inputData];
-        } else {
-            [stdInFH writeData:[inputData dataUsingEncoding:NSUTF8StringEncoding]];
+        @try {
+            if ([inputData isKindOfClass:[NSData class]]) {
+                [stdInFH writeData:inputData];
+            } else {
+                [stdInFH writeData:[inputData dataUsingEncoding:NSUTF8StringEncoding]];
+            }
+        }
+        @catch (NSException *exception) {
+            [skin logWarn:@"Exception while writing to hs.task handle"];
         }
 
         // If we're not a streaming task, we can close the file handle now
