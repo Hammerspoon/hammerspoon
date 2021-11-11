02488c2b522978e2799eccd73229d3b3f275ef63
Chris Jones <cmsj@tenshu.net>
2021-11-11 00:03:01 +0000

    Recover build log from CI builds

--- scripts/github-ci-testbuild.sh
+++ scripts/github-ci-testbuild.sh
@@ -6,4 +6,8 @@ set -o pipefail
 
 export IS_CI=1
 
+mkdir -p artifacts
+
 ./scripts/build.sh build -s Release -d -e -x "Hammerspoon/Build Configs/Hammerspoon-Test.xcconfig"
+
+mv build/Release-build.log artifacts/build.log
