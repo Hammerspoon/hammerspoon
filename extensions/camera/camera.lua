5f2a551cb2a4f1b1c0b6c1ff0009c9f5b7c8cf45
Chris Jones <cmsj@tenshu.net>
2021-11-22 00:09:38 +0000

    Add hs.camera and some basic functions/methods to go with it, including a watcher for cameras being added/removed

---
+++ extensions/camera/camera.lua
@@ -0,0 +1,8 @@
+--- === hs.camera ===
+---
+--- Inspect the system's camera devices
+
+local module = require("hs.libcamera")
+
+-- Return Module Object --------------------------------------------------
+return module
