e911565e3415f2af8de7f6566ea61c675e698491
Chris Jones <cmsj@tenshu.net>
2021-12-13 09:19:14 +0000

    Update build_docs.py to use correct mistune syntax

--- scripts/docs/bin/build_docs.py
+++ scripts/docs/bin/build_docs.py
@@ -439,7 +439,7 @@ def process_markdown(data):
     from pygments.lexers import get_lexer_by_name
     from pygments.formatters import html
 
-    class HighlightRenderer(mistune.Renderer):
+    class HighlightRenderer(mistune.HTMLRenderer):
         def block_code(self, code, lang):
             if not lang:
                 return '\n<pre><code>%s</code></pre>\n' % \
