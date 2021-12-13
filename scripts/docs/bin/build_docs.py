0ccc98f7447192c0193e7ff19c724022d3db84a7
Chris Jones <cmsj@tenshu.net>
2021-12-13 09:25:27 +0000

    Fix another mistune API issue

--- scripts/docs/bin/build_docs.py
+++ scripts/docs/bin/build_docs.py
@@ -440,7 +440,7 @@ def process_markdown(data):
     from pygments.formatters import html
 
     class HighlightRenderer(mistune.HTMLRenderer):
-        def block_code(self, code, lang):
+        def block_code(self, code, lang=None):
             if not lang:
                 return '\n<pre><code>%s</code></pre>\n' % \
                     mistune.escape(code)
