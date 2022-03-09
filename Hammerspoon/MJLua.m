Hammerspoon/hammerspoon

commit d24bf24fd7e59626f59d3b336c937b2fb0b7b833

Fix two code analysis issues that Xcode identified

Hammerspoon/MJLua.m
@@ -522,10 +522,17 @@ static int automaticallyChecksForUpdates(lua_State *L) {
             // for any method which accepts the same signature list for the target.
                 [myInvocation setSelector:@selector(setAutomaticallyChecksForUpdates:)];
                 [myInvocation setArgument:&myBoolValue atIndex:2];
+                //[myInvocation setReturnValue:&myBoolValue];
                 [myInvocation invoke];
 
             }
-            lua_pushboolean(L, (BOOL)[sharedUpdater performSelector:@selector(automaticallyChecksForUpdates)]) ;
+            BOOL autoCheckUpdates = NO;
+            NSMethodSignature *autoCheckSignature = [NSClassFromString(@"SUUpdater") instanceMethodSignatureForSelector:@selector(automaticallyChecksForUpdates)];
+            NSInvocation *autoCheckInvocation = [NSInvocation invocationWithMethodSignature:autoCheckSignature];
+            [autoCheckInvocation setTarget:sharedUpdater];
+            [autoCheckInvocation setReturnValue:&autoCheckUpdates];
+            [autoCheckInvocation invoke];
+            lua_pushboolean(L, autoCheckUpdates) ;
 #pragma clang diagnostic pop
         } else {
             [skin logWarn:@"Sparkle Update framework not available for the running instance of Hammerspoon."] ;

Hammerspoon/HSuicore.m
@@ -575,11 +575,8 @@ cleanup:
     NSImage *image = nil;
     CGWindowImageOption makeOpaque = keepTransparency ? kCGWindowImageDefault : kCGWindowImageShouldBeOpaque;
     CGRect windowRect = CGRectNull;
-    CFArrayRef targetWindow = CFArrayCreate(NULL, (const void **)(&windowID), 1, NULL);
-    CGImageRef windowImage = CGWindowListCreateImageFromArray(windowRect,
-                                                              targetWindow,
-                                                              kCGWindowImageBoundsIgnoreFraming | makeOpaque);
-    CFRelease(targetWindow);
+    CGImageRef windowImage = CGWindowListCreateImage(windowRect, kCGWindowListOptionIncludingWindow, windowID, kCGWindowImageBoundsIgnoreFraming | makeOpaque);
+
     if (windowImage) {
         image = [[NSImage alloc] initWithCGImage:windowImage size:windowRect.size];
         CFRelease(windowImage);

commit f63c1c56c734af6c533d09a63aa676147d250797

Fix hs.automaticallyCheckForUpdates() which was broken by #3136

Hammerspoon/MJLua.m
@@ -530,8 +530,9 @@ static int automaticallyChecksForUpdates(lua_State *L) {
             NSMethodSignature *autoCheckSignature = [NSClassFromString(@"SUUpdater") instanceMethodSignatureForSelector:@selector(automaticallyChecksForUpdates)];
             NSInvocation *autoCheckInvocation = [NSInvocation invocationWithMethodSignature:autoCheckSignature];
             [autoCheckInvocation setTarget:sharedUpdater];
-            [autoCheckInvocation setReturnValue:&autoCheckUpdates];
+            [autoCheckInvocation setSelector:@selector(automaticallyChecksForUpdates)];
             [autoCheckInvocation invoke];
+            [autoCheckInvocation getReturnValue:&autoCheckUpdates];
             lua_pushboolean(L, autoCheckUpdates) ;
 #pragma clang diagnostic pop
         } else {