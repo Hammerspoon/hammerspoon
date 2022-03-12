32a68371d8ba1a74e7da00cf1eec79569cee9b02
asmagill <asmagill@users.noreply.github.com>
2022-03-11 23:57:51 -0600

    add user dylib support to package.cpath to aid in migrating external modules into core

--- Hammerspoon/setup.lua
+++ Hammerspoon/setup.lua
@@ -1,7 +1,7 @@
 local modpath, frameworkspath, prettypath, fullpath, configdir, docstringspath, hasinitfile, autoload_extensions = ...
 
 package.path=configdir.."/?.lua"..";"..configdir.."/?/init.lua"..";"..configdir.."/Spoons/?.spoon/init.lua"..";"..package.path..";"..modpath.."/?.lua"..";"..modpath.."/?/init.lua"
-package.cpath=configdir.."/?.so"..";"..package.cpath..";"..frameworkspath.."/?.dylib"
+package.cpath=configdir.."/?.dylib"..";"..configdir.."/?.so"..";"..package.cpath..";"..frameworkspath.."/?.dylib"
 
 print("-- package.path: "..package.path)
 print("-- package.cpath: "..package.cpath)
