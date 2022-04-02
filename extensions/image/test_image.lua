59186f92d597ddd4f46c1c1ab10500771e4c7c3d
Chris Hocking <latenitefilms@users.noreply.github.com>
2022-04-02 22:50:54 +0100

    Removed white space

--- extensions/image/test_image.lua
+++ extensions/image/test_image.lua
@@ -1,9 +1,7 @@
 function testGetExifFromPath()
-  
   assertIsNil(hs.image.getExifFromPath(""))
   assertIsNil(hs.image.getExifFromPath("asdfasdfasddsa"))
   assertIsNil(hs.image.getExifFromPath("/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.sdef"))
   assertIsTable(hs.image.getExifFromPath("/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns"))
-
   return success()
 end
