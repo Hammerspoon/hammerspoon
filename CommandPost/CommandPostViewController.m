1431caf95fd391938ac4d911d867145dd915a7b7
Chris Hocking <latenitefilms@users.noreply.github.com>
2022-04-29 21:44:08 +1000

    Proof of concept

--- CommandPost/CommandPostViewController.m
+++ CommandPost/CommandPostViewController.m
@@ -11,15 +11,26 @@
 #import <ProExtension/ProExtension.h>
 #import <ProExtensionHost/ProExtensionHost.h>
 
-@interface CommandPostViewController ()
+@interface CommandPostViewController () <FCPXTimelineObserver>
+@property (weak) IBOutlet NSButton *doSomething;
 @end
 
-
 @implementation CommandPostViewController
+- (IBAction)doSomething:(id)sender {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"Do something pressed!"];
+    [alert setInformativeText:@"You pressed a button."];
+    [alert addButtonWithTitle:@"OK"];
+    [alert runModal];    
+}
 
 - (void) awakeFromNib
 {
     [super awakeFromNib];
+    
+    // Set up the timeline observer:
+    id<FCPXHost> host = (id<FCPXHost>)ProExtensionHostSingleton();
+    [host.timeline addTimelineObserver:self];
 }
 
 - (NSString*) nibName
@@ -27,17 +38,55 @@
     return @"CommandPostViewController";
 }
 
+- (void) activeSequenceChanged
+{
+    id<FCPXHost> host = (id<FCPXHost>)ProExtensionHostSingleton();
+    FCPXSequence* sequence = host.timeline.activeSequence;
+    
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"activeSequenceChanged"];
+    [alert setInformativeText:sequence.name];
+    [alert addButtonWithTitle:@"OK"];
+    [alert runModal];
+}
+
+- (void)playheadTimeChanged {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"playheadTimeChanged"];
+    [alert setInformativeText:@""];
+    [alert addButtonWithTitle:@"OK"];
+    [alert runModal];
+}
+
+- (void)sequenceTimeRangeChanged {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"sequenceTimeRangeChanged"];
+    [alert setInformativeText:@""];
+    [alert addButtonWithTitle:@"OK"];
+    [alert runModal];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
 }
 
 - (NSString*) hostInfoString
 {
-	id<FCPXHost> host = (id<FCPXHost>)ProExtensionHostSingleton();
-	
-	return [NSString stringWithFormat:@"%@ %@", host.name, host.versionString];
+    id<FCPXHost> host = (id<FCPXHost>)ProExtensionHostSingleton();
+    
+    return [NSString stringWithFormat:@"%@ %@", host.name, host.versionString];
 }
 
+
+- (BOOL)commitEditingAndReturnError:(NSError *__autoreleasing  _Nullable * _Nullable)error {
+    
+    return YES;
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+    
+}
 
 @end
