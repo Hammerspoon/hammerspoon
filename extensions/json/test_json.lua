3b003268da3689fce56716f49421c9d9cf85664d
Arnold Lestin <amlestin@gmail.com>
2022-06-06 20:11:46 -0500

    catch error and test

--- extensions/json/test_json.lua
+++ extensions/json/test_json.lua
@@ -18,7 +18,10 @@ function testEncodeDecode()
 end
 
 function testEncodeDecodeFailures()
-  hs.json.encode(notATable)
+  local status, err = pcall(hs.json.encode(notATable))
+  
+  -- assert an error was raised
+  assertFalse(status)
 
   return success()
 end
