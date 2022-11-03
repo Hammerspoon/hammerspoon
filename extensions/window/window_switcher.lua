153edabc0880afc7534ba6a9ffe58250275c3115
Kevin Slagle <kjslag@gmail.com>
2022-11-03 07:49:12 -0700

    remove timer delay before window switch
    El Capitan is no longer supported by Hammerspoon, so we no longer need the timer delay workaround.

--- extensions/window/window_switcher.lua
+++ extensions/window/window_switcher.lua
@@ -224,7 +224,7 @@ local function exit(self)
   --  if windows[selected]:application():bundleID()~='com.apple.finder' then
   --    windows[selected]:focus()
   --  else
-  timer.doAfter(0.01,function()windows[selected]:focus()end) -- el cap bugs out (desktop "floats" on top) if done directly
+  function()windows[selected]:focus()
   --  end
 end
 
