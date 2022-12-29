97a4fa22d27529945f6b446519664c5d79d64db8
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-29 03:09:58 -0500

    spelling: explicit
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- extensions/doc/markdown.h
+++ extensions/doc/markdown.h
@@ -39,7 +39,7 @@ extern "C" {
 enum mkd_autolink {
 	MKDA_NOT_AUTOLINK,	/* used internally when it is not an autolink*/
 	MKDA_NORMAL,		/* normal http/http/ftp/mailto/etc link */
-	MKDA_EMAIL,			/* e-mail link without explit mailto: */
+	MKDA_EMAIL,			/* e-mail link without explicit mailto: */
 };
 
 enum mkd_tableflags {
