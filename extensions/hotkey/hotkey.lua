5942fb47e93fb916f1386de7cdc363838613ba3e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-29 03:16:08 -0500

    spelling: hotkey
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- extensions/hotkey/hotkey.lua
+++ extensions/hotkey/hotkey.lua
@@ -238,7 +238,7 @@ end
 --- Returns:
 ---  * if the hotkey combination is in use by a system function, returns a table containing the following keys:
 ---    * keycode - the numberic keycode for the hotkey
----    * mods    - a numeric representation of the modifier flags for the htokey
+---    * mods    - a numeric representation of the modifier flags for the hotkey
 ---    * enabled - a boolean indicating whether or not the key is currently enabled
 ---  * if the hotkey combination is not in use by the operating system, returns the boolean value `false`
 ---
