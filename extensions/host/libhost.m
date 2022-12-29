b66c39a59beb505e6e10a6d47276e39e652baed9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-29 04:24:48 -0500

    spelling: parsable
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- extensions/host/libhost.m
+++ extensions/host/libhost.m
@@ -350,7 +350,7 @@ static int hs_thermalStateString(lua_State *L) {
 ///  * The operating system version as a table containing the keys major, minor, and patch corresponding to the version number determined and a key named "exact" or "approximation" depending upon the method used to determine the OS Version information.
 ///
 /// Notes:
-///  * Prior to 10.10 (Yosemite), there was no definitive way to reliably get an exact OS X version number without either mapping it to the Darwin kernel version, mapping it to the AppKitVersionNumber (the recommended method), or parsing the result of NSProcessingInfo's `operatingSystemVersionString` selector, which Apple states is not guaranteed to be reliably parseable.
+///  * Prior to 10.10 (Yosemite), there was no definitive way to reliably get an exact OS X version number without either mapping it to the Darwin kernel version, mapping it to the AppKitVersionNumber (the recommended method), or parsing the result of NSProcessingInfo's `operatingSystemVersionString` selector, which Apple states is not guaranteed to be reliably parsable.
 ///    * for OS X versions prior to 10.10, the version number is approximately determined by evaluating the AppKitVersionNumber.  For these operating systems, the `approximate` key is defined and set to true, as the exact patch level cannot be definitively determined.
 ///    * for OS X Versions starting at 10.10 and going forward, an exact value for the version number can be determined with NSProcessingInfo's `operatingSystemVersion` selector and the `exact` key is defined and set to true if this method is used.
 static int hs_operatingSystemVersion(lua_State *L) {
