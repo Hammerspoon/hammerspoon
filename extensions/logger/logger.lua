22620404b3352e26f59d0d834a28128c65ff5b6b
Evgeni Kolev <evgenysw@gmail.com>
2025-05-17 11:32:52 +0300

    Fix typo in logger's documentation

--- extensions/logger/logger.lua
+++ extensions/logger/logger.lua
@@ -222,7 +222,7 @@ logger.defaultLogLevel = 'warning'
 ---
 --- Notes:
 ---  * the logger instance created by this method is not a regular object, but a plain table with "static" functions;
----    therefore, do not use the colon syntax for so-called "methods" in this module (as in `mylogger.setLogLevel(3)`);
+---    therefore, do not use the colon syntax for so-called "methods" in this module (as in `mylogger:setLogLevel(3)`);
 ---    you must instead use the regular dot syntax: `mylogger.setLogLevel(3)`
 ---
 --- Example:
